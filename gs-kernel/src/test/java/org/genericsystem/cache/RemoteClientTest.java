7a229af703edf6a7ef57f82423d84af2adf36a60
Matthieu Beaupérin <mbeauperin@middlewarefactory.com>
2015-07-31 11:18:33 +0200

    Merge

--- gs-kernel/src/test/java/org/genericsystem/cache/RemoteClientTest.java
+++ gs-kernel/src/test/java/org/genericsystem/cache/RemoteClientTest.java
@@ -3,32 +3,27 @@ package org.genericsystem.cache;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-
 import org.genericsystem.kernel.Statics;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class RemoteClientTest extends AbstractTest {
 
-	@BeforeClass
+	@BeforeTest
 	public void beforeClass() {
-		HttpGSServer.create();
+		HttpLocalGSServer.create();
 
 	}
 
-	private interface HttpGSServer {
+	private interface HttpLocalGSServer {
 
 		public static void close() {
-			Vertx.vertx().undeploy(HttpGSClient.class.getName());
+			Vertx.vertx().undeploy(HttpGSServer.class.getName());
 		};
 
 		public static void create(int port, String persistanceRepositoryPath) {
-			Vertx.vertx().deployVerticle(
-					HttpGSClient.class.getName(),
-					new DeploymentOptions().setConfig(new JsonObject().put(
-							"port", port).put("persistanceRepositoryPath",
-							persistanceRepositoryPath)));
+			Vertx.vertx().deployVerticle(HttpGSServer.class.getName(), new DeploymentOptions().setConfig(new JsonObject().put("port", port).put("persistanceRepositoryPath", persistanceRepositoryPath)));
 		}
 
 		public static void create() {
@@ -37,17 +32,35 @@ public class RemoteClientTest extends AbstractTest {
 
 	}
 
-	@AfterClass
+	@AfterTest
 	public void afterClass() {
-		HttpGSServer.close();
+		HttpLocalGSServer.close();
 	}
 
-	@Test
+	@Test(invocationCount = 10)
 	public void test_001() {
+
+		System.out.println("Thread : " + System.identityHashCode(Thread.currentThread()));
 		ClientEngine clientEngine = new ClientEngine("firstEngine");
-		ClientGeneric myVehicle = clientEngine.addInstance("Vehicle");
-		clientEngine.getCurrentCache().flush();
-		ClientEngine clientEngine2 = new ClientEngine("secondEngine");
+		// Thread thread = new Thread() {
+		// @Override
+		// public void run() {
+		// System.out.println("Thread : " + System.identityHashCode(Thread.currentThread()));
+		// ClientEngine clientEngine = new ClientEngine("firstEngine");
+		// }
+		// };
+		// thread.start();
+		// try {
+		// thread.join();
+		// } catch (InterruptedException e) {
+		// e.printStackTrace();
+		// }
+
+		// System.out.println("Thread : " + System.identityHashCode(Thread.currentThread()));
+		// ClientEngine clientEngine = new ClientEngine("firstEngine");
+		// ClientGeneric myVehicle = clientEngine.addInstance("Vehicle");
+		// clientEngine.getCurrentCache().flush();
+		// ClientEngine clientEngine2 = new ClientEngine("secondEngine");
 		// ClientGeneric mySecondVehicle = clientEngine2.getInstance("Vehicle");
 
 	}
