687f742ebf001521fd4780c7f0bc52ecb2fdc39f
Matthieu Beaupérin <mbeauperin@middlewarefactory.com>
2015-09-03 15:58:12 +0200

    code cleaning

--- gs-kernel/src/main/java/org/genericsystem/cache/HttpGSServer.java
+++ gs-kernel/src/main/java/org/genericsystem/cache/HttpGSServer.java
@@ -4,27 +4,35 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+
 import org.genericsystem.api.core.exceptions.ConcurrencyControlException;
 import org.genericsystem.api.core.exceptions.OptimisticLockConstraintViolationException;
 import org.genericsystem.kernel.Root;
 
-public abstract class HttpGSServer extends AbstractGSServer {
+public class HttpGSServer extends AbstractGSServer {
 
 	private List<HttpServer> httpServers = new ArrayList<>();
+	private final int port;
+	private final String host;
 
-	@Override
+	public HttpGSServer(GSDeploymentOptions options) {
+		super(options);
+		this.port = options.getPort();
+		this.host = options.getHost();
+	}
+
 	public void start() {
-		super.start();
 		Vertx vertx = Vertx.vertx();
 		for (int i = 0; i < 2 * Runtime.getRuntime().availableProcessors(); i++) {
-			HttpServer httpServer = vertx.createHttpServer(new HttpServerOptions().setPort(config().getInteger("port")));
+			HttpServer httpServer = vertx.createHttpServer(new HttpServerOptions().setPort(port).setHost(host));
 			httpServer.requestHandler(request -> {
 				String path = request.path();
-				Root root = getRoots().get(path);
+				Root root = roots.get(path);
 				if (root == null)
 					throw new IllegalStateException("Unable to find database :" + path);
 				request.exceptionHandler(e -> {
