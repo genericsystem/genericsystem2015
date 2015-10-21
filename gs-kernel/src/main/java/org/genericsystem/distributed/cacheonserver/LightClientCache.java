c806d9f079d5a993c30fa67f8e0e2af255ab11e4
Juba MEHDI <jmehdi@middlewarefactory.com>
2015-10-21 14:36:56 +0200

    rien

--- gs-kernel/src/main/java/org/genericsystem/distributed/cacheonserver/LightClientCache.java
+++ gs-kernel/src/main/java/org/genericsystem/distributed/cacheonserver/LightClientCache.java
@@ -1,7 +1,9 @@
 package org.genericsystem.distributed.cacheonserver;
 
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.genericsystem.api.core.Snapshot;
 import org.genericsystem.api.core.exceptions.ConcurrencyControlException;
