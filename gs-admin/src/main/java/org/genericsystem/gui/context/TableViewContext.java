7c35a4bf7fdf08318fe8f89f8c55ac6050f4ea0f
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2015-11-16 08:40:08 +0100

    Refactoring, Remove unused files

--- gs-admin/src/main/java/org/genericsystem/gui/context/TableViewContext.java
+++ gs-admin/src/main/java/org/genericsystem/gui/context/TableViewContext.java
@@ -1,13 +1,10 @@
 package org.genericsystem.gui.context;
 
 import java.util.Objects;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
 
 import org.genericsystem.common.Generic;
 import org.genericsystem.distributed.cacheonclient.CocCache;
@@ -17,15 +14,7 @@ public class TableViewContext extends ForeachContext {
 	public ObservableValue<String> columnTitle;
 
 	public TableViewContext(GenericContext parent) {
-		super(parent, parent.genericProperty);
-
-		Function<Generic, GenericContext> transformation = generic -> {
-			GenericContext genericContext = new GenericContext(parent, genericProperty);
-			genericContext.genericProperty = new ReadOnlyObjectWrapper<Generic>(generic);
-			return genericContext;
-		};
-		Supplier<ObservableList<Generic>> observableListSupplier = () -> ((CocCache) parent.getCurrentCache()).getInstancesObservableList(genericProperty.getValue());
+		super(parent, parent.genericProperty, generic -> ((CocCache) parent.getCurrentCache()).getInstancesObservableList(generic), generic -> new GenericContext(parent, new ReadOnlyObjectWrapper<Generic>(generic)));
 		columnTitle = Bindings.createStringBinding(() -> Objects.toString(genericProperty.getValue()), genericProperty);
-		subContexObservableList = this.<Generic, GenericContext> getSubContextsObservableList(transformation, observableListSupplier, genericProperty);
 	}
 }
