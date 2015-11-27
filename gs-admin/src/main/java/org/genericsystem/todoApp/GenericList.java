b53fd9d5d4ccceeb92822e2e6b888f45cf4bfcbf
Juba MEHDI <jmehdi@middlewarefactory.com>
2015-11-27 17:57:32 +0100

    implements methods (clean,mount,unmount and flush)

--- gs-admin/src/main/java/org/genericsystem/todoApp/GenericList.java
+++ gs-admin/src/main/java/org/genericsystem/todoApp/GenericList.java
@@ -12,6 +12,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import org.genericsystem.common.Generic;
@@ -56,23 +57,53 @@ public class GenericList {
 
 		Thread.sleep(1000);
 		System.out.println(dependenciesObservableList);
-
 		genericList = FXCollections.observableArrayList();
 		genericList.addAll(dependenciesObservableList.stream().map(dep -> new GenericWrapper(dep)).collect(Collectors.toList()));
+
 		System.out.println(genericList);
+	}
+
+	private void initGenericList() {
+		genericList.clear();
+		genericList.addAll(dependenciesObservableList.stream().map(dep -> new GenericWrapper(dep)).collect(Collectors.toList()));
+
+	}
+
+	public void flush() {
+		engine.getCurrentCache().flush();
+	}
+
+	public void clear() {
+		engine.getCurrentCache().clear();
+		try {
+			Thread.sleep(100);
+			initGenericList();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// genericList.addAll(dependenciesObservableList.stream().map(dep -> new GenericWrapper(dep)).collect(Collectors.toList()));
+
+	}
+
+	public void mount() {
+		engine.getCurrentCache().mount();
+		initGenericList();
+	}
+
+	public void unmount() {
+		engine.getCurrentCache().unmount();
+		initGenericList();
 	}
 
 	public void create() {
 		engine.addInstance(name.getValue());
-		genericList.clear();
-		genericList.addAll(dependenciesObservableList.stream().map(dep -> new GenericWrapper(dep)).collect(Collectors.toList()));
+		initGenericList();
 	}
 
 	public void remove(GenericWrapper genericWrapper) {
 		genericWrapper.remove();
-		genericList.clear();
-		genericList.addAll(dependenciesObservableList.stream().map(dep -> new GenericWrapper(dep)).collect(Collectors.toList()));
-
+		initGenericList();
 	}
 
 	public ObservableList<GenericWrapper> getGenericList() {
@@ -105,9 +136,14 @@ public class GenericList {
 		Element genericsHBox = new Element(genericsVBox, VBox.class, "", Binding.bindToMethod(GenericList.class, GenericList::getGenericList, Binder.foreach()));
 		Element genericsLabel = new Element(genericsHBox, Label.class, "", Binding.bindToMethod(GenericWrapper.class, GenericWrapper::getString, Binder.textBind()));
 		Element genericsRemoveButton = new Element(genericsHBox, Button.class, "remove", Binding.bindToMethod(GenericList.class, "remove", Binder.methodBind(), GenericWrapper.class));
-
 		Element genericsCreatLabel = new Element(genericsVBox, TextField.class, "", Binding.bindToField(GenericList.class, "name", Binder.inputTextBind()));
-		Element genericsCreateButton = new Element(genericsVBox, Button.class, "create", Binding.bindToMethod(GenericList.class, "create", Binder.methodBind()));
+
+		Element genericsHB = new Element(genericsVBox, HBox.class, "");
+		Element genericsCreateButton = new Element(genericsHB, Button.class, "create", Binding.bindToMethod(GenericList.class, "create", Binder.methodBind()));
+		Element genericsFlushButton = new Element(genericsHB, Button.class, "flush", Binding.bindToMethod(GenericList.class, "flush", Binder.methodBind()));
+		Element genericsClearButton = new Element(genericsHB, Button.class, "clear", Binding.bindToMethod(GenericList.class, "clear", Binder.methodBind()));
+		Element genericsMountButton = new Element(genericsHB, Button.class, "mount", Binding.bindToMethod(GenericList.class, "mount", Binder.methodBind()));
+		Element genericsUnmountButton = new Element(genericsHB, Button.class, "unmount", Binding.bindToMethod(GenericList.class, "unmount", Binder.methodBind()));
 
 		return genericsVBox.apply(this).getNode();
 	}
