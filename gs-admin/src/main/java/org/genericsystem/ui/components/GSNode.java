989dfd8911d8b4678a47f18b863c38dffd5259a3
Juba MEHDI <jmehdi@middlewarefactory.com>
2015-12-14 10:49:44 +0100

    merge

---
+++ gs-admin/src/main/java/org/genericsystem/ui/components/GSNode.java
@@ -0,0 +1,41 @@
+package org.genericsystem.ui.components;
+
+import java.util.function.Function;
+
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+
+import org.genericsystem.ui.Element;
+
+public abstract class GSNode<Component extends GSNode<Component, N>, N extends Node> extends Element<N> {
+
+	public GSNode(Element parent, Class<N> nodeClass) {
+		super(parent, nodeClass);
+	}
+
+	public <PARENTNODE> GSNode(Element parent, Class<N> paneClass, Function<? super PARENTNODE, ObservableList<?>> getGraphicChildren) {
+		super(parent, paneClass, getGraphicChildren);
+	}
+
+	public <M> Component setStyleClass(String text) {
+		addObservableListBoot(N::getStyleClass, text);
+		return (Component) this;
+	}
+
+	public <M> Component setOptionalStyleClass(Function<M, ObservableValue<Boolean>> function, String text) {
+		addObservableListBinding(N::getStyleClass, function, text);
+		return (Component) this;
+	}
+
+	public <M> Component setVisibility(boolean visibility) {
+		addBoot(N::visibleProperty, visibility);
+		return (Component) this;
+	}
+
+	public <M> Component setOptionalVisibility(Function<M, ObservableValue<Boolean>> observableVisibility) {
+		addBinding(N::visibleProperty, observableVisibility);
+		return (Component) this;
+	}
+
+}
