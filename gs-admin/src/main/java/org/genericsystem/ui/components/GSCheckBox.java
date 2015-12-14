f525d2f200c727c55770687bc6393818082a9aac
Juba MEHDI <jmehdi@middlewarefactory.com>
2015-12-14 11:59:07 +0100

    create GSCheckBox component

--- gs-admin/src/main/java/org/genericsystem/ui/components/GSCheckBox.java
+++ gs-admin/src/main/java/org/genericsystem/ui/components/GSCheckBox.java
@@ -1,5 +1,31 @@
 package org.genericsystem.ui.components;
 
-public class GSCheckBox {
+import java.util.function.Function;
 
+import javafx.beans.property.Property;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.CheckBox;
+
+import org.genericsystem.ui.Element;
+
+public class GSCheckBox extends GSRegion<GSCheckBox, CheckBox> {
+
+	public GSCheckBox(Element parent) {
+		super(parent, CheckBox.class);
+	}
+
+	public <M> GSCheckBox(Element parent, Function<M, Property<Boolean>> function) {
+		super(parent, CheckBox.class);
+		bindSelectedProperty(function);
+	}
+
+	public <M> GSCheckBox setText(Function<M, ObservableValue<String>> function) {
+		addBinding(CheckBox::textProperty, function);
+		return this;
+	}
+
+	public <M> GSCheckBox bindSelectedProperty(Function<M, Property<Boolean>> function) {
+		addBidirectionalBinding(CheckBox::selectedProperty, function);
+		return this;
+	}
 }
