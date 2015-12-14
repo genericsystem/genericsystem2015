cdb0918e2b4b65fe221bdd2cc03065bdd9bd9e98
Juba MEHDI <jmehdi@middlewarefactory.com>
2015-12-14 13:47:17 +0100

    merge

--- gs-admin/src/main/java/org/genericsystem/ui/components/GSPane.java
+++ gs-admin/src/main/java/org/genericsystem/ui/components/GSPane.java
@@ -1,10 +1,10 @@
 package org.genericsystem.ui.components;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
-
+import javafx.beans.property.Property;
 import javafx.collections.ObservableList;
 import javafx.scene.layout.Pane;
-
 import org.genericsystem.ui.Element;
 
 public class GSPane<Component extends GSPane<Component, N>, N extends Pane> extends GSRegion<Component, N> {
@@ -15,6 +15,12 @@ public class GSPane<Component extends GSPane<Component, N>, N extends Pane> exte
 
 	public <PARENTNODE> GSPane(Element parent, Class<N> paneClass, Function<? super PARENTNODE, ObservableList<?>> getGraphicChildren) {
 		super(parent, paneClass, getGraphicChildren);
+	}
+
+	@Override
+	public <M, T> Component addForEachMetaBinding(Function<M, ObservableList<T>> function, Function<T, Property<M>> injectedProperty, Consumer<Element<N>> subModelInit) {
+		super.addForEachMetaBinding(function, injectedProperty, subModelInit);
+		return (Component) this;
 	}
 
 }
