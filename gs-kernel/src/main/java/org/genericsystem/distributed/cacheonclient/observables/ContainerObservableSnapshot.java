ccd69edb8dfd75fa3587ce402abca0990f09015e
Juba MEHDI <jmehdi@middlewarefactory.com>
2016-01-11 11:23:18 +0100

    merge

--- gs-kernel/src/main/java/org/genericsystem/distributed/cacheonclient/observables/ContainerObservableSnapshot.java
+++ gs-kernel/src/main/java/org/genericsystem/distributed/cacheonclient/observables/ContainerObservableSnapshot.java
@@ -1,65 +1,65 @@
-package org.genericsystem.distributed.cacheonclient.observables;
-
-import java.util.Iterator;
-
-import org.genericsystem.common.PseudoConcurrentCollection;
-
-public class ContainerObservableSnapshot<E> extends AbstractObservableSnapshot<E> {
-
-	PseudoConcurrentCollection<E> container = new PseudoConcurrentCollection<>();
-
-	@Override
-	public E get(int index) {
-		return container.getByIndex(index);
-	}
-
-	public E get(Object o) {
-		return container.get(o);
-	}
-
-	@Override
-	public Iterator<E> iterator() {
-		Iterator<E> containerIt = container.iterator();
-		return new Iterator<E>() {
-
-			private E next;
-
-			@Override
-			public boolean hasNext() {
-				return containerIt.hasNext();
-			}
-
-			@Override
-			public E next() {
-				return next = containerIt.next();
-			}
-
-			@Override
-			public void remove() {
-				containerIt.remove();
-				callObservers(new SimpleRemoveChange(next));
-			}
-		};
-	}
-
-	@Override
-	public int size() {
-		return container.size();
-	}
-
-	@Override
-	public boolean add(E element) {
-		container.add(element);
-		callObservers(new SimpleAddChange(element));
-		return true;
-	}
-
-	@Override
-	public boolean remove(Object element) {
-		boolean result = container.remove((E) element);
-		if (result)
-			callObservers(new SimpleRemoveChange((E) element));
-		return result;
-	}
-
-}
+//package org.genericsystem.distributed.cacheonclient.observables;
+//
+//import java.util.Iterator;
+//
+//import org.genericsystem.common.PseudoConcurrentCollection;
+//
+//public class ContainerObservableSnapshot<E> extends AbstractObservableSnapshot<E> {
+//
+//	PseudoConcurrentCollection<E> container = new PseudoConcurrentCollection<>();
+//
+//	@Override
+//	public E get(int index) {
+//		return container.getByIndex(index);
+//	}
+//
+//	public E get(Object o) {
+//		return container.get(o);
+//	}
+//
+//	@Override
+//	public Iterator<E> iterator() {
+//		Iterator<E> containerIt = container.iterator();
+//		return new Iterator<E>() {
+//
+//			private E next;
+//
+//			@Override
+//			public boolean hasNext() {
+//				return containerIt.hasNext();
+//			}
+//
+//			@Override
+//			public E next() {
+//				return next = containerIt.next();
+//			}
+//
+//			@Override
+//			public void remove() {
+//				containerIt.remove();
+//				callObservers(new SimpleRemoveChange(next));
+//			}
+//		};
+//	}
+//
+//	@Override
+//	public int size() {
+//		return container.size();
+//	}
+//
+//	@Override
+//	public boolean add(E element) {
+//		container.add(element);
+//		callObservers(new SimpleAddChange(element));
+//		return true;
+//	}
+//
+//	@Override
+//	public boolean remove(Object element) {
+//		boolean result = container.remove((E) element);
+//		if (result)
+//			callObservers(new SimpleRemoveChange((E) element));
+//		return result;
+//	}
+//
+// }
--- gs-kernel/src/main/java/org/genericsystem/distributed/cacheonclient/observables/ObservableSnapshot.java
+++ gs-kernel/src/main/java/org/genericsystem/distributed/cacheonclient/observables/ObservableSnapshot.java
@@ -1,29 +1,29 @@
-package org.genericsystem.distributed.cacheonclient.observables;
-
-import java.util.function.Predicate;
-
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
-import javafx.collections.ObservableSet;
-
-public interface ObservableSnapshot<T> extends ObservableSet<T> {
-
-	T get(int index);
-
-	default ObservableSnapshot<T> filtered(Predicate<T> predicate) {
-		return new FilterObservableSnapshotImpl<>(this, predicate);
-	}
-
-	default ObservableSnapshot<T> filtered(ObservableValue<Predicate<T>> predicate) {
-		return new ObservableFilterObservableSnapshotImpl<>(this, predicate);
-	}
-
-	default ObservableSnapshot<T> concat(ObservableSnapshot<T> toConcatenate) {
-		return new ConcatObservableSnapshotImpl<>(this, toConcatenate);
-	}
-
-	default ObservableList<T> toObservableList() {
-		return new ObservableListSnapshot<>(this);
-	}
-
-}
+//package org.genericsystem.distributed.cacheonclient.observables;
+//
+//import java.util.function.Predicate;
+//
+//import javafx.beans.value.ObservableValue;
+//import javafx.collections.ObservableList;
+//import javafx.collections.ObservableSet;
+//
+//public interface ObservableSnapshot<T> extends ObservableSet<T> {
+//
+//	T get(int index);
+//
+//	default ObservableSnapshot<T> filtered(Predicate<T> predicate) {
+//		return new FilterObservableSnapshotImpl<>(this, predicate);
+//	}
+//
+//	default ObservableSnapshot<T> filtered(ObservableValue<Predicate<T>> predicate) {
+//		return new ObservableFilterObservableSnapshotImpl<>(this, predicate);
+//	}
+//
+//	default ObservableSnapshot<T> concat(ObservableSnapshot<T> toConcatenate) {
+//		return new ConcatObservableSnapshotImpl<>(this, toConcatenate);
+//	}
+//
+//	default ObservableList<T> toObservableList() {
+//		return new ObservableListSnapshot<>(this);
+//	}
+//
+// }
