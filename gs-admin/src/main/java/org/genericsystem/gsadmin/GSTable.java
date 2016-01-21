2369cc71e67a176eb5d408f07e310d4c39ad06ba
juba MEHDI <jmehdi@middlewarefactory.com>
2016-01-21 14:09:12 +0100

    gstable

---
+++ gs-admin/src/main/java/org/genericsystem/gsadmin/GSTable.java
@@ -0,0 +1,120 @@
+package org.genericsystem.gsadmin;
+
+import java.util.function.Function;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.layout.HBox;
+
+import org.genericsystem.gsadmin.GSRow.GSTableCellRow;
+import org.genericsystem.gsadmin.GSRow.GSTextCellFirstRow;
+import org.genericsystem.gsadmin.GSRow.GSTextCellRow;
+import org.genericsystem.ui.Element;
+import org.genericsystem.ui.components.GSHBox;
+import org.genericsystem.ui.components.GSSCrollPane;
+import org.genericsystem.ui.components.GSVBox;
+import org.genericsystem.ui.table.Row;
+import org.genericsystem.ui.table.Table;
+import org.genericsystem.ui.table.Window;
+
+public abstract class GSTable extends GSVBox {
+
+	public GSTable(Element<?> parent) {
+		super(parent);
+	}
+
+	@Override
+	protected void initChildren() {
+		GSSCrollPane scrollPane = new GSSCrollPane(this).setStyleClass("scrollable");
+		{
+			GSVBox tablePanel = new GSVBox(scrollPane).setStyleClass(Table::getStyleClass).setSuperPrefWidth(getSuperPrefWidth()).setSuperPrefHeight(getSuperPrefHeight());
+			{
+				// new GSHBox(tablePanel).select(Table::getFirstElement).include(new TextCellFirstRowBuilder<>()::init).setStyleClass(Row::getStyleClass).setMinHeight(Table::getFirstRowHeight).setMaxHeight(Table::getFirstRowHeight)
+				// .setPrefHeight(Table::getFirstRowHeight);
+				// GSHBox hb = new GSHBox(tablePanel);
+				new GSTextCellFirstRow(tablePanel).select(Table::getFirstElement).setStyleClass(Row::getStyleClass).setMinHeight(Table::getFirstRowHeight).setMaxHeight(Table::getFirstRowHeight).setPrefHeight(Table::getFirstRowHeight);
+				createSelectionHBox(tablePanel).forEach(Table::getElements).setStyleClass(Row::getStyleClass).setMinHeight(Table::getRowHeight).setMaxHeight(Table::getRowHeight).setPrefHeight(Table::getRowHeight);
+			}
+		}
+	}
+
+	protected abstract GSHBox createSelectionHBox(Element<?> parent);
+
+	protected abstract <M> Function<M, ObservableValue<Number>> getSuperPrefWidth();
+
+	protected abstract <M> Function<M, ObservableValue<Number>> getSuperPrefHeight();
+
+	public static class GSTextCellTable extends GSTable {
+
+		public GSTextCellTable(Element<?> parent) {
+			super(parent);
+		}
+
+		@Override
+		protected <M> Function<M, ObservableValue<Number>> getSuperPrefWidth() {
+			return table -> ((Table) table).getColumnWidth();
+		}
+
+		@Override
+		protected <M> Function<M, ObservableValue<Number>> getSuperPrefHeight() {
+			return table -> ((Table) table).getRowHeight();
+		}
+
+		@Override
+		public GSHBox createSelectionHBox(Element<?> parent) {
+			return (GSHBox) new GSTextCellRow(parent).addBindMetaAction(HBox::onMouseClickedProperty, GenericWindow::selectRowGenericTable);
+			// return (GSHBox) new GSHBox(parent).include(new TextCellRowBuilder<>()::init).addBindMetaAction(HBox::onMouseClickedProperty, GenericWindow::selectRowGenericTable);
+		}
+
+	}
+
+	public static class GSTableCellTable extends GSTable {
+
+		public GSTableCellTable(Element<?> parent) {
+			super(parent);
+		}
+
+		@Override
+		protected <M> Function<M, ObservableValue<Number>> getSuperPrefWidth() {
+			return app -> new SimpleObjectProperty<>(900);
+		}
+
+		@Override
+		protected <M> Function<M, ObservableValue<Number>> getSuperPrefHeight() {
+			return app -> ((Window) app).getHeight();
+		}
+
+		@Override
+		public GSHBox createSelectionHBox(Element<?> parent) {
+			return (GSHBox) new GSTableCellRow(parent).addBindMetaAction(HBox::onMouseClickedProperty, GenericWindow::selectRowEngineTable);
+			// return (GSHBox) new GSHBox(parent).include(new TableCellRowBuilder<>()::init).addBindMetaAction(HBox::onMouseClickedProperty, GenericWindow::selectRowEngineTable);
+		}
+	}
+
+	public static class GSTableCellTable2 extends GSTableCellTable {
+
+		public GSTableCellTable2(Element<?> parent) {
+			super(parent);
+		}
+
+		@Override
+		public GSHBox createSelectionHBox(Element<?> parent) {
+			return (GSHBox) new GSTableCellRow(parent).addBindMetaAction(HBox::onMouseClickedProperty, GenericWindow::selectRowGenericTable);
+			// return (GSHBox) new GSHBox(parent).include(new TableCellRowBuilder<>()::init).addBindMetaAction(HBox::onMouseClickedProperty, GenericWindow::selectRowGenericTable);
+		}
+	}
+
+	public static class GSEditTableCellTable extends GSTableCellTable {
+
+		public GSEditTableCellTable(Element<?> parent) {
+			super(parent);
+		}
+
+		@Override
+		public GSHBox createSelectionHBox(Element<?> parent) {
+			return (GSHBox) new GSTableCellRow(parent).addBindMetaAction(HBox::onMouseClickedProperty, GenericCrud::test);
+			// return (GSHBox) new GSHBox(parent).include(new TableCellRowBuilder<>()::init).addBindMetaAction(HBox::onMouseClickedProperty, GenericWindow::selectRowGenericTable);
+		}
+	}
+
+}
