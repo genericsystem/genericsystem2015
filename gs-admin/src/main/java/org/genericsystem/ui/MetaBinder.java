7ae29757a16a9e6640ed798255af5e04e5c85b52
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2016-01-23 21:34:49 +0100

    Refactoring

--- gs-admin/src/main/java/org/genericsystem/ui/MetaBinder.java
+++ gs-admin/src/main/java/org/genericsystem/ui/MetaBinder.java
@@ -1,112 +1,54 @@
 package org.genericsystem.ui;
 
-import java.util.AbstractList;
-import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.beans.value.WeakChangeListener;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.collections.WeakListChangeListener;
+
+import org.genericsystem.ui.ModelContext.ModelContextList;
 
 public interface MetaBinder<N, W> {
 
-	default void init(Function<?, W> method, ModelContext modelContext, ViewContext<N> viewContext, Element<?> childElement) {
-		init(modelContext.applyOnModel(method), modelContext, viewContext, childElement);
+	default void init(Function<?, W> method, ViewContext<N> viewContext, Element<?> childElement) {
+		init(viewContext.getModelContext().applyOnModel(method), viewContext, childElement);
 	}
 
-	default void init(Supplier<W> applyOnModel, ModelContext modelContext, ViewContext<N> viewContext, Element<?> childElement) {
-		init(applyOnModel.get(), modelContext, viewContext, childElement);
+	default void init(Supplier<W> applyOnModel, ViewContext<N> viewContext, Element<?> childElement) {
+		init(applyOnModel.get(), viewContext, childElement);
 	}
 
-	default void init(W wrapper, ModelContext modelContext, ViewContext<N> viewContext, Element<?> childElement) {
+	default void init(W wrapper, ViewContext<N> viewContext, Element<?> childElement) {
 	}
 
 	public static <N, W> MetaBinder<N, ObservableList<W>> foreachBinder() {
 		return new MetaBinder<N, ObservableList<W>>() {
+			private ListChangeListener<W> listener;
 
 			@Override
-			public void init(ObservableList<W> wrapper, ModelContext modelContext, ViewContext<N> viewContext, Element<?> childElement) {
-
-				List<ModelContext> children = modelContext.getChildren(childElement);
-
-				class ForEachList extends AbstractList<W> implements ListChangeListener<W> {
-					{
-						addAll(wrapper);
-					}
-
-					@SuppressWarnings("unchecked")
-					@Override
-					public W get(int index) {
-						return (W) children.get(index).getModel();
-					}
-
-					@Override
-					public int size() {
-						return children.size();
-					}
-
-					@Override
-					public void add(int index, W model) {
-						ModelContext childContext = new ModelContext(modelContext, model);
-						new ViewContext<>(viewContext, childContext, childElement, null);
-						children.add(index, childContext);
-					}
-
-					@Override
-					public W set(int index, W element) {
-						W remove = remove(index);
-						add(index, element);
-						return remove;
-					}
-
-					@Override
-					public W remove(int index) {
-						return children.remove(index).destroy();
-					}
-
-					@Override
-					public void onChanged(Change<? extends W> change) {
-						while (change.next()) {
-							if (change.wasPermutated()) {
-								subList(change.getFrom(), change.getTo()).clear();
-								addAll(change.getFrom(), change.getList().subList(change.getFrom(), change.getTo()));
-							} else {
-								if (change.wasRemoved())
-									subList(change.getFrom(), change.getFrom() + change.getRemovedSize()).clear();
-								if (change.wasAdded())
-									addAll(change.getFrom(), change.getAddedSubList());
-							}
-						}
-					}
-				}
-				wrapper.addListener(new ForEachList());
+			public void init(ObservableList<W> wrapper, ViewContext<N> viewContext, Element<?> childElement) {
+				ModelContextList children = viewContext.getModelContext().getChildren(childElement);
+				wrapper.addListener(new WeakListChangeListener<>(listener = children.getListChangeListener(viewContext)));
+				int index = 0;
+				for (W model : wrapper)
+					children.insert(index++, model, viewContext);
 			}
 		};
 	}
 
 	public static <N, W> MetaBinder<N, ObservableValue<W>> selectorBinder() {
 		return new MetaBinder<N, ObservableValue<W>>() {
+			private ChangeListener<W> listener;
+
 			@Override
-			public void init(ObservableValue<W> wrapper, ModelContext modelContext, ViewContext<N> viewContext, Element<?> childElement) {
-				List<ModelContext> children = modelContext.getChildren(childElement);
-				Consumer<W> consumer = (newModel) -> {
-					if (newModel != null) {
-						ModelContext childContext = new ModelContext(modelContext, newModel);
-						new ViewContext<>(viewContext, childContext, childElement, null);
-						children.add(childContext);
-						assert children.size() == 1;
-					}
-				};
-				wrapper.addListener((o, oldModel, newModel) -> {
-					if (oldModel == newModel)
-						return;
-					if (oldModel != null)
-						children.remove(0).destroy();
-					consumer.accept(newModel);
-				});
-				consumer.accept(wrapper.getValue());
+			public void init(ObservableValue<W> wrapper, ViewContext<N> viewContext, Element<?> childElement) {
+				ModelContextList children = viewContext.getModelContext().getChildren(childElement);
+				wrapper.addListener(new WeakChangeListener<>(listener = viewContext.getModelContext().getChildren(childElement).getChangeListener(viewContext)));
+				listener.changed(wrapper, null, wrapper.getValue());
 			}
 		};
 	}
--- gs-admin/src/main/java/org/genericsystem/ui/components/GSApplication.java
+++ gs-admin/src/main/java/org/genericsystem/ui/components/GSApplication.java
@@ -3,13 +3,12 @@ package org.genericsystem.ui.components;
 import javafx.scene.Group;
 
 import org.genericsystem.ui.Element;
-import org.genericsystem.ui.ModelContext;
-import org.genericsystem.ui.ViewContext;
+import org.genericsystem.ui.ViewContext.RootViewContext;
 
 public class GSApplication extends Element<Group> {
 
 	public GSApplication(Object model, Group parentNode) {
 		super(Group.class);
-		new ViewContext<Group>(null, new ModelContext(null, model), this, parentNode).getNode();
+		new RootViewContext<>(model, this, parentNode);
 	}
 }
