11bb5629fb90cd010b56f14cd6d21e07c6877ee3
nfeybesse <nfeybesse@middlewarefactory.com>
2016-01-25 11:51:51 +0100

    Improve TodoTableList

--- gs-admin/src/main/java/org/genericsystem/todomvctable/TodoColumn.java
+++ gs-admin/src/main/java/org/genericsystem/todomvctable/TodoColumn.java
@@ -3,8 +3,13 @@ package org.genericsystem.todomvctable;
 import javafx.beans.property.Property;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
+import org.genericsystem.ui.Model;
 
-public class TodoColumn {
+public class TodoColumn extends Model {
+	public TodoColumn(Model parent) {
+		super(parent);
+	}
+
 	Property<String> title = new SimpleObjectProperty<String>();
 
 	public ObservableValue<String> getTitle() {
