574078f4cd478233edc082fe7c13a7950fe2e2af
nfeybesse <nfeybesse@middlewarefactory.com>
2016-01-29 10:45:54 +0100

    Merge

--- gs-admin/src/main/java/org/genericsystem/gsadmin/GenericWindow.java
+++ gs-admin/src/main/java/org/genericsystem/gsadmin/GenericWindow.java
@@ -8,7 +8,6 @@ import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-
 import org.genericsystem.common.Generic;
 import org.genericsystem.distributed.cacheonclient.CocClientEngine;
 import org.genericsystem.gsadmin.TableBuilder.TableCellTableBuilder;
@@ -18,66 +17,66 @@ import org.genericsystem.ui.table.Window;
 
 public class GenericWindow extends Window {
 
-	private final Property<GenericCrud> firstCrud = new SimpleObjectProperty<>();
-	private final Property<GenericCrud> secondCrud = new SimpleObjectProperty<>();
+	private final Property<GenericCrud> engineCrud = new SimpleObjectProperty<>();
+	private final Property<GenericCrud> genericCrud = new SimpleObjectProperty<>();
 
 	public GenericWindow(GenericCrud tableCrud, ObservableValue<? extends Number> width, ObservableValue<? extends Number> height) {
 		super(width, height);
-		this.firstCrud.setValue(tableCrud);
+		this.engineCrud.setValue(tableCrud);
 	}
 
 	public Property<GenericCrud> getFirstCrud() {
-		return firstCrud;
+		return engineCrud;
 	}
 
 	public Property<GenericCrud> getSecondCrud() {
-		return secondCrud;
+		return genericCrud;
 	}
 
 	public void flush() {
-		firstCrud.getValue().<Generic> getModel().getCurrentCache().flush();
+		engineCrud.getValue().<Generic> getModel().getCurrentCache().flush();
 	}
 
 	public void shiftTs() {
-		firstCrud.getValue().<Generic> getModel().getCurrentCache().shiftTs();
+		engineCrud.getValue().<Generic> getModel().getCurrentCache().shiftTs();
 	}
 
 	public void cancel() {
-		firstCrud.getValue().<Generic> getModel().getCurrentCache().clear();
+		engineCrud.getValue().<Generic> getModel().getCurrentCache().clear();
 	}
 
 	public void mount() {
-		firstCrud.getValue().<Generic> getModel().getCurrentCache().mount();
+		engineCrud.getValue().<Generic> getModel().getCurrentCache().mount();
 	}
 
 	public StringBinding getCacheLevel() {
-		return Bindings.createStringBinding(() -> "Cache level : " + firstCrud.getValue().<CocClientEngine> getModel().getCurrentCache().getCacheLevelObservable().getValue(), firstCrud.getValue().<CocClientEngine> getModel().getCurrentCache()
+		return Bindings.createStringBinding(() -> "Cache level : " + engineCrud.getValue().<CocClientEngine> getModel().getCurrentCache().getCacheLevelObservable().getValue(), engineCrud.getValue().<CocClientEngine> getModel().getCurrentCache()
 				.getCacheLevelObservable());
 	}
 
 	public void unmount() {
-		firstCrud.getValue().<CocClientEngine> getModel().getCurrentCache().unmount();
+		engineCrud.getValue().<CocClientEngine> getModel().getCurrentCache().unmount();
 	}
 
 	public static GenericWindow createWindow(ObservableValue<? extends Number> width, ObservableValue<? extends Number> height, CocClientEngine engine) {
-		TableCellTableBuilder<Generic, Generic> tableModel = new TableCellTableBuilder<>(engine.getObservableSubInstances(), engine.getObservableAttributes().filtered(attribute -> attribute.isCompositeForInstances(engine)),
-				itemTableCell -> columnTableCell -> {
-					TextTableBuilder<Generic, Generic> textTableModel = new TextTableBuilder<>(itemTableCell.getObservableHolders(columnTableCell), FXCollections.observableArrayList(itemTableCell.getComponents()), null, null,
-							firstColumString -> new ReadOnlyStringWrapper("" + firstColumString), null);
-					Table tab = textTableModel.buildTable();
-					return new ReadOnlyObjectWrapper<>(tab);
-				}, firstRowString -> new ReadOnlyStringWrapper("" + firstRowString), itemTableCell -> {
-					TextTableBuilder<Generic, Generic> textTableModel = new TextTableBuilder<>(FXCollections.observableArrayList(itemTableCell), FXCollections.observableArrayList(itemTableCell.getComponents()), item -> col -> new ReadOnlyStringWrapper(""
-							+ col), null, firstColumString -> new ReadOnlyStringWrapper("" + firstColumString), null);
-					Table tab = textTableModel.buildTableFirstColumn();
-					return new ReadOnlyObjectWrapper<>(tab);
-				}, column -> new ReadOnlyStringWrapper("Delete"));
+		TableCellTableBuilder<Generic, Generic> tableModel = new TableCellTableBuilder<>(new ReadOnlyStringWrapper("Structurals"), new ReadOnlyStringWrapper("Action"), engine.getObservableSubInstances(), engine.getObservableAttributes().filtered(
+				attribute -> attribute.isCompositeForInstances(engine)), itemTableCell -> columnTableCell -> {
+			TextTableBuilder<Generic, Generic> textTableModel = new TextTableBuilder<>(new ReadOnlyStringWrapper("Table"), new ReadOnlyStringWrapper("Action"), itemTableCell.getObservableHolders(columnTableCell),
+							FXCollections.observableArrayList(itemTableCell.getComponents()), null, null, firstColumString -> new ReadOnlyStringWrapper("" + firstColumString), null);
+			Table tab = textTableModel.buildTable();
+			return new ReadOnlyObjectWrapper<>(tab);
+		}, firstRowString -> new ReadOnlyStringWrapper("" + firstRowString), itemTableCell -> {
+			TextTableBuilder<Generic, Generic> textTableModel = new TextTableBuilder<>(new ReadOnlyStringWrapper("Table"), new ReadOnlyStringWrapper("Action"), FXCollections.observableArrayList(itemTableCell),
+							FXCollections.observableArrayList(itemTableCell.getComponents()), item -> col -> new ReadOnlyStringWrapper("" + col), null, firstColumString -> new ReadOnlyStringWrapper("" + firstColumString), null);
+			Table tab = textTableModel.buildTableFirstColumn();
+			return new ReadOnlyObjectWrapper<>(tab);
+		}, column -> new ReadOnlyStringWrapper("Delete"));
 
 		Table table = tableModel.buildTable();
 		table.getFirstRowHeight().setValue(30);
-		table.getFirstColumnWidth().setValue(300);
+		table.getFirstColumnWidth().setValue(100);
 		table.getRowHeight().setValue(50);
-		table.getColumnWidth().setValue(300);
+		table.getColumnWidth().setValue(100);
 		GenericCrud crud = new GenericCrud(new SimpleObjectProperty<>(table), engine);
 		return new GenericWindow(crud, width, height);
 	}
