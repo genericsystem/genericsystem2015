d630bd5dee857d7caffc4e0afc2ace1ce341c81c
nfeybesse <nfeybesse@middlewarefactory.com>
2016-02-01 17:08:11 +0100

    Adjust scrollbars

--- gs-kernel/src/main/java/org/genericsystem/distributed/cacheonclient/AsyncIDifferential.java
+++ gs-kernel/src/main/java/org/genericsystem/distributed/cacheonclient/AsyncIDifferential.java
@@ -1,7 +1,8 @@
 package org.genericsystem.distributed.cacheonclient;
 
+import java.util.concurrent.CompletableFuture;
 import javafx.beans.Observable;
-
+import org.genericsystem.api.core.Snapshot;
 import org.genericsystem.common.Generic;
 import org.genericsystem.common.IDifferential;
 
@@ -11,4 +12,6 @@ import org.genericsystem.common.IDifferential;
  */
 public interface AsyncIDifferential extends IDifferential<Generic> {
 	public Observable getInvalidator(Generic generic);
+
+	CompletableFuture<Snapshot<Generic>> getDependenciesPromise(Generic generic);
 }
