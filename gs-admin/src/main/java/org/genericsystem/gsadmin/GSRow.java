19628c6c57040914170621caac44d710a57b48eb
juba MEHDI <jmehdi@middlewarefactory.com>
2016-02-02 12:07:55 +0100

    create GSEditableCellRow

--- gs-admin/src/main/java/org/genericsystem/gsadmin/GSRow.java
+++ gs-admin/src/main/java/org/genericsystem/gsadmin/GSRow.java
@@ -9,6 +9,7 @@ import org.genericsystem.ui.Element;
 import org.genericsystem.ui.components.GSButton;
 import org.genericsystem.ui.components.GSHBox;
 import org.genericsystem.ui.components.GSLabel;
+import org.genericsystem.ui.components.GSTextField;
 import org.genericsystem.ui.table.Cell;
 import org.genericsystem.ui.table.Row;
 import org.genericsystem.ui.table.Table;
@@ -25,7 +26,7 @@ public abstract class GSRow extends GSHBox {
 		GSHBox firstCell = new GSHBox(this).setStyleClass(Cell<Generic>::getStyleClass).select(Row::getFirstElement).setMinWidth(Cell<Generic>::getFirstColumnCellsWidth).setPrefWidth(Cell<Generic>::getFirstColumnCellsWidth)
 				.setMaxWidth(Cell<Generic>::getFirstColumnCellsWidth).setStyleClass(Cell<Generic>::getStyleClass);
 		{
-			createGSCell(firstCell);
+			createFirstCell(firstCell);
 		}
 
 		GSHBox cells = new GSHBox(this).forEach(GenericRow::getElements).setMinWidth(Cell<Generic>::getColumnCellsWidth).setMaxWidth(Cell<Generic>::getColumnCellsWidth).setPrefWidth(Cell<Generic>::getColumnCellsWidth)
@@ -44,8 +45,29 @@ public abstract class GSRow extends GSHBox {
 
 	protected abstract Element<?> createGSCell(Element<?> parent);
 
+	protected Element<?> createFirstCell(Element<?> parent) {
+		return new GSLabel(parent, Cell<String>::getObservableModel);
+	}
+
 	protected Element<?> createLastCell(Element<?> parent) {
 		return new GSButton(parent, Cell<String>::getObservableModel).setAction(GenericRow::delete).addBoot(Button::paddingProperty, new Insets(2, 2, 2, 2));
+	}
+
+	public static class GSEditableCellRow extends GSRow {
+
+		public GSEditableCellRow(Element<?> parent) {
+			super(parent);
+		}
+
+		@Override
+		protected Element<?> createGSCell(Element<?> parent) {
+			return new GSTextField(parent, Cell<Generic>::getName);
+		}
+
+		@Override
+		protected Element<?> createFirstCell(Element<?> parent) {
+			return new GSTextCellTable(parent).select(Cell<Table>::getObservableModel);
+		}
 
 	}
 
@@ -80,6 +102,11 @@ public abstract class GSRow extends GSHBox {
 		@Override
 		protected Element<?> createGSCell(Element<?> parent) {
 			return new GSTextCellTable(parent).select(Cell<Table>::getObservableModel);
+		}
+
+		@Override
+		protected Element<?> createFirstCell(Element<?> parent) {
+			return createGSCell(parent);
 		}
 	}
 }
