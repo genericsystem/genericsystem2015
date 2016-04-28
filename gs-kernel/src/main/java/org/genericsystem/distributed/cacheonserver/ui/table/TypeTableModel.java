adaac789c134909899827e0bae430faa9e5e2074
nfeybesse <nfeybesse@middlewarefactory.com>
2016-04-28 17:55:29 +0200

    Unstable

--- gs-kernel/src/main/java/org/genericsystem/distributed/cacheonserver/ui/table/TypeTableModel.java
+++ gs-kernel/src/main/java/org/genericsystem/distributed/cacheonserver/ui/table/TypeTableModel.java
@@ -5,10 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 import javafx.collections.ObservableList;
 import org.genericsystem.common.Generic;
-import org.genericsystem.distributed.ui.models.CompositeModel;
-import org.genericsystem.distributed.ui.models.CompositeModel.Conf;
 import org.genericsystem.distributed.ui.models.GenericCompositeModel;
-import org.genericsystem.distributed.ui.models.GenericCompositeModel.CompositeConf;
 import org.genericsystem.distributed.ui.models.GenericModel;
 
 /**
@@ -16,7 +13,7 @@ import org.genericsystem.distributed.ui.models.GenericModel;
  *
  */
 
-public class TypeTableModel extends CompositeModel<InstanceRowModel> {
+public class TypeTableModel extends GenericCompositeModel<InstanceRowModel> {
 
 	public TypeTableModel(Generic generic, List<Function<Generic[], ObservableList<Generic>>> observableListExtractors) {
 		this(generic, observableListExtractors, InstanceRowModel::new, GenericCompositeModel<GenericModel>::new, GenericModel::new);
@@ -45,18 +42,6 @@ public class TypeTableModel extends CompositeModel<InstanceRowModel> {
 		Function<Generic, GenericCompositeModel<?>> rowBuild = instance -> builders.get(1).apply(new CompositeConf<>(instance, stringExtractors.get(1), observableListExtractors.get(1), cellBuild));
 		Function<Generic, GenericCompositeModel<?>> tableBuild = type -> builders.get(0).apply(new CompositeConf<>(type, stringExtractors.get(0), observableListExtractors.get(0), rowBuild));
 		return tableBuild;
-	}
-
-	public TypeTableModel(Generic generic, Function<Generic, String> stringExtractor, Function<Generic, ObservableList<Generic>> observableListExtractor, Function<Conf, InstanceRowModel> rowBuilder,
-			Function<Conf, CompositeModel<GenericModel>> cellBuilder, Function<Generic, GenericModel> subCellBuilder) {
-		super(new Conf(generic, stringExtractor, typ -> typ.getObservableSubInstances(), instance -> rowBuilder.apply(new Conf(instance, observableListExtractor, attribute -> cellBuilder.apply(new Conf(instance, inst -> inst
-				.getObservableHolders(attribute), subCellBuilder))))));
-	}
-
-	public static Conf build(Generic generic, Function<Generic, String> stringExtractor, Function<Generic, ObservableList<Generic>> observableListExtractor, Function<Conf, InstanceRowModel> rowBuilder,
-			Function<Conf, CompositeModel<GenericModel>> cellBuilder, Function<Generic, GenericModel> subCellBuilder) {
-		return (new Conf(generic, stringExtractor, typ -> typ.getObservableSubInstances(), instance -> rowBuilder.apply(new Conf(instance, observableListExtractor, attribute -> cellBuilder.apply(new Conf(instance, inst -> inst
-				.getObservableHolders(attribute), subCellBuilder))))));
 	}
 
 }
