b82d6f8a0f568535aac98ddfcd7ca7b1314c79d6
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2016-05-09 08:22:52 +0200

    fix comilation errors

--- gs-kernel/src/main/java/org/genericsystem/distributed/cacheonserver/ui/table/title/TitleTypeTableModel.java
+++ gs-kernel/src/main/java/org/genericsystem/distributed/cacheonserver/ui/table/title/TitleTypeTableModel.java
@@ -1,26 +1,26 @@
-package org.genericsystem.distributed.cacheonserver.ui.table.title;
-
-import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.beans.value.ObservableValue;
-
-import org.genericsystem.common.Generic;
-import org.genericsystem.distributed.cacheonserver.ui.table.TypeTableModel;
-
-/**
- * @author Nicolas Feybesse
- *
- */
-public class TitleTypeTableModel extends TypeTableModel {
-
-	private final ObservableValue<TitleRowModel> titleRowModel;
-
-	public TitleTypeTableModel(Generic[] generics, StringExtractor stringExtractor) {
-		super(generics, stringExtractor);
-		titleRowModel = new ReadOnlyObjectWrapper<>(buildTableModel(generics[0]));
-	}
-
-	public ObservableValue<TitleRowModel> getTitleRowModel() {
-		return titleRowModel;
-	}
-
-}
+//package org.genericsystem.distributed.cacheonserver.ui.table.title;
+//
+//import javafx.beans.property.ReadOnlyObjectWrapper;
+//import javafx.beans.value.ObservableValue;
+//
+//import org.genericsystem.common.Generic;
+//import org.genericsystem.distributed.cacheonserver.ui.table.TypeTableModel;
+//
+///**
+// * @author Nicolas Feybesse
+// *
+// */
+//public class TitleTypeTableModel extends TypeTableModel {
+//
+//	private final ObservableValue<TitleRowModel> titleRowModel;
+//
+//	public TitleTypeTableModel(Generic[] generics, StringExtractor stringExtractor) {
+//		super(generics, stringExtractor);
+//		titleRowModel = new ReadOnlyObjectWrapper<>(buildTableModel(generics[0]));
+//	}
+//
+//	public ObservableValue<TitleRowModel> getTitleRowModel() {
+//		return titleRowModel;
+//	}
+//
+// }
