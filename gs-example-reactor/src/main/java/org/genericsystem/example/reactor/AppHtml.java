1b54275ce4330b84e4757939eea4879fc191717b
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2016-06-01 10:31:31 +0200

    Merge

--- gs-example-reactor/src/main/java/org/genericsystem/example/reactor/AppHtml.java
+++ gs-example-reactor/src/main/java/org/genericsystem/example/reactor/AppHtml.java
@@ -1,7 +1,5 @@
 package org.genericsystem.example.reactor;
 
-import io.vertx.core.http.ServerWebSocket;
-
 import org.genericsystem.carcolor.model.Car;
 import org.genericsystem.carcolor.model.CarColor;
 import org.genericsystem.carcolor.model.Color;
@@ -20,12 +18,14 @@ import org.genericsystem.reactor.composite.table.TypeTableHtmlTemplate.TypeTable
 import org.genericsystem.reactor.html.HtmlApp;
 import org.genericsystem.reactor.html.HtmlDiv;
 
+import io.vertx.core.http.ServerWebSocket;
+
 public class AppHtml extends HtmlApp<AppModel> {
 
 	public static void main(String[] args) {
 		ApplicationsDeploymentConfig appsConfig = new ApplicationsDeploymentConfig();
-		appsConfig.addApplication("/apphtml", AppHtml.class, AppModel.class, Engine.class, System.getenv("HOME") + "/genericsystem/cars/", Car.class,
-				Power.class, Color.class, CarColor.class);
+		appsConfig.addApplication("/", AppHtml.class, AppModel.class, Engine.class, System.getenv("HOME") + "/genericsystem/cars/", Car.class, Power.class,
+				Color.class, CarColor.class);
 		new ApplicationServer(appsConfig).start();
 	}
 
