beb443c2fa9097c5ec5992b3488336382e20a20f
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2016-07-07 12:01:27 +0200

    Clean carcolor model

--- gs-carcolor-model/src/main/java/org/genericsystem/carcolor/model/Color.java
+++ gs-carcolor-model/src/main/java/org/genericsystem/carcolor/model/Color.java
@@ -1,35 +1,10 @@
 package org.genericsystem.carcolor.model;
 
-import org.genericsystem.api.core.annotations.Meta;
 import org.genericsystem.api.core.annotations.SystemGeneric;
-import org.genericsystem.api.core.annotations.value.StringValue;
 import org.genericsystem.reactor.annotation.InstanceColorize;
 
 @SystemGeneric
 @InstanceColorize
 public class Color {
 
-	@SystemGeneric
-	@Meta(Color.class)
-	@StringValue("White")
-	public static class White {
-	}
-
-	@SystemGeneric
-	@Meta(Color.class)
-	@StringValue("Red")
-	public static class Red {
-	}
-
-	@SystemGeneric
-	@Meta(Color.class)
-	@StringValue("Blue")
-	public static class Blue {
-	}
-
-	@SystemGeneric
-	@Meta(Color.class)
-	@StringValue("Yellow")
-	public static class Yellow {
-	}
 }
