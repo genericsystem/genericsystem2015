feeeb57c51409b081b00e48f7bf100e774499988
Arnaud Lievre <alievre@middlewarefactory.com>
2016-07-07 18:19:44 +0200

    Prepare version 4.0.final

---
+++ gs-demo/src/main/java/org/genericsystem/demo/App.java
@@ -0,0 +1,13 @@
+package org.genericsystem.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
