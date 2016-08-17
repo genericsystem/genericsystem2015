de8b52c18bec206757530e9d1dc0bbcc8b04f61e
Fabienne Ducroquet <fducroquet@middlewarefactory.com>
2016-08-17 16:42:32 +0200

    gs-reactor: Refactor: Creation of GSAttributeOfInstanceEditor class
    To modify, add and remove links or holders corresponding to a single relation or
    attribute of an instance.

--- gs-reactor/src/main/java/org/genericsystem/reactor/gs/GSEditor.java
+++ gs-reactor/src/main/java/org/genericsystem/reactor/gs/GSEditor.java
@@ -1,18 +1,10 @@
 package org.genericsystem.reactor.gs;
 
-import java.util.stream.Collectors;
-
-import org.genericsystem.api.core.ApiStatics;
-import org.genericsystem.reactor.gs.GSSubcellDisplayer.GSSubcellAdder;
 import org.genericsystem.reactor.gs.GSSubcellDisplayer.GSSubcellEditor;
-import org.genericsystem.reactor.gs.GSSubcellDisplayer.GSSubcellEditorWithRemoval;
 import org.genericsystem.reactor.gs.GSSubcellDisplayer.InstanceLinkTitleDisplayer;
 import org.genericsystem.reactor.gstag.GSH1;
-import org.genericsystem.reactor.model.GenericModel;
 import org.genericsystem.reactor.model.ObservableListExtractor;
 import org.genericsystem.reactor.model.StringExtractor;
-
-import javafx.collections.FXCollections;
 
 /**
  * @author Nicolas Feybesse
@@ -90,36 +82,9 @@ public class GSEditor extends GSComposite {
 								new GSSubcellEditor(this);
 							}
 						};
-						new GSSection(this, FlexDirection.COLUMN) {
+						new GSAttributeOfInstanceEditor(this) {
 							{
 								forEach_(ObservableListExtractor.ATTRIBUTES_OF_INSTANCES);
-								addStyle("flex", "1");
-								addStyle("overflow", "hidden");
-								new GSSubcellEditor(this) {
-									{
-										addStyle("flex", "1");
-										// forEach_ should work here, but it causes errors…
-										select((model, holders) -> model.getGeneric().isRequiredConstraintEnabled(ApiStatics.BASE_POSITION) && holders.size() == 1
-												? FXCollections.observableArrayList(holders.stream().map(holder -> new GenericModel(model, GenericModel.addToGenerics(holder, model.getGenerics()))).collect(Collectors.toList()))
-												: FXCollections.emptyObservableList());
-									}
-								};
-								new GSSubcellEditorWithRemoval(this) {
-									{
-										addStyle("flex", "1");
-										select((model, holders) -> (!model.getGeneric().isRequiredConstraintEnabled(ApiStatics.BASE_POSITION) && holders.size() == 1) || holders.size() > 1
-												? FXCollections.observableArrayList(holders.stream().map(holder -> new GenericModel(model, GenericModel.addToGenerics(holder, model.getGenerics()))).collect(Collectors.toList()))
-												: FXCollections.emptyObservableList());
-									}
-								};
-								new GSSubcellAdder(this) {
-									{
-										select((model,
-												holders) -> holders.isEmpty() || (model.getGeneric().getComponents().size() < 2 && !model.getGeneric().isPropertyConstraintEnabled())
-														|| (model.getGeneric().getComponents().size() >= 2 && !model.getGeneric().isSingularConstraintEnabled(ApiStatics.BASE_POSITION)) ? FXCollections.singletonObservableList(model)
-																: FXCollections.emptyObservableList());
-									}
-								};
 							}
 						};
 					}
