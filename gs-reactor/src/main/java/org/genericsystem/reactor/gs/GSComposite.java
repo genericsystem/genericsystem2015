7cac0395db89d94f6b52f5fa35a20694825ec1a0
Fabienne Ducroquet <fducroquet@middlewarefactory.com>
2016-08-31 15:19:49 +0200

    gs-reactor: ColorCompositeRadio: Put button inside label

--- gs-reactor/src/main/java/org/genericsystem/reactor/gs/GSComposite.java
+++ gs-reactor/src/main/java/org/genericsystem/reactor/gs/GSComposite.java
@@ -112,6 +112,7 @@ public class GSComposite extends GSSection {
 		protected void sections() {
 			new GSSection(this, CompositeRadio.this.getReverseDirection()) {
 				{
+					addStyle("flex", "1");
 					forEach(CompositeRadio.this);
 					new HtmlRadio(this);
 					new HtmlLabel(this) {
@@ -152,14 +153,20 @@ public class GSComposite extends GSSection {
 
 		@Override
 		protected void sections() {
-			flexSubElement = new GSSection(this, ColorCompositeRadio.this.getReverseDirection()) {
+			flexSubElement = new HtmlLabel(this) {
 				{
+					addStyle("flex", "1");
+					addStyle("justify-content", "center");
+					addStyle("align-items", "center");
+					addStyle("text-align", "center");
 					forEach(ColorCompositeRadio.this);
+					bindText();
 					bindStyle("background-color", ReactorStatics.BACKGROUND, model -> getGenericStringProperty(model));
-					new HtmlRadio(this);
-					new HtmlLabel(this) {
+					new HtmlRadio(this) {
 						{
-							bindText();
+							addStyle("float", "left");
+							addStyle("vertical-align", "middle");
+							addStyle("margin", "4px");
 						}
 					};
 				}
