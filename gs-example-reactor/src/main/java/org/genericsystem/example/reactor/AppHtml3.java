fc70350e413d40d6ce9356906a3970a7730d09f5
Fabienne Ducroquet <fducroquet@middlewarefactory.com>
2016-09-09 16:44:03 +0200

    gs-example-reactor: AppHtml3: Avoid repetition of classes’ lists

--- gs-example-reactor/src/main/java/org/genericsystem/example/reactor/AppHtml3.java
+++ gs-example-reactor/src/main/java/org/genericsystem/example/reactor/AppHtml3.java
@@ -1,5 +1,7 @@
 package org.genericsystem.example.reactor;
 
+import java.util.stream.Stream;
+
 import org.genericsystem.carcolor.model.Car;
 import org.genericsystem.carcolor.model.CarColor;
 import org.genericsystem.carcolor.model.CarColor2;
@@ -7,6 +9,7 @@ import org.genericsystem.carcolor.model.Color;
 import org.genericsystem.carcolor.model.Power;
 import org.genericsystem.carcolor.model.UsedCar;
 import org.genericsystem.example.reactor.AppHtml.ExampleReactorScript;
+import org.genericsystem.reactor.TagImpl;
 import org.genericsystem.reactor.annotations.DependsOnModel;
 import org.genericsystem.reactor.annotations.RunScript;
 import org.genericsystem.reactor.appserver.ApplicationServer;
@@ -58,19 +61,18 @@ public class AppHtml3 extends GSApp implements SelectionDefaults {
 		addStyle("justify-content", "center");
 		createSelectionProperty();
 
-		new TreeRootTagImpl(this, TitleContent.class, TypeNameDisplayer.class, AttributeNameDisplayer.class, ComponentNameDisplayer.class, InstanceNameBuilderInput.class, HolderBuilderInput.class, BooleanHolderBuilderInput.class,
-				ComponentBuilderSelect.class, AddButton.class, RowNameDisplayer.class, ComponentLabel.class, EmptyCell.class, BooleanDisplayer.class, ValueDisplayer.class, RemoveButton.class).select(Car.class);
-		new TreeRootTagImpl(this, TitleContent.class, TypeNameDisplayer.class, AttributeNameDisplayer.class, ComponentNameDisplayer.class, InstanceNameBuilderInput.class, HolderBuilderInput.class, BooleanHolderBuilderInput.class,
-				ComponentBuilderSelect.class, AddButton.class, RowNameDisplayer.class, ComponentLabel.class, EmptyCell.class, BooleanDisplayer.class, ValueDisplayer.class, RemoveButton.class, HorizontalTable.class).select(Car.class);
-
-		new TreeRootTagImpl(this, EditorTitleContent.class, HorizontalGSEditor.class, TypeNameDisplayer.class, AttributeNameDisplayer.class, ComponentNameDisplayer.class, InstanceComponentName.class, InstanceType.class, InstanceTypeAttribute.class,
+		Class<? extends TagImpl>[] tableClasses = new Class[] { TitleContent.class, TypeNameDisplayer.class, AttributeNameDisplayer.class, ComponentNameDisplayer.class, InstanceNameBuilderInput.class, HolderBuilderInput.class,
+				BooleanHolderBuilderInput.class, ComponentBuilderSelect.class, AddButton.class, RowNameDisplayer.class, ComponentLabel.class, EmptyCell.class, BooleanDisplayer.class, ValueDisplayer.class, RemoveButton.class };
+		Class<? extends TagImpl>[] editorClasses = new Class[] { EditorTitleContent.class, TypeNameDisplayer.class, AttributeNameDisplayer.class, ComponentNameDisplayer.class, InstanceComponentName.class, InstanceType.class, InstanceTypeAttribute.class,
 				InstanceNameEditor.class, Checkbox.class, ReversedRelationDisplayer.class, DirectRelationComponentEditor.class, BooleanHolderEditorInput.class, HolderEditorInput.class, RemovalLink.class, BooleanHolderAdderInput.class,
-				BooleanHolderAdditionLink.class, HolderAdderInput.class, HolderAdditionLink.class, ComponentAdderSelect.class).select__(context -> getSelectionProperty(context));
-		new TreeRootTagImpl(this, EditorTitleContent.class, TypeNameDisplayer.class, AttributeNameDisplayer.class, ComponentNameDisplayer.class, InstanceType.class, InstanceComponentName.class, InstanceTypeAttribute.class, InstanceNameEditor.class,
-				Checkbox.class, ReversedRelationDisplayer.class, DirectRelationComponentEditor.class, BooleanHolderEditorInput.class, HolderEditorInput.class, RemovalLink.class, BooleanHolderAdderInput.class, BooleanHolderAdditionLink.class,
-				HolderAdderInput.class, HolderAdditionLink.class, ComponentAdderSelect.class).select__(context -> getSelectionProperty(context));
+				BooleanHolderAdditionLink.class, HolderAdderInput.class, HolderAdditionLink.class, ComponentAdderSelect.class };
 
-		new TreeRootTagImpl(this, TitleContent.class, TypeNameDisplayer.class, AttributeNameDisplayer.class, ComponentNameDisplayer.class, InstanceNameBuilderInput.class, HolderBuilderInput.class, BooleanHolderBuilderInput.class,
-				ComponentBuilderSelect.class, AddButton.class, RowNameDisplayer.class, ComponentLabel.class, EmptyCell.class, BooleanDisplayer.class, ValueDisplayer.class, RemoveButton.class).select(Color.class);
+		new TreeRootTagImpl(this, tableClasses).select(Car.class);
+		new TreeRootTagImpl(this, Stream.concat(Stream.of(tableClasses), Stream.of(HorizontalTable.class)).toArray(Class[]::new)).select(Car.class);
+
+		new TreeRootTagImpl(this, Stream.concat(Stream.of(editorClasses), Stream.of(HorizontalGSEditor.class)).toArray(Class[]::new)).select__(context -> getSelectionProperty(context));
+		new TreeRootTagImpl(this, editorClasses).select__(context -> getSelectionProperty(context));
+
+		new TreeRootTagImpl(this, tableClasses).select(Color.class);
 	}
 }
