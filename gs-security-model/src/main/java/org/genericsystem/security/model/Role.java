20f428c666a790555e9922ed438047c5b5c2e391
Fabienne Ducroquet <fducroquet@middlewarefactory.com>
2016-10-18 11:08:57 +0200

    Add gs-security-model and gs-security projects

---
+++ gs-security-model/src/main/java/org/genericsystem/security/model/Role.java
@@ -0,0 +1,8 @@
+package org.genericsystem.security.model;
+
+import org.genericsystem.api.core.annotations.SystemGeneric;
+
+@SystemGeneric
+public class Role {
+
+}
---
+++ gs-security/src/main/java/org/genericsystem/security/SecurityApp.java
@@ -0,0 +1,54 @@
+package org.genericsystem.security;
+
+import org.genericsystem.reactor.htmltag.HtmlH1;
+
+import org.genericsystem.reactor.gscomponents.GSApp;
+
+import org.genericsystem.reactor.gscomponents3.AppHeader;
+import org.genericsystem.reactor.gscomponents3.AppHeader.TitleDiv;
+import org.genericsystem.reactor.gscomponents3.DivWithTitle.TitledInstancesTable;
+import org.genericsystem.reactor.gscomponents3.Modal.ModalEditor;
+import org.genericsystem.reactor.gscomponents3.Monitor;
+import org.genericsystem.reactor.gscomponents3.Responsive;
+
+import org.genericsystem.common.Root;
+import org.genericsystem.reactor.annotations.Children;
+import org.genericsystem.reactor.annotations.DependsOnModel;
+import org.genericsystem.reactor.annotations.DirectSelect;
+import org.genericsystem.reactor.annotations.RunScript;
+import org.genericsystem.reactor.annotations.SetText;
+import org.genericsystem.reactor.annotations.Style;
+import org.genericsystem.reactor.appserver.ApplicationServer;
+import org.genericsystem.reactor.appserver.Script;
+import org.genericsystem.security.SecurityApp.ExampleScript;
+import org.genericsystem.security.model.Role;
+import org.genericsystem.security.model.User;
+
+/**
+ * @author Nicolas Feybesse
+ *
+ */
+
+@RunScript(ExampleScript.class)
+@DependsOnModel({ User.class })
+@Style(name = "background-color", value = "Red")
+//@Style(path = Responsive.class, name = "width", value = "80%")
+//@Style(name = "align-items", value = "center")
+@Children({ ModalEditor.class, AppHeader.class, Responsive.class, Monitor.class })
+@Children(path = Responsive.class, value = { TitledInstancesTable.class, TitledInstancesTable.class })
+@SetText(path = { AppHeader.class, TitleDiv.class, HtmlH1.class }, value = "gs-security")
+@DirectSelect(path = { Responsive.class, TitledInstancesTable.class }, value = { User.class, Role.class })
+public class SecurityApp extends GSApp {
+
+	public static void main(String[] mainArgs) {
+		ApplicationServer.startSimpleGenericApp(mainArgs, SecurityApp.class, "/example");
+	}
+
+	public static class ExampleScript implements Script {
+
+		public void run(Root engine) {
+			// Generic role = engine.find(Role.class);
+			engine.getCurrentCache().flush();
+		}
+	}
+}
