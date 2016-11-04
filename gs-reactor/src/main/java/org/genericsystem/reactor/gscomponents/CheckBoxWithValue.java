cd920021a5c4cc116e23cff4e51d2d8338c57741
Paul Lagarrigue <plagarrigue@middlewarefactory.com>
2016-11-04 15:30:17 +0100

    gs-carcolor : InstanceEditor improvement

--- gs-reactor/src/main/java/org/genericsystem/reactor/gscomponents/CheckBoxWithValue.java
+++ gs-reactor/src/main/java/org/genericsystem/reactor/gscomponents/CheckBoxWithValue.java
@@ -1,9 +1,13 @@
 package org.genericsystem.reactor.gscomponents;
 
 import org.genericsystem.reactor.modelproperties.ConvertedValueDefaults;
+import org.genericsystem.reactor.modelproperties.SelectionDefaults;
 
+import org.genericsystem.common.Generic;
 import org.genericsystem.reactor.ReactorStatics;
 import org.genericsystem.reactor.gscomponents.HtmlTag.HtmlCheckBox;
+
+import javafx.beans.property.Property;
 
 public class CheckBoxWithValue extends HtmlCheckBox implements ConvertedValueDefaults {
 
@@ -12,11 +16,16 @@ public class CheckBoxWithValue extends HtmlCheckBox implements ConvertedValueDef
 		bindOptionalBiDirectionalAttribute(VALUE, ReactorStatics.CHECKED, ReactorStatics.CHECKED);
 	}
 
-	public static class CheckBoxEditor extends CheckBoxWithValue {
+	public static class CheckBoxEditor extends CheckBoxWithValue implements SelectionDefaults {
 
 		public CheckBoxEditor() {
 			initValueProperty(model -> (Boolean) model.getGeneric().getValue());
-			addConvertedValueChangeListener((model, nva) -> model.getGeneric().updateValue(nva));
+			addConvertedValueChangeListener((model, nva) -> {
+				Generic updatedGeneric = model.getGeneric().updateValue(nva);
+				Property<Generic> genericProperty = getUpdatedGenericProperty(model);
+				if (genericProperty != null)
+					genericProperty.setValue(updatedGeneric);
+			});
 		}
 	}
 
