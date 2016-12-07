3a45197be841c7975b68b3c726506d902708db8f
Fabienne Ducroquet <fducroquet@middlewarefactory.com>
2016-12-07 18:02:03 +0100

    gs-example-reactor: Rename variable from diesel to usedCar

--- gs-example-reactor/src/main/java/org/genericsystem/reactor/example/App.java
+++ gs-example-reactor/src/main/java/org/genericsystem/reactor/example/App.java
@@ -55,7 +55,7 @@ public class App extends RootTagImpl {
 		public void run(Root engine) {
 			Generic car = engine.find(Car.class);
 			Generic power = engine.find(Power.class);
-			Generic diesel = engine.find(UsedCar.class);
+			Generic used = engine.find(UsedCar.class);
 			car.setAttribute("Description");
 			Generic person = engine.setInstance("Person");
 			Generic category = engine.setInstance("Category");
@@ -77,23 +77,23 @@ public class App extends RootTagImpl {
 			Generic owner = category.setInstance("Owner");
 			Generic audiS4 = car.setInstance("Audi S4");
 			audiS4.setHolder(power, 333);
-			audiS4.setHolder(diesel, false);
+			audiS4.setHolder(used, false);
 			audiS4.setLink(carColor, "Audi S4 Green", green);
 			audiS4.setLink(carPerson, "Audi S4 owner", owner, jsnow);
 			audiS4.setLink(carPerson, "Audi S4 driver", driver, hoover);
 			Generic bmwM3 = car.setInstance("BMW M3");
 			bmwM3.setHolder(power, 450);
-			bmwM3.setHolder(diesel, false);
+			bmwM3.setHolder(used, false);
 			bmwM3.setLink(carColor, "BMW M3 Red", red);
 			bmwM3.setLink(carPerson, "BMW M3 owner", owner, jdoe);
 			bmwM3.setLink(carPerson, "BMW M3 owner", driver, jdoe);
 			Generic ferrariF40 = car.setInstance("Ferrari F40");
 			ferrariF40.setHolder(power, 478);
-			ferrariF40.setHolder(diesel, false);
+			ferrariF40.setHolder(used, false);
 			ferrariF40.setLink(carColor, "Ferrari F40 red", red);
 			Generic miniCooper = car.setInstance("Mini Cooper");
 			miniCooper.setHolder(power, 175);
-			miniCooper.setHolder(diesel, true);
+			miniCooper.setHolder(used, true);
 			miniCooper.setLink(carColor, "Mini Cooper", black);
 			car.setInstance("Audi A4 3.0 TDI").setHolder(power, 233);
 			car.setInstance("Peugeot 106 GTI").setHolder(power, 120);
