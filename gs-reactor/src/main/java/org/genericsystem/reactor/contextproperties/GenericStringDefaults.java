4434f9d8677999a16b3d35e9ff52524b968df280
Fabienne Ducroquet <fducroquet@middlewarefactory.com>
2016-12-28 17:11:16 +0100

    gs-reactor: Listen for changes on @SetStringExtractor

--- gs-reactor/src/main/java/org/genericsystem/reactor/contextproperties/GenericStringDefaults.java
+++ gs-reactor/src/main/java/org/genericsystem/reactor/contextproperties/GenericStringDefaults.java
@@ -4,9 +4,8 @@ import org.genericsystem.reactor.Context;
 import org.genericsystem.reactor.Tag;
 import org.genericsystem.reactor.context.StringExtractor;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.Property;
-import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 
 public interface GenericStringDefaults extends TextPropertyDefaults {
@@ -14,10 +13,13 @@ public interface GenericStringDefaults extends TextPropertyDefaults {
 	public static final String GENERIC_STRING = "genericString";
 	public static final String EXTRACTOR = "extractor";
 
-	default ObservableValue<String> getGenericStringProperty(Context model) {
-		if (!model.containsProperty((Tag) this, GENERIC_STRING))
-			storeProperty(GENERIC_STRING, model, m -> new ReadOnlyStringWrapper(getStringExtractor(m).apply(m.getGeneric())));
-		return getProperty(GENERIC_STRING, model);
+	default ObservableValue<String> getGenericStringProperty(Context context) {
+		if (!context.containsProperty((Tag) this, GENERIC_STRING))
+			storeProperty(GENERIC_STRING, context, m -> Bindings.createStringBinding(() -> {
+				Property<StringExtractor> stringExtractor = getStringExtractorProperty(m);
+				return stringExtractor.getValue() != null ? stringExtractor.getValue().apply(m.getGeneric()) : StringExtractor.SIMPLE_CLASS_EXTRACTOR.apply(m.getGeneric());
+			}, getStringExtractorProperty(m)));
+		return getObservableValue(GENERIC_STRING, context);
 	}
 
 	default void bindText() {
@@ -28,19 +30,14 @@ public interface GenericStringDefaults extends TextPropertyDefaults {
 		getDomNodeTextProperty(context).bind(getGenericStringProperty(context));
 	}
 
-	default StringExtractor getStringExtractor(Context model) {
-		Property<StringExtractor> stringExtractorProperty = getProperty(EXTRACTOR, model);
-		return stringExtractorProperty != null ? stringExtractorProperty.getValue() : StringExtractor.SIMPLE_CLASS_EXTRACTOR;
+	default Property<StringExtractor> getStringExtractorProperty(Context context) {
+		if (!context.containsProperty((Tag) this, EXTRACTOR))
+			createNewInitializedProperty(EXTRACTOR, context, c -> null);
+		return getProperty(EXTRACTOR, context);
 	}
 
 	default void setStringExtractor(Context context, StringExtractor extractor) {
-		if (!context.containsProperty((Tag) this, EXTRACTOR))
-			storeProperty(EXTRACTOR, context, m -> {
-				Property<StringExtractor> extractorProperty = new SimpleObjectProperty<>(extractor);
-				return extractorProperty;
-			});
-		else
-			getProperty(EXTRACTOR, context).setValue(extractor);
+		getStringExtractorProperty(context).setValue(extractor);
 	}
 
 	default void setStringExtractor(StringExtractor extractor) {
