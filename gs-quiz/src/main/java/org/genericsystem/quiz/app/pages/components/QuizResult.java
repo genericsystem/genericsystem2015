b0f72804aa489d5c3e4ada60c7055d93d1b4bb7b
JClaude Kaing <jckaing@middlewarefactory.com>
2017-01-16 17:13:42 +0100

    gs-quiz : Results can now be displayed when no user's logged

--- gs-quiz/src/main/java/org/genericsystem/quiz/app/pages/components/QuizResult.java
+++ gs-quiz/src/main/java/org/genericsystem/quiz/app/pages/components/QuizResult.java
@@ -1,36 +1,41 @@
 package org.genericsystem.quiz.app.pages.components;
 
 import org.genericsystem.common.Generic;
-import org.genericsystem.quiz.app.pages.components.QuizResult.AllSumResults.ScoreDiv;
-import org.genericsystem.quiz.app.pages.components.QuizResult.AllSumResults.ScoreDiv.ResultDiv01;
-import org.genericsystem.quiz.app.pages.components.QuizResult.AllSumResults.ScoreDiv.ResultDiv02;
-import org.genericsystem.quiz.app.pages.components.QuizResult.AllSumResults.TitleResult;
+import org.genericsystem.quiz.app.pages.components.QuizResult.AllResults;
+import org.genericsystem.quiz.app.pages.components.QuizResult.AllResults.ScoreDiv;
+import org.genericsystem.quiz.app.pages.components.QuizResult.AllResults.ScoreDiv.QuizDiv;
+import org.genericsystem.quiz.app.pages.components.QuizResult.AllResults.ScoreDiv.Score01;
+import org.genericsystem.quiz.app.pages.components.QuizResult.AllResults.ScoreDiv.Score02;
+import org.genericsystem.quiz.app.pages.components.QuizResult.AllResults.ScoreDiv.UserDiv;
+import org.genericsystem.quiz.app.pages.components.QuizResult.AllResults.TitleResult;
 import org.genericsystem.quiz.app.pages.components.QuizResult.MySumResult.MyResultP;
 import org.genericsystem.quiz.app.pages.components.QuizResult.MySumResult.TitleResultH1;
 import org.genericsystem.quiz.app.pages.components.QuizResult.SummaryResults;
 import org.genericsystem.quiz.model.ScoreUserQuiz;
-import org.genericsystem.quiz.utils.QuizExtractors.SCORES_EXTRACTOR;
+import org.genericsystem.quiz.utils.QuizExtractors.QUIZ_EXTRACTOR;
 import org.genericsystem.quiz.utils.QuizExtractors.USER_EXTRACTOR;
 import org.genericsystem.quiz.utils.ScoreUtils;
 import org.genericsystem.reactor.annotations.BindText;
 import org.genericsystem.reactor.annotations.Children;
+import org.genericsystem.reactor.annotations.DirectSelect;
 import org.genericsystem.reactor.annotations.ForEach;
 import org.genericsystem.reactor.annotations.Select;
 import org.genericsystem.reactor.annotations.SelectContext;
 import org.genericsystem.reactor.annotations.SetText;
 import org.genericsystem.reactor.annotations.Style;
 import org.genericsystem.reactor.context.ObservableContextSelector.SELECTION_SELECTOR;
+import org.genericsystem.reactor.context.ObservableListExtractor.SUBINSTANCES;
 import org.genericsystem.reactor.contextproperties.SelectionDefaults;
+import org.genericsystem.reactor.gscomponents.FlexDiv.FlexRow;
 import org.genericsystem.reactor.gscomponents.HtmlTag.HtmlDiv;
 import org.genericsystem.reactor.gscomponents.HtmlTag.HtmlH1;
 import org.genericsystem.reactor.gscomponents.HtmlTag.HtmlP;
 
-@Children(SummaryResults.class)
-@SetText("DIV DE RESULTATS !!!!!!!!!!")
+@Children({ SummaryResults.class, AllResults.class })
 public class QuizResult extends HtmlDiv {
 
-	// Pour s'afficher, SummaryResult a besoin d'un Quiz en context. Il est utilisé pour afficher les score d'un utilisateur après qu'il ait passé un quiz
-	@Children({ MySumResult.class, AllSumResults.class })
+	// Pour s'afficher, SummaryResult a besoin d'un Quiz en context. Il est utilisé pour afficher les scores d'un utilisateur après qu'il ait passé un quiz
+	@Children({ MySumResult.class })
 	@SelectContext(SELECTION_SELECTOR.class)
 	public static class SummaryResults extends HtmlDiv implements SelectionDefaults {
 
@@ -69,75 +74,75 @@ public class QuizResult extends HtmlDiv {
 		}
 	}
 
+	@DirectSelect(ScoreUserQuiz.class)
 	@Children({ TitleResult.class, ScoreDiv.class })
-	@SetText("Tous les résultats :")
-	//
+	@ForEach(path = ScoreDiv.class, value = SUBINSTANCES.class)
+	@Style(name = "width", value = "90%")
 	@Style(path = { HtmlDiv.class, HtmlDiv.class }, name = "flex", value = "1")
-	public static class AllSumResults extends HtmlDiv {
+	public static class AllResults extends HtmlDiv {
 
-		@Children({ HtmlDiv.class, HtmlDiv.class, HtmlDiv.class, HtmlDiv.class })
+		@Children({ HtmlDiv.class, HtmlDiv.class, HtmlDiv.class, HtmlDiv.class, HtmlDiv.class })
 		@SetText(path = HtmlDiv.class, pos = 0, value = "Pseudo")
 		@SetText(path = HtmlDiv.class, pos = 1, value = "Réponses correctes")
 		@SetText(path = HtmlDiv.class, pos = 2, value = "Score 1")
 		@SetText(path = HtmlDiv.class, pos = 3, value = "Score 2")
+		@SetText(path = HtmlDiv.class, pos = 4, value = "Quiz")
 		//
-		@Style(name = "display", value = "flex")
-		@Style(name = "flex", value = "1")
-		@Style(name = "flex-direction", value = "row")
-		@Style(name = "width", value = "100%")
-		public static class TitleResult extends HtmlDiv {
+		public static class TitleResult extends FlexRow {
 
 		}
 
-		@Children({ HtmlDiv.class, HtmlDiv.class, ResultDiv01.class, ResultDiv02.class })
-		@ForEach(SCORES_EXTRACTOR.class)
-		@Select(path = HtmlDiv.class, pos = 0, value = USER_EXTRACTOR.class)
-		@BindText(path = HtmlDiv.class, pos = 0)
+		@Children({ UserDiv.class, HtmlDiv.class, Score01.class, Score02.class, QuizDiv.class })
+		@Select(path = UserDiv.class, value = USER_EXTRACTOR.class)
+		@Select(path = QuizDiv.class, value = QUIZ_EXTRACTOR.class)
+		@BindText(path = UserDiv.class)
 		@BindText(path = HtmlDiv.class, pos = 1)
-		//
-		@Style(name = "display", value = "flex")
-		@Style(name = "flex", value = "1")
-		@Style(name = "flex-direction", value = "row")
-		@Style(name = "width", value = "100%")
-		public static class ScoreDiv extends HtmlDiv {
+		@BindText(path = QuizDiv.class)
+		public static class ScoreDiv extends FlexRow {
 
-			public static class ResultDiv01 extends HtmlDiv {
+			@Override
+			public void init() {
+				addPrefixBinding(context -> {
+
+					// REMARQUE : context.getGeneric().getComponent(0) == User.
+					// context.getGeneric().getComponent(1) == Quiz.
+					if (context.getGeneric().getComponent(0).equals(getLoggedUserProperty(context).getValue())) {
+						this.addStyle(context, "font-weight", "bold");
+					}
+				});
+			}
+
+			public static class UserDiv extends HtmlDiv {
+
+			}
+
+			public static class Score01 extends HtmlDiv {
+
+				@Override
+				public void init() {
+					addPrefixBinding(context -> {
+						Double grade = ScoreUtils.calculateSimpleGrade(context, context.getGeneric().getComponent(1), context.getGeneric().getComponent(0));
+						this.setText(context, grade + " / 20");
+					});
+				}
+			}
+
+			public static class Score02 extends HtmlDiv {
 
 				@Override
 				public void init() {
 
 					addPrefixBinding(context -> {
-						Generic scoreUserQuiz = context.getGeneric();
-
-						if (!(scoreUserQuiz.getMeta().equals(scoreUserQuiz.getRoot().find(ScoreUserQuiz.class))))
-							return;
-
-						Generic user = scoreUserQuiz.getComponent(0);
-						Generic quiz = scoreUserQuiz.getComponent(1);
-						Double grade01 = ScoreUtils.calculateSimpleGrade(context, quiz, user);
+						Double grade01 = ScoreUtils.calculateDualGrade(context, context.getGeneric().getComponent(1), context.getGeneric().getComponent(0));
 						this.setText(context, grade01 + " / 20 ");
 					});
 				}
 			}
 
-			public static class ResultDiv02 extends HtmlDiv {
+			public static class QuizDiv extends HtmlDiv {
 
-				@Override
-				public void init() {
-
-					addPrefixBinding(context -> {
-						Generic scoreUserQuiz = context.getGeneric();
-
-						if (!(scoreUserQuiz.getMeta().equals(scoreUserQuiz.getRoot().find(ScoreUserQuiz.class))))
-							return;
-
-						Generic user = scoreUserQuiz.getComponent(0);
-						Generic quiz = scoreUserQuiz.getComponent(1);
-						Double grade02 = ScoreUtils.calculateDualGrade(context, quiz, user);
-						this.setText(context, grade02 + " / 20");
-					});
-				}
 			}
 		}
+
 	}
 }
