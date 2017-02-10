e03751617cf5e47367642450530ba36d25f58f27
lerotham <jmathorel@middlewarefactory.com>
2017-02-10 18:19:50 +0100

    villes France

--- gs-geography/src/main/java/org/genericsystem/geography/app/GeoApp.java
+++ gs-geography/src/main/java/org/genericsystem/geography/app/GeoApp.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import org.genericsystem.common.Generic;
 import org.genericsystem.common.Root;
 import org.genericsystem.geography.app.GeoApp.GeoScript;
+import org.genericsystem.geography.model.AdministrativeTerritory;
 import org.genericsystem.geography.model.AdministrativeTerritory.Adm1;
 import org.genericsystem.geography.model.AdministrativeTerritory.Adm2;
 import org.genericsystem.geography.model.AdministrativeTerritory.Adm3;
@@ -30,13 +31,14 @@ import org.genericsystem.reactor.gscomponents.Responsive;
 import org.genericsystem.reactor.gscomponents.RootTagImpl;
 
 @RunScript(GeoScript.class)
-@DependsOnModel({ Subdivision.class, Continent.class, Country.class, City.class, Building.class })
+@DependsOnModel({ Subdivision.class, AdministrativeTerritory.class, Continent.class, Country.class, City.class,
+		Building.class })
 @Style(name = "background-color", value = "#00afeb")
 @Children({ ModalEditor.class, AppHeader.class, Responsive.class, Monitor.class })
 @Children(path = Responsive.class, value = { TitledInstancesTable.class, TitledInstancesTable.class,
-		TitledInstancesTable.class })
+		TitledInstancesTable.class, TitledInstancesTable.class, TitledInstancesTable.class })
 @DirectSelect(path = { Responsive.class, TitledInstancesTable.class }, value = { Continent.class, Country.class,
-		Adm1.class })
+		Adm1.class, Adm2.class, Adm3.class })
 public class GeoApp extends RootTagImpl {
 
 	public static void main(String[] mainArgs) {
@@ -45,8 +47,8 @@ public class GeoApp extends RootTagImpl {
 
 	public static class GeoScript implements Script {
 
-		Generic continent, country, adm1, adm2, adm3, city, name;
-		Generic continentInstance, countryInstance, adm1Instance, adm2Instance, adm3Instance, cityInstance;
+		Generic continent, adm, country, adm1, adm2, adm3, city, name;
+		Generic continentInstance, admInstance, countryInstance, adm1Instance, adm2Instance, adm3Instance, cityInstance;
 
 		@Override
 		public void run(Root engine) {
@@ -64,6 +66,7 @@ public class GeoApp extends RootTagImpl {
 			// }
 
 			continent = engine.find(Continent.class);
+			adm = engine.find(AdministrativeTerritory.class);
 			country = engine.find(Country.class);
 			adm1 = engine.find(Adm1.class);
 			adm2 = engine.find(Adm2.class);
@@ -105,17 +108,19 @@ public class GeoApp extends RootTagImpl {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			// First level administrations
+			// First level administrations (only France)
 			try {
 				reader = new BufferedReader(new FileReader("src/main/resources/adm1.csv"));
 				while ((line = reader.readLine()) != null) {
 					try {
 						parts = line.split("\\|");
-						System.out.println("adm1" + parts[0] + "@" + parts[1] + "@" + parts[2]);
-						countryInstance = country.getInstance("CTRY_" + parts[0]);
-						adm1Instance = adm1.addInstance("ADM1_" + parts[0] + "_" + parts[1], countryInstance);
-						adm1Instance.addHolder(name, parts[2]);
-						engine.getCurrentCache().flush();
+						if (parts[0].equals("FR")) {
+							System.out.println("adm1" + parts[0] + "@" + parts[1] + "@" + parts[2]);
+							countryInstance = country.getInstance("CTRY_" + parts[0]);
+							adm1Instance = adm1.addInstance("ADM1_" + parts[0] + "_" + parts[1], countryInstance);
+							adm1Instance.addHolder(name, parts[2]);
+							engine.getCurrentCache().flush();
+						}
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -125,18 +130,20 @@ public class GeoApp extends RootTagImpl {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			// Second level administrations
+			// Second level administrations (only France)
 			try {
 				reader = new BufferedReader(new FileReader("src/main/resources/adm2.csv"));
 				while ((line = reader.readLine()) != null) {
 					try {
 						parts = line.split("\\|");
-						System.out.println("adm2" + parts[0] + "@" + parts[1] + "@" + parts[2] + "@" + parts[3]);
-						adm1Instance = adm1.getInstance("ADM1_" + parts[0] + "_" + parts[1]);
-						adm2Instance = adm2.addInstance("ADM2_" + parts[0] + "_" + parts[1] + "_" + parts[2],
-								adm1Instance);
-						adm2Instance.addHolder(name, parts[3]);
-						engine.getCurrentCache().flush();
+						if (parts[0].equals("FR")) {
+							System.out.println("adm2" + parts[0] + "@" + parts[1] + "@" + parts[2] + "@" + parts[3]);
+							adm1Instance = adm1.getInstance("ADM1_" + parts[0] + "_" + parts[1]);
+							adm2Instance = adm2.addInstance("ADM2_" + parts[0] + "_" + parts[1] + "_" + parts[2],
+									adm1Instance);
+							adm2Instance.addHolder(name, parts[3]);
+							engine.getCurrentCache().flush();
+						}
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -146,19 +153,22 @@ public class GeoApp extends RootTagImpl {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			// Third level administrations
+			// Third level administrations (only France)
 			try {
 				reader = new BufferedReader(new FileReader("src/main/resources/adm3.csv"));
 				while ((line = reader.readLine()) != null) {
 					try {
 						parts = line.split("\\|");
-						System.out.println(
-								"adm3" + parts[0] + "@" + parts[1] + "@" + parts[2] + "@" + parts[3] + "@" + parts[4]);
-						adm2Instance = adm2.getInstance("ADM2_" + parts[0] + "_" + parts[1] + "_" + parts[2]);
-						adm3Instance = adm3.addInstance(
-								"ADM2_" + parts[0] + "_" + parts[1] + "_" + parts[2] + "_" + parts[3], adm2Instance);
-						adm3Instance.addHolder(name, parts[4]);
-						engine.getCurrentCache().flush();
+						if (parts[0].equals("FR")) {
+							System.out.println("adm3" + parts[0] + "@" + parts[1] + "@" + parts[2] + "@" + parts[3]
+									+ "@" + parts[4]);
+							adm2Instance = adm2.getInstance("ADM2_" + parts[0] + "_" + parts[1] + "_" + parts[2]);
+							adm3Instance = adm3.addInstance(
+									"ADM3_" + parts[0] + "_" + parts[1] + "_" + parts[2] + "_" + parts[3],
+									adm2Instance);
+							adm3Instance.addHolder(name, parts[4]);
+							engine.getCurrentCache().flush();
+						}
 					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -168,9 +178,38 @@ public class GeoApp extends RootTagImpl {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			// Populated places
+			// Populated places (only France)
+			String stradm;
 			try {
-				reader = new BufferedReader(new FileReader("src/main/resources/ppl.csv"));
+				reader = new BufferedReader(new FileReader("src/main/resources/populated_fr.csv"));
+				while ((line = reader.readLine()) != null) {
+					try {
+						parts = line.split("\\|");
+						// if (parts[0].equals("FR")) {
+						if (parts[3].length() != 0) {
+							stradm = "ADM3_" + parts[0] + "_" + parts[1] + "_" + parts[2] + "_" + parts[3];
+							admInstance = adm3.getInstance(stradm);
+						} else if (parts[2].length() != 0) {
+							stradm = "ADM2_" + parts[0] + "_" + parts[1] + "_" + parts[2];
+							admInstance = adm2.getInstance(stradm);
+						} else if (parts[1].length() != 0) {
+							stradm = "ADM1_" + parts[0] + "_" + parts[1];
+							admInstance = adm1.getInstance(stradm);
+						} else {
+							stradm = "CTRY_" + parts[0];
+							admInstance = country.getInstance(stradm);
+						}
+						cityInstance = city.addInstance(stradm + "_" + parts[4] + "_" + parts[5] + "_" + parts[6],
+								admInstance);
+						cityInstance.addHolder(name, parts[4]);
+						engine.getCurrentCache().flush();
+						System.out.println(cityInstance);
+						// }
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
