95db196f602508452c64a06fc5aab5d7d2d46064
lerotham <jmathorel@middlewarefactory.com>
2017-02-27 12:06:41 +0100

    Deux types d'autocompletion : avec Datalist mais recherche sur toute la chaîne avec les subdivisions, et perso.

--- gs-geography/src/main/java/org/genericsystem/geography/app/GeoStore.java
+++ gs-geography/src/main/java/org/genericsystem/geography/app/GeoStore.java
@@ -12,8 +12,9 @@ import org.genericsystem.geography.model.AdministrativeTerritory;
 import org.genericsystem.geography.model.AdministrativeTerritory.Adm1;
 import org.genericsystem.geography.model.AdministrativeTerritory.Adm2;
 import org.genericsystem.geography.model.AdministrativeTerritory.Adm3;
-import org.genericsystem.geography.model.Building;
 import org.genericsystem.geography.model.City;
+import org.genericsystem.geography.model.City.Latitude;
+import org.genericsystem.geography.model.City.Longitude;
 import org.genericsystem.geography.model.Country;
 import org.genericsystem.reactor.annotations.Children;
 import org.genericsystem.reactor.annotations.DependsOnModel;
@@ -30,65 +31,82 @@ import org.genericsystem.reactor.gscomponents.Responsive;
 import org.genericsystem.reactor.gscomponents.RootTagImpl;
 
 @RunScript(GeoScript.class)
-@DependsOnModel({ AdministrativeTerritory.class, Country.class, City.class, Building.class })
+@DependsOnModel({ AdministrativeTerritory.class, Country.class, City.class })
 @Style(name = "background-color", value = "#00afeb")
 @Children({ ModalEditor.class, AppHeader.class, Responsive.class, Monitor.class })
 @Children(path = Responsive.class, value = { TitledInstancesTable.class, TitledInstancesTable.class,
 		TitledInstancesTable.class, TitledInstancesTable.class, TitledInstancesTable.class })
 @DirectSelect(path = { Responsive.class, TitledInstancesTable.class }, value = { Country.class, Adm1.class, Adm2.class,
-		Adm3.class })
+		Adm3.class, City.class })
 public class GeoStore extends RootTagImpl {
 
 	public static void main(String[] mainArgs) {
 		ApplicationServer.startSimpleGenericApp(mainArgs, GeoStore.class, "/GeoApp");
 	}
 
-	public static void readFile(String filename, Root engine, Generic returnType) {
+	public static void readFile(String filename, Root engine, Generic returnType, int n) {
 		try {
 			BufferedReader reader;
 			String line;
 			reader = new BufferedReader(new FileReader(filename));
 			while ((line = reader.readLine()) != null)
-				readLine(line, engine, returnType);
+				readLine(line, engine, returnType, n);
 			reader.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		engine.getCurrentCache().flush();
-		System.out.println("ok");
 	}
 
-	public static String componentCode(String[] parts) {
+	public static String componentCode(String[] parts, int n) {
 		String code = parts[0];
-		for (int i = 1; i < parts.length - 2; i++)
+		for (int i = 1; i < n - 1; i++)
 			code += "_" + parts[i];
 		return code;
 	}
 
 	private static HashMap<String, Generic> hm;
 
-	public static void readLine(String line, Root engine, Generic returnType) {
-		System.out.println("@" + line);
+	public static void readLine(String line, Root engine, Generic returnType, int n) {
 		String[] parts = line.split("\\|");
-		// if (!"CD".equals(parts[0]) && !"CV".equals(parts[0]) && !"GE".equals(parts[0]) && !"TJ".equals(parts[0])) {
-		Generic currentInstance;
-		String currentCode;
-		if (returnType.getBaseComponent() == null) {
-			currentCode = parts[parts.length - 2];
-			currentInstance = returnType.addInstance(parts[parts.length - 1]);
-			hm.put(currentCode, currentInstance);
-		} else {
-			String componentCode = componentCode(parts);
-			currentCode = componentCode + "_" + parts[parts.length - 2];
-			Generic componentGeneric = hm.get(componentCode);
-			// currentGeneric = returnType.addInstance(parts[parts.length - 1], componentGeneric);
-			if (componentGeneric != null) {
-				currentInstance = engine.getCurrentCache().buildAndPlug(null, returnType, Collections.emptyList(),
-						parts[parts.length - 1], Collections.singletonList(componentGeneric));
+		if ("FR".equals(parts[0])) {
+			System.out.println("@" + line);
+			Generic currentInstance;
+			String currentCode;
+			if (returnType.getBaseComponent() == null) {
+				currentCode = parts[parts.length - 2];
+				currentInstance = returnType.addInstance(parts[n]);
 				hm.put(currentCode, currentInstance);
+			} else {
+				String componentCode = componentCode(parts, n);
+				// System.out.println(componentCode);
+				currentCode = componentCode + "_" + parts[n - 1];
+				Generic componentInstance = hm.get(componentCode);
+				if (componentInstance != null) {
+					// currentGeneric = returnType.addInstance(parts[parts.length - 1], componentGeneric);
+					String currentValue;
+					if (returnType == engine.find(City.class)) {
+						if ("44".equals(parts[2])) {
+							currentInstance = engine.getCurrentCache().buildAndPlug(null, returnType,
+									Collections.emptyList(), parts[n - 1],
+									Collections.singletonList(componentInstance));
+							Generic latInstance = engine.getCurrentCache().buildAndPlug(null,
+									engine.find(Latitude.class), Collections.emptyList(), parts[n],
+									Collections.singletonList(currentInstance));
+							Generic lonInstance = engine.getCurrentCache().buildAndPlug(null,
+									engine.find(Longitude.class), Collections.emptyList(), parts[n + 1],
+									Collections.singletonList(currentInstance));
+							hm.put(currentCode, currentInstance);
+						}
+					} else {
+						currentValue = parts[n];
+						currentInstance = engine.getCurrentCache().buildAndPlug(null, returnType,
+								Collections.emptyList(), parts[n], Collections.singletonList(componentInstance));
+						hm.put(currentCode, currentInstance);
+					}
+				}
 			}
 		}
-		// }
 	}
 
 	public static class GeoScript implements Script {
@@ -98,61 +116,15 @@ public class GeoStore extends RootTagImpl {
 
 			hm = new HashMap<>();
 
-			readFile("src/main/resources/countries.csv", engine, engine.find(Country.class));
+			readFile("src/main/resources/countries.csv", engine, engine.find(Country.class), 1);
 
-			readFile("src/main/resources/adm1.csv", engine, engine.find(Adm1.class));
+			readFile("src/main/resources/adm1.csv", engine, engine.find(Adm1.class), 2);
 
-			readFile("src/main/resources/adm2.csv", engine, engine.find(Adm2.class));
+			readFile("src/main/resources/adm2.csv", engine, engine.find(Adm2.class), 3);
 
-			readFile("src/main/resources/adm3.csv", engine, engine.find(Adm3.class));
+			readFile("src/main/resources/adm3.csv", engine, engine.find(Adm3.class), 4);
 
-			// // Populated places (only France)
-			// try {
-			// long i = 0;
-			// reader = new BufferedReader(new FileReader("src/main/resources/populated_fr.csv"));
-			// while ((line = reader.readLine()) != null) {
-			// try {
-			// parts = line.split("\\|");
-			// // if (parts[0].equals("FR")) {
-			// start = System.currentTimeMillis();
-			// countryInstance = country.getInstance(parts[0]);
-			// if (parts[3].length() != 0) {
-			// adm1Instance = adm1.getInstance(parts[1], countryInstance);
-			// adm2Instance = adm2.getInstance(parts[2], adm1Instance);
-			// admInstance = adm3.getInstance(parts[3], adm2Instance);
-			// } else if (parts[2].length() != 0) {
-			// adm1Instance = adm1.getInstance(parts[1], countryInstance);
-			// admInstance = adm2.getInstance(parts[2], adm1Instance);
-			// } else if (parts[1].length() != 0) {
-			// admInstance = adm1.getInstance(parts[1], countryInstance);
-			// } else {
-			// admInstance = countryInstance;
-			// }
-			// String truc = parts[4] + "_" + parts[5] + "_" + parts[6];
-			// List<Generic> sglList = Collections.singletonList(admInstance);
-			// List<Generic> sglList0 = Collections.emptyList();
-			// AbstractCache cache = engine.getCurrentCache();
-			// start = System.currentTimeMillis();
-			//
-			// // cityInstance = city.addInstance(parts[4] + "_" + parts[5] + "_" + parts[6], admInstance);
-			// cityInstance = cache.buildAndPlug(null, city, sglList0, truc, sglList);
-			//
-			// i++;
-			// if (i == 100) {
-			// System.out.println(parts[4]);
-			// engine.getCurrentCache().flush();
-			// i = 0;
-			// }
-			// } catch (Exception e) {
-			// // TODO Auto-generated catch block
-			// e.printStackTrace();
-			// }
-			// }
-			// } catch (Exception e) {
-			// // TODO Auto-generated catch block
-			// e.printStackTrace();
-			// //
-			// }
+			readFile("src/main/resources/populated.csv", engine, engine.find(City.class), 5);
 
 		}
 
