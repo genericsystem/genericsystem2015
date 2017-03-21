8570699d79d83bc26ed77e9b26787f96900d6e6c
lerotham <jmathorel@middlewarefactory.com>
2017-03-21 11:10:20 +0100

    heure minute

--- gs-events/src/main/java/org/gs/events/model/Date.java
+++ gs-events/src/main/java/org/gs/events/model/Date.java
@@ -6,11 +6,13 @@ import org.genericsystem.api.core.annotations.Supers;
 import org.genericsystem.api.core.annotations.SystemGeneric;
 import org.genericsystem.api.core.annotations.constraints.InstanceValueClassConstraint;
 import org.gs.events.model.Date.Day;
+import org.gs.events.model.Date.Hour;
+import org.gs.events.model.Date.Minute;
 import org.gs.events.model.Date.Month;
 import org.gs.events.model.Date.Year;
 
 @SystemGeneric
-@Dependencies({ Year.class, Month.class, Day.class })
+@Dependencies({ Year.class, Month.class, Day.class, Hour.class, Minute.class })
 public class Date {
 
 	@SystemGeneric
@@ -31,6 +33,20 @@ public class Date {
 	@Components(Month.class)
 	@Supers(Date.class)
 	public class Day {
+	}
+
+	@SystemGeneric
+	@InstanceValueClassConstraint(Integer.class)
+	@Components(Day.class)
+	@Supers(Date.class)
+	public class Hour {
+	}
+
+	@SystemGeneric
+	@InstanceValueClassConstraint(Integer.class)
+	@Components(Hour.class)
+	@Supers(Date.class)
+	public class Minute {
 	}
 
 }
