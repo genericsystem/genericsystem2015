19441fbe6e74bd5e2747ad9a5d4848d1d71dc73c
Fabienne Ducroquet <fducroquet@middlewarefactory.com>
2017-05-12 17:55:53 +0200

    Continue automatic image classification

--- gs-cv/src/main/java/org/genericsystem/cv/Img.java
+++ gs-cv/src/main/java/org/genericsystem/cv/Img.java
@@ -2,6 +2,8 @@ package org.genericsystem.cv;
 
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.ImageIcon;
@@ -11,12 +13,14 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
 
 import javafx.scene.Node;
 import javafx.scene.image.Image;
@@ -36,6 +40,12 @@ public class Img {
 	public Img sobel(int ddepth, int dx, int dy, int ksize, double scale, double delta, int borderType) {
 		Mat result = new Mat();
 		Imgproc.Sobel(src, result, ddepth, dx, dy, ksize, scale, delta, borderType);
+		return new Img(result);
+	}
+
+	public Img adaptiveThresHold(double maxValue, int adaptiveMethod, int thresholdType, int blockSize, double C) {
+		Mat result = new Mat();
+		Imgproc.adaptiveThreshold(src, result, maxValue, adaptiveMethod, thresholdType, blockSize, C);
 		return new Img(result);
 	}
 
@@ -67,10 +77,125 @@ public class Img {
 		return result;
 	}
 
+	public Img dilate(Mat kernel) {
+		Mat result = new Mat();
+		Imgproc.dilate(src, result, kernel);
+		return new Img(result);
+	}
+
+	public Img dilate(Mat kernel, Point anchor, int iterations) {
+		Mat result = new Mat();
+		Imgproc.dilate(src, result, kernel, anchor, iterations);
+		return new Img(result);
+	}
+
 	public Img canny(double threshold1, double threshold2) {
 		Mat result = new Mat();
 		Imgproc.Canny(src, result, threshold1, threshold2);
 		return new Img(result);
+	}
+
+	public Img canny(double threshold1, double threshold2, int apertureSize, boolean L2gradient) {
+		Mat result = new Mat();
+		Imgproc.Canny(src, result, threshold1, threshold2, apertureSize, L2gradient);
+		return new Img(result);
+	}
+
+	public void drawContours(List<MatOfPoint> contours, int contourIdx, Scalar color, int thickness) {
+		Imgproc.drawContours(src, contours, contourIdx, color, thickness);
+	}
+
+	public Img gaussianBlur(Size ksize, double sigmaX, double sigmaY) {
+		Mat result = new Mat();
+		Imgproc.GaussianBlur(src, result, ksize, sigmaX, sigmaY);
+		return new Img(result);
+	}
+
+	public Img medianBlur(int ksize) {
+		Mat result = new Mat();
+		Imgproc.medianBlur(src, result, ksize);
+		return new Img(result);
+	}
+
+	public Img gray() {
+		Mat result = new Mat();
+		Imgproc.cvtColor(src, result, Imgproc.COLOR_BGR2GRAY);
+		return new Img(result);
+	}
+
+	private static double angle(Point p1, Point p2, Point p0) {
+		double dx1 = p1.x - p0.x;
+		double dy1 = p1.y - p0.y;
+		double dx2 = p2.x - p0.x;
+		double dy2 = p2.y - p0.y;
+		return (dx1 * dx2 + dy1 * dy2) / Math.sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2) + 1e-10);
+	}
+
+	public Img cropAndDeskew() {
+		Img blurred = medianBlur(9);
+		Img gray = blurred.gray(); // new Img(new Mat(blurred.size(), CvType.CV_8U));// = blurred.gray();
+		Img gray_;// = new Img(gray.src);
+
+		List<MatOfPoint> contours = new ArrayList<>();
+
+		double maxArea = 0;
+		int maxId = -1;
+		MatOfPoint2f maxContour = null;
+
+		gray_ = gray.canny(10, 20, 3, true);
+		gray_ = gray_.dilate(new Mat(), new Point(-1, -1), 1);
+
+		contours = gray_.findContours(new Img[1], Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+		for (MatOfPoint contour : contours) {
+			MatOfPoint2f temp = new MatOfPoint2f(contour.toArray());
+			double area = Imgproc.contourArea(contour);
+			MatOfPoint2f approxCurve = new MatOfPoint2f();
+			Imgproc.approxPolyDP(temp, approxCurve, Imgproc.arcLength(temp, true) * 0.02, true);
+
+			if (approxCurve.total() == 4 && area >= maxArea) {
+				double maxCosine = 0;
+
+				List<Point> curves = approxCurve.toList();
+				for (int j = 2; j < 5; j++) {
+					double cosine = Math.abs(angle(curves.get(j % 4), curves.get(j - 2), curves.get(j - 1)));
+					maxCosine = Math.max(maxCosine, cosine);
+				}
+
+				if (maxCosine < 0.3) {
+					maxArea = area;
+					maxId = contours.indexOf(contour);
+					maxContour = approxCurve;
+				}
+			}
+		}
+		Img result = new Img(src);
+		if (maxId >= 0)
+			result = transform(maxContour);
+		// TODO: Warning if no contour found.
+		return result;
+	}
+
+	public Img transform(MatOfPoint2f contour2f) {
+		Mat target = new Mat();
+		List<Point> list = Arrays.asList(contour2f.toArray());
+		double width = Math.max(Math.sqrt(Math.pow(list.get(0).x - list.get(1).x, 2) + Math.pow(list.get(0).y - list.get(1).y, 2)), Math.sqrt(Math.pow(list.get(2).x - list.get(3).x, 2) + Math.pow(list.get(2).y - list.get(3).y, 2)));
+		double height = Math.max(Math.sqrt(Math.pow(list.get(1).x - list.get(2).x, 2) + Math.pow(list.get(1).y - list.get(2).y, 2)), Math.sqrt(Math.pow(list.get(3).x - list.get(0).x, 2) + Math.pow(list.get(3).y - list.get(0).y, 2)));
+		boolean toReverse = width < height;
+		if (toReverse) {
+			System.out.println("inversion width height");
+			double tmp = width;
+			width = height;
+			height = tmp;
+		}
+		List<Point> targets = new LinkedList<>(Arrays.asList(new Point(width, 0), new Point(0, 0), new Point(0, height), new Point(width, height)));
+		if (toReverse) {
+			Point first = targets.get(0);
+			targets.remove(0);
+			targets.add(first);
+		}
+		Imgproc.warpPerspective(src, target, Imgproc.getPerspectiveTransform(contour2f, Converters.vector_Point2f_to_Mat(targets)), new Size(width, height), Imgproc.INTER_CUBIC);
+		return new Img(target);
 	}
 
 	public Size size() {
--- gs-watch/src/main/java/org/genericsystem/watch/ClassifierVerticle.java
+++ gs-watch/src/main/java/org/genericsystem/watch/ClassifierVerticle.java
@@ -1,18 +1,25 @@
 package org.genericsystem.watch;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.genericsystem.cv.Classifier;
+import org.genericsystem.cv.Img;
 import org.genericsystem.cv.ImgClass;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
 public class ClassifierVerticle extends FileCreateEventsHandlerVerticle {
+
+	private static final String LOCK_FILE_NAME = ".lock";
+
 	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
@@ -28,30 +35,46 @@ public class ClassifierVerticle extends FileCreateEventsHandlerVerticle {
 	@Override
 	public void handle(Path newFile) {
 		Mat img = Imgcodecs.imread(newFile.toString(), Imgcodecs.CV_LOAD_IMAGE_COLOR);
-		ImgClass matchingClass = null;
+		Path matchingClassDir = null;
 		Mat alignedImage = null;
 		Path classesDirectory = Paths.get("..", "gs-cv", "classes");
 		classesDirectory.toFile().mkdirs();
-		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(classesDirectory, Files::isDirectory)) {
-			for (Path path : directoryStream) {
-				ImgClass imgClass = new ImgClass(null, path.toString());
-				Mat alignedImage_ = Classifier.compareFeature(img, imgClass.getMean().getSrc(), Classifier.MATCHING_THRESHOLD);
-				if (alignedImage_ != null) {
-					if (matchingClass != null)
-						throw new IllegalStateException("Two matching classes found");
-					matchingClass = imgClass;
-					alignedImage = alignedImage_;
+		try (RandomAccessFile raf = new RandomAccessFile(classesDirectory.resolve(LOCK_FILE_NAME).toString(), "rw"); FileChannel channel = raf.getChannel()) {
+			channel.lock();
+
+			try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(classesDirectory, Files::isDirectory)) {
+				for (Path path : directoryStream) {
+					ImgClass imgClass = new ImgClass(null, path.toString());
+					Mat alignedImage_ = Classifier.compareFeature(img, imgClass.getMean().getSrc(), Classifier.MATCHING_THRESHOLD);
+					if (alignedImage_ != null) {
+						if (matchingClassDir != null)
+							throw new IllegalStateException("Two matching classes found");
+						matchingClassDir = path;
+						alignedImage = alignedImage_;
+					}
+				}
+			} catch (IOException e) {
+				throw new IllegalStateException(e);
+			}
+			if (matchingClassDir == null) {
+				matchingClassDir = classesDirectory.resolve(System.nanoTime() + "");
+				matchingClassDir.toFile().mkdirs();
+				try {
+					alignedImage = new Img(img).cropAndDeskew().getSrc();
+				} catch (Exception e) {
+					throw new IllegalStateException(e);
 				}
 			}
+			String[] fileNameParts = newFile.getFileName().toString().split("\\.(?=[^\\.]+$)");
+			File savedFile;
+			try {
+				savedFile = File.createTempFile(fileNameParts[0] + "-", "." + fileNameParts[1], matchingClassDir.toFile());
+			} catch (IOException e) {
+				throw new IllegalStateException(e);
+			}
+			Imgcodecs.imwrite(savedFile.toString(), alignedImage);
 		} catch (IOException e) {
 			throw new IllegalStateException(e);
-		}
-		if (matchingClass != null)
-			Imgcodecs.imwrite(matchingClass.getDirectory() + "/" + newFile.getFileName().toString(), alignedImage);
-		else {
-			Path unclassifiedDir = Paths.get("..", "gs-cv", "unclassified");
-			unclassifiedDir.toFile().mkdirs();
-			Imgcodecs.imwrite(unclassifiedDir.toString() + "/" + newFile.getFileName().toString(), img);
 		}
 	}
 }
