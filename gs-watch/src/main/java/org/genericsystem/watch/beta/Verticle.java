d4ea0a1fdfde023854c14413eb1f0c117c82eb17
ymoumna <youssef.moumna@middlewarefactory.com>
2017-06-27 17:47:25 +0200

    Message / Task verticle

--- gs-watch/src/main/java/org/genericsystem/watch/beta/Verticle.java
+++ gs-watch/src/main/java/org/genericsystem/watch/beta/Verticle.java
@@ -72,7 +72,6 @@ public class Verticle extends AbstractVerticle {
 					try {
 						Thread.sleep(10000);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 
