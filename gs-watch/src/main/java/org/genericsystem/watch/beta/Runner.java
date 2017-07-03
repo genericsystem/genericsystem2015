819e23fc6d2abc203063275c9dc363aeac6f8743
ymoumna <ymoumna@middlewarefactory.com>
2017-07-03 15:24:39 +0200

    Runner

--- gs-watch/src/main/java/org/genericsystem/watch/beta/Runner.java
+++ gs-watch/src/main/java/org/genericsystem/watch/beta/Runner.java
@@ -8,185 +8,492 @@ import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 
+/*
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
 public class Runner {
 
-	private static final String WEB_EXAMPLES_DIR = "web-examples";
-	private static final String WEB_EXAMPLES_JAVA_DIR = WEB_EXAMPLES_DIR + "/src/main/java/";
-	private static final String WEB_EXAMPLES_JS_DIR = WEB_EXAMPLES_DIR + "/src/main/js/";
-	private static final String WEB_EXAMPLES_GROOVY_DIR = WEB_EXAMPLES_DIR + "/src/main/groovy/";
-	private static final String WEB_EXAMPLES_RUBY_DIR = WEB_EXAMPLES_DIR + "/src/main/ruby/";
+	private static final String CORE_EXAMPLES_DIR = "core-examples";
+	private static final String CORE_EXAMPLES_JAVA_DIR = CORE_EXAMPLES_DIR + "/src/main/java/";
+	private static final String CORE_EXAMPLES_JS_DIR = CORE_EXAMPLES_DIR + "/src/main/js/";
+	private static final String CORE_EXAMPLES_GROOVY_DIR = CORE_EXAMPLES_DIR + "/src/main/groovy/";
+	private static final String CORE_EXAMPLES_RUBY_DIR = CORE_EXAMPLES_DIR + "/src/main/ruby/";
 
 	public static void runClusteredExample(Class clazz) {
-		runExample(WEB_EXAMPLES_JAVA_DIR, clazz, new VertxOptions().setClustered(true), null);
+		runExample(CORE_EXAMPLES_JAVA_DIR, clazz, new VertxOptions().setClustered(true), null);
+	}
+
+	public static void runClusteredExample(Class clazz, VertxOptions options) {
+		runExample(CORE_EXAMPLES_JAVA_DIR, clazz, options.setClustered(true), null);
 	}
 
 	public static void runExample(Class clazz) {
-		runExample(WEB_EXAMPLES_JAVA_DIR, clazz, new VertxOptions().setClustered(false), null);
+		runExample(CORE_EXAMPLES_JAVA_DIR, clazz, new VertxOptions().setClustered(false), null);
 	}
 
 	public static void runExample(Class clazz, DeploymentOptions options) {
-		runExample(WEB_EXAMPLES_JAVA_DIR, clazz, new VertxOptions().setClustered(false), options);
+		runExample(CORE_EXAMPLES_JAVA_DIR, clazz, new VertxOptions().setClustered(false), options);
 	}
 
 	// JavaScript examples
 
 	public static void runJSExample(String scriptName) {
-		runScriptExample(WEB_EXAMPLES_JS_DIR, scriptName, new VertxOptions().setClustered(false));
+		runScriptExample(CORE_EXAMPLES_JS_DIR, scriptName, new VertxOptions().setClustered(false));
 	}
 
 	public static void runJSExampleClustered(String scriptName) {
-		runScriptExample(WEB_EXAMPLES_JS_DIR, scriptName, new VertxOptions().setClustered(true));
+		runScriptExample(CORE_EXAMPLES_JS_DIR, scriptName, new VertxOptions().setClustered(true));
 	}
 
-	static class JSAuthRunner {
+	static class JSEchoServerRunner {
 		public static void main(String[] args) {
-			Runner.runJSExample("io/vertx/example/web/auth/server.js");
+			Runner.runJSExample("io/vertx/example/core/net/echo/server.js");
 		}
 	}
 
-	static class JSAuthJDBC {
+	static class JSEchoClientRunner {
 		public static void main(String[] args) {
-			Runner.runJSExample("io/vertx/example/web/authjdbc/server.js");
+			Runner.runJSExample("io/vertx/example/core/net/echo/client.js");
 		}
 	}
 
-	static class JSHelloWorldRunner {
+	static class JSEchoSslServerRunner {
 		public static void main(String[] args) {
-			Runner.runJSExample("io/vertx/example/web/helloworld/server.js");
+			Runner.runJSExample("io/vertx/example/core/net/echossl/server.js");
 		}
 	}
 
-	static class JSRealtimeRunner {
+	static class JSEchoSslClientRunner {
 		public static void main(String[] args) {
-			Runner.runJSExample("io/vertx/example/web/realtime/server.js");
+			Runner.runJSExample("io/vertx/example/core/net/echossl/client.js");
 		}
 	}
 
-	static class JSChatRunner {
+	static class JSEventBusPointToPointReceiverRunner {
 		public static void main(String[] args) {
-			Runner.runJSExample("io/vertx/example/web/chat/server.js");
+			Runner.runJSExampleClustered("io/vertx/example/core/eventbus/pointtopoint/receiver.js");
 		}
 	}
 
-	static class JSSessionsRunner {
+	static class JSEventBusPointToPointSenderRunner {
 		public static void main(String[] args) {
-			Runner.runJSExample("io/vertx/example/web/sessions/server.js");
+			Runner.runJSExampleClustered("io/vertx/example/core/eventbus/pointtopoint/sender.js");
 		}
 	}
 
-	static class JSTemplatingRunner {
+	static class JSEventBusPubSubReceiverRunner {
 		public static void main(String[] args) {
-			Runner.runJSExample("io/vertx/example/web/templating/mvel/server.js");
+			Runner.runJSExampleClustered("io/vertx/example/core/eventbus/pubsub/receiver.js");
+		}
+	}
+
+	static class JSEventBusPubSubSenderRunner {
+		public static void main(String[] args) {
+			Runner.runJSExampleClustered("io/vertx/example/core/eventbus/pubsub/sender.js");
+		}
+	}
+
+	static class JSHttpsServerRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/https/server.js");
+		}
+	}
+
+	static class JSHttpsClientRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/https/client.js");
+		}
+	}
+
+	static class JSHttpProxyServerRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/proxy/server.js");
+		}
+	}
+
+	static class JSHttpProxyProxyRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/proxy/proxy.js");
+		}
+	}
+
+	static class JSHttpProxyClientRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/proxy/client.js");
+		}
+	}
+
+	static class JSHttpSendFileRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/sendfile/send_file.js");
+		}
+	}
+
+	static class JSHttpSimpleServerRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/simple/server.js");
+		}
+	}
+
+	static class JSHttpSimpleClientRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/simple/client.js");
+		}
+	}
+
+	static class JSHttpSimpleFormRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/simpleform/simple_form_server.js");
+		}
+	}
+
+	static class JSHttpSimpleFormUploadRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/simpleformupload/simple_form_upload_server.js");
+		}
+	}
+
+	static class JSHttpUploadServerRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/upload/server.js");
+		}
+	}
+
+	static class JSHttpUploadClientRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/upload/client.js");
+		}
+	}
+
+	static class JSHttpWebsocketsServerRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/websockets/server.js");
+		}
+	}
+
+	static class JSHttpWebsocketsClientRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/http/websockets/client.js");
+		}
+	}
+
+	static class JSVerticleAsyncStartRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/verticle/asyncstart/deploy_example.js");
+		}
+	}
+
+	static class JSVerticleDeployRunner {
+		public static void main(String[] args) {
+			Runner.runJSExample("io/vertx/example/core/verticle/deploy/deploy_example.js");
 		}
 	}
 
 	// Groovy examples
 
 	public static void runGroovyExample(String scriptName) {
-		runScriptExample(WEB_EXAMPLES_GROOVY_DIR, scriptName, new VertxOptions().setClustered(false));
+		runScriptExample(CORE_EXAMPLES_GROOVY_DIR, scriptName, new VertxOptions().setClustered(false));
 	}
 
 	public static void runGroovyExampleClustered(String scriptName) {
-		runScriptExample(WEB_EXAMPLES_GROOVY_DIR, scriptName, new VertxOptions().setClustered(true));
+		runScriptExample(CORE_EXAMPLES_GROOVY_DIR, scriptName, new VertxOptions().setClustered(true));
 	}
 
-	static class GroovyAuthRunner {
+	static class GroovyEchoServerRunner {
 		public static void main(String[] args) {
-			Runner.runGroovyExample("io/vertx/example/web/auth/server.groovy");
+			Runner.runGroovyExample("io/vertx/example/core/net/echo/server.groovy");
 		}
 	}
 
-	static class GroovyAuthJDBC {
+	static class GroovyEchoClientRunner {
 		public static void main(String[] args) {
-			Runner.runGroovyExample("io/vertx/example/web/authjdbc/server.groovy");
+			Runner.runGroovyExample("io/vertx/example/core/net/echo/client.groovy");
 		}
 	}
 
-	static class GroovyHelloWorldRunner {
+	static class GroovyEchoSslServerRunner {
 		public static void main(String[] args) {
-			Runner.runGroovyExample("io/vertx/example/web/helloworld/server.groovy");
+			Runner.runGroovyExample("io/vertx/example/core/net/echossl/server.groovy");
 		}
 	}
 
-	static class GroovyChatRunner {
+	static class GroovyEchoSslClientRunner {
 		public static void main(String[] args) {
-			Runner.runGroovyExample("io/vertx/example/web/chat/server.groovy");
+			Runner.runGroovyExample("io/vertx/example/core/net/echossl/client.groovy");
 		}
 	}
 
-	static class GroovyRealtimeRunner {
+	static class GroovyEventBusPointToPointReceiverRunner {
 		public static void main(String[] args) {
-			Runner.runGroovyExample("io/vertx/example/web/realtime/server.groovy");
+			Runner.runGroovyExampleClustered("io/vertx/example/core/eventbus/pointtopoint/receiver.groovy");
 		}
 	}
 
-	static class GroovySessionsRunner {
+	static class GroovyEventBusPointToPointSenderRunner {
 		public static void main(String[] args) {
-			Runner.runGroovyExample("io/vertx/example/web/sessions/server.groovy");
+			Runner.runGroovyExampleClustered("io/vertx/example/core/eventbus/pointtopoint/sender.groovy");
 		}
 	}
 
-	static class GroovyTemplatingRunner {
+	static class GroovyEventBusPubSubReceiverRunner {
 		public static void main(String[] args) {
-			Runner.runGroovyExample("io/vertx/example/web/templating/mvel/server.groovy");
+			Runner.runGroovyExampleClustered("io/vertx/example/core/eventbus/pubsub/receiver.groovy");
 		}
 	}
 
-	static class GroovyRestRunner {
+	static class GroovyEventBusPubSubSenderRunner {
 		public static void main(String[] args) {
-			Runner.runGroovyExample("io/vertx/example/web/rest/simple_rest.groovy");
+			Runner.runGroovyExampleClustered("io/vertx/example/core/eventbus/pubsub/sender.groovy");
+		}
+	}
+
+	static class GroovyHttpsServerRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/https/server.groovy");
+		}
+	}
+
+	static class GroovyHttpsClientRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/https/client.groovy");
+		}
+	}
+
+	static class GroovyHttpProxyServerRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/proxy/server.groovy");
+		}
+	}
+
+	static class GroovyHttpProxyProxyRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/proxy/proxy.groovy");
+		}
+	}
+
+	static class GroovyHttpProxyClientRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/proxy/client.groovy");
+		}
+	}
+
+	static class GroovyHttpSendFileRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/sendfile/send_file.groovy");
+		}
+	}
+
+	static class GroovyHttpSimpleServerRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/simple/server.groovy");
+		}
+	}
+
+	static class GroovyHttpSimpleClientRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/simple/client.groovy");
+		}
+	}
+
+	static class GroovyHttpSimpleFormRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/simpleform/simple_form_server.groovy");
+		}
+	}
+
+	static class GroovyHttpSimpleFormUploadRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/simpleformupload/simple_form_upload_server.groovy");
+		}
+	}
+
+	static class GroovyHttpUploadServerRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/upload/server.groovy");
+		}
+	}
+
+	static class GroovyHttpUploadClientRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/upload/client.groovy");
+		}
+	}
+
+	static class GroovyHttpWebsocketsServerRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/websockets/server.groovy");
+		}
+	}
+
+	static class GroovyHttpWebsocketsClientRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/http/websockets/client.groovy");
+		}
+	}
+
+	static class GroovyVerticleAsyncStartRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/verticle/asyncstart/deploy_example.groovy");
+		}
+	}
+
+	static class GroovyVerticleDeployRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/verticle/deploy/deploy_example.groovy");
 		}
 	}
 
 	// Ruby examples
 
 	public static void runRubyExample(String scriptName) {
-		runScriptExample(WEB_EXAMPLES_RUBY_DIR, scriptName, new VertxOptions().setClustered(false));
+		runScriptExample(CORE_EXAMPLES_RUBY_DIR, scriptName, new VertxOptions().setClustered(false));
 	}
 
 	public static void runRubyExampleClustered(String scriptName) {
-		runScriptExample(WEB_EXAMPLES_RUBY_DIR, scriptName, new VertxOptions().setClustered(true));
+		runScriptExample(CORE_EXAMPLES_GROOVY_DIR, scriptName, new VertxOptions().setClustered(true));
 	}
 
-	static class RubyAuthRunner {
+	static class RubyEchoServerRunner {
 		public static void main(String[] args) {
-			Runner.runRubyExample("io/vertx/example/web/auth/server.rb");
+			Runner.runRubyExample("io/vertx/example/core/net/echo/server.rb");
 		}
 	}
 
-	static class RubyAuthJDBC {
+	static class RubyEchoClientRunner {
 		public static void main(String[] args) {
-			Runner.runRubyExample("io/vertx/example/web/authjdbc/server.rb");
+			Runner.runRubyExample("io/vertx/example/core/net/echo/client.rb");
 		}
 	}
 
-	static class RubyHelloWorldRunner {
+	static class RubyEchoSslServerRunner {
 		public static void main(String[] args) {
-			Runner.runRubyExample("io/vertx/example/web/helloworld/server.rb");
+			Runner.runRubyExample("io/vertx/example/core/net/echossl/server.rb");
 		}
 	}
 
-	static class RubyChatRunner {
+	static class RubyEchoSslClientRunner {
 		public static void main(String[] args) {
-			Runner.runRubyExample("io/vertx/example/web/chat/server.rb");
+			Runner.runRubyExample("io/vertx/example/core/net/echossl/client.rb");
 		}
 	}
 
-	static class RubyRealtimeRunner {
+	static class RubyEventBusPointToPointReceiverRunner {
 		public static void main(String[] args) {
-			Runner.runRubyExample("io/vertx/example/web/realtime/server.rb");
+			Runner.runRubyExampleClustered("io/vertx/example/core/eventbus/pointtopoint/receiver.rb");
 		}
 	}
 
-	static class RubySessionsRunner {
+	static class RubyEventBusPointToPointSenderRunner {
 		public static void main(String[] args) {
-			Runner.runRubyExample("io/vertx/example/web/sessions/server.rb");
+			Runner.runRubyExampleClustered("io/vertx/example/core/eventbus/pointtopoint/sender.rb");
 		}
 	}
 
-	static class RubyTemplatingRunner {
+	static class RubyEventBusPubSubReceiverRunner {
 		public static void main(String[] args) {
-			Runner.runRubyExample("io/vertx/example/web/templating/mvel/server.rb");
+			Runner.runRubyExampleClustered("io/vertx/example/core/eventbus/pubsub/receiver.rb");
+		}
+	}
+
+	static class RubyEventBusPubSubSenderRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExampleClustered("io/vertx/example/core/eventbus/pubsub/sender.rb");
+		}
+	}
+
+	static class RubyHttpsServerRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/https/server.rb");
+		}
+	}
+
+	static class RubyHttpsClientRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/https/client.rb");
+		}
+	}
+
+	static class RubyHttpProxyServerRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/proxy/server.rb");
+		}
+	}
+
+	static class RubyHttpProxyProxyRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/proxy/proxy.rb");
+		}
+	}
+
+	static class RubyHttpProxyClientRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/proxy/client.rb");
+		}
+	}
+
+	static class RubyHttpSendFileRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/sendfile/send_file.rb");
+		}
+	}
+
+	static class RubyHttpSimpleServerRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/simple/server.rb");
+		}
+	}
+
+	static class RubyHttpSimpleClientRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/simple/client.rb");
+		}
+	}
+
+	static class RubyHttpSimpleFormRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/simpleform/simple_form_server.rb");
+		}
+	}
+
+	static class RubyHttpSimpleFormUploadRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/simpleformupload/simple_form_upload_server.rb");
+		}
+	}
+
+	static class RubyHttpUploadServerRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/upload/server.rb");
+		}
+	}
+
+	static class RubyHttpUploadClientRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/upload/client.rb");
+		}
+	}
+
+	static class RubyHttpWebsocketsServerRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/websockets/server.rb");
+		}
+	}
+
+	static class RubyHttpWebsocketsClientRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/http/websockets/client.rb");
+		}
+	}
+
+	static class RubyVerticleAsyncStartRunner {
+		public static void main(String[] args) {
+			Runner.runRubyExample("io/vertx/example/core/verticle/asyncstart/deploy_example.rb");
+		}
+	}
+
+	static class RubyVerticleDeployRunner {
+		public static void main(String[] args) {
+			Runner.runGroovyExample("io/vertx/example/core/verticle/deploy/deploy_example.rb");
 		}
 	}
 
