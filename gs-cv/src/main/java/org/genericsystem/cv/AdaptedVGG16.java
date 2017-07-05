05ec8d9b7866ea64be90e318e26b976c7a155f4d
Fabienne Ducroquet <fducroquet@middlewarefactory.com>
2017-07-05 11:11:35 +0200

    Improve performance of VGG16-based network

--- gs-cv/src/main/java/org/genericsystem/cv/AdaptedVGG16.java
+++ gs-cv/src/main/java/org/genericsystem/cv/AdaptedVGG16.java
@@ -2,11 +2,9 @@ package org.genericsystem.cv;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
 
-import org.apache.commons.io.FilenameUtils;
 import org.datavec.api.io.filters.BalancedPathFilter;
 import org.datavec.api.io.labels.ParentPathLabelGenerator;
 import org.datavec.api.split.FileSplit;
@@ -17,13 +15,12 @@ import org.datavec.image.transform.ImageTransform;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.iterator.AsyncDataSetIterator;
 import org.deeplearning4j.earlystopping.EarlyStoppingConfiguration;
-import org.deeplearning4j.earlystopping.saver.LocalFileGraphSaver;
 import org.deeplearning4j.earlystopping.scorecalc.DataSetLossCalculatorCG;
 import org.deeplearning4j.earlystopping.termination.MaxEpochsTerminationCondition;
 import org.deeplearning4j.earlystopping.termination.ScoreImprovementEpochTerminationCondition;
-import org.deeplearning4j.earlystopping.trainer.EarlyStoppingGraphTrainer;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.conf.GradientNormalization;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
@@ -36,7 +33,8 @@ import org.deeplearning4j.zoo.PretrainedType;
 import org.deeplearning4j.zoo.ZooModel;
 import org.deeplearning4j.zoo.model.VGG16;
 import org.nd4j.jita.conf.CudaEnvironment;
-import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.activations.impl.ActivationLReLU;
+import org.nd4j.linalg.activations.impl.ActivationSoftmax;
 import org.nd4j.linalg.dataset.ExistingMiniBatchDataSetIterator;
 import org.nd4j.linalg.dataset.api.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
@@ -60,9 +58,9 @@ public class AdaptedVGG16 {
 		.setMaximumHostCacheableLength(1024 * 1024 * 1024L)
 		.setMaximumHostCache(6L * 1024 * 1024 * 1024L);
 
-		double learningRate = 0.1;
-		int batchSize = 2;
-		int nEpochs = 30;
+		double learningRate = 0.005;
+		int batchSize = 4;
+		int nEpochs = 100;
 		int height = 224;
 		int width = 224;
 		int channels = 3;
@@ -73,9 +71,9 @@ public class AdaptedVGG16 {
 		File parentDir = new File(System.getProperty("user.dir"), "training");
 		FileSplit filesInDir = new FileSplit(parentDir, allowedExtensions, randNumGen);
 		ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
-		BalancedPathFilter pathFilter = new BalancedPathFilter(randNumGen, allowedExtensions, labelMaker, 0, 0, 10, 0);
+		BalancedPathFilter pathFilter = new BalancedPathFilter(randNumGen, allowedExtensions, labelMaker, 0, 0, 100, 0);
 
-		InputSplit[] filesInDirSplit = filesInDir.sample(pathFilter, 70, 15, 15);
+		InputSplit[] filesInDirSplit = filesInDir.sample(pathFilter, .70, .15, .15);
 		InputSplit trainData = filesInDirSplit[0];
 		InputSplit validData = filesInDirSplit[1];
 		InputSplit testData = filesInDirSplit[2];
@@ -101,20 +99,18 @@ public class AdaptedVGG16 {
 				.fineTuneConfiguration(fineTuneConfig)
 				.setFeatureExtractor(featurizedLayer)
 				.removeVertexKeepConnections("predictions")
-				//				.addLayer("fc3", new DenseLayer.Builder()
-				//						.activation(Activation.RELU)
-				//						.weightInit(WeightInit.RELU)
-				//						.nIn(4096).nOut(2048).build(), "fc2")
+				.addLayer("fc3", new DenseLayer.Builder()
+						.activation(new ActivationLReLU(0.33))
+						.weightInit(WeightInit.RELU)
+						.dropOut(0.5)
+						.nIn(4096).nOut(2048).build(), "fc2")
 				.addLayer("predictions", 
 						new OutputLayer.Builder(LossFunctions.LossFunction.MCXENT)
-						.nIn(4096).nOut(outputNum)
+						.nIn(2048).nOut(outputNum)
 						.weightInit(WeightInit.RELU)
-						.activation(Activation.SOFTMAX).build(), "fc2")
+						.dropOut(0.5)
+						.activation(new ActivationSoftmax()).build(), "fc3")
 				.build();
-
-		String tempDir = System.getProperty("java.io.tmpdir");
-		String saveDirectory = FilenameUtils.concat(tempDir, "EarlyStoppingIntermediaryResults/");
-		Paths.get(saveDirectory).toFile().mkdirs();
 
 		TransferLearningHelper transferLearningHelper = new TransferLearningHelper(net, featurizedLayer);
 		ComputationGraph graph = transferLearningHelper.unfrozenGraph();
@@ -130,26 +126,13 @@ public class AdaptedVGG16 {
 		saveFeaturized(getDataSetIterator(recordReader, testData, null, batchSize, outputNum), transferLearningHelper, "test");
 
 		EarlyStoppingConfiguration<ComputationGraph> esConf = new EarlyStoppingConfiguration.Builder<ComputationGraph>()
-				.epochTerminationConditions(new MaxEpochsTerminationCondition(nEpochs))
 				.evaluateEveryNEpochs(1)
-				.epochTerminationConditions(new ScoreImprovementEpochTerminationCondition(20))
+				.epochTerminationConditions(new ScoreImprovementEpochTerminationCondition(20), new MaxEpochsTerminationCondition(nEpochs))
 				.scoreCalculator(new DataSetLossCalculatorCG(getPresavedIterator("validation"), true))
-				.modelSaver(new LocalFileGraphSaver(saveDirectory))
 				.build();
 
-		EarlyStoppingGraphTrainer trainer = new EarlyStoppingGraphTrainer(esConf, graph, getPresavedIterator("train"));
+		EarlyStoppingGraphFeaturizedTrainer trainer = new EarlyStoppingGraphFeaturizedTrainer(esConf, transferLearningHelper, getPresavedIterator("train"));
 		trainer.fit();
-
-		//		ImageTransform flipTransform1 = new FlipImageTransform(rng);
-		// ImageTransform rotateTransform = new RotateImageTransform(rng, 20, 20, 100, 5);
-		//		ImageTransform warpTransform = new WarpImageTransform(rng, 42);
-		//		List<ImageTransform> transforms = Arrays.asList(new ImageTransform[] { warpTransform/* , rotateTransform */ });
-		//		for (ImageTransform transform : transforms) {
-		//			System.out.print("\nTraining on transformation: " + transform.getClass().toString() + "\n\n");
-		//			dataIter = getDataSetIterator(recordReader, trainData, transform, batchSize, outputNum);
-		//			trainer = new EarlyStoppingGraphTrainer(esConf, result.getBestModel(), dataIter);
-		//			result = trainer.fit();
-		//		}
 
 		log.info("Model evaluation:");
 
@@ -157,12 +140,11 @@ public class AdaptedVGG16 {
 		log.info(eval.stats(true));
 
 		File modelFile = new File("AdaptedVGG16-" + System.currentTimeMillis() + ".zip");
-		ModelSerializer.writeModel(graph, modelFile, true);
+		ModelSerializer.writeModel(net, modelFile, true);
 		log.info("Model saved to " + modelFile);
 	}
 
 	public static DataSetIterator getPresavedIterator(String name) {
-		System.out.println("format : " +  "images-" + featurizedLayer + "-" + name + "-%d.bin");
 		DataSetIterator existingTestData = new ExistingMiniBatchDataSetIterator(new File(name + "Folder"), "images-" + featurizedLayer + "-" + name + "-%d.bin");
 		DataSetIterator asyncTestIter = new AsyncDataSetIterator(existingTestData);
 		return asyncTestIter;
@@ -187,7 +169,7 @@ public class AdaptedVGG16 {
 		log.info("Saved " + name + "dataset #" + iterNum);
 	}
 
-	private static DataSetIterator getDataSetIterator(ImageRecordReader recordReader, InputSplit data, ImageTransform transform, int batchSize, int outputNum) {
+	public static DataSetIterator getDataSetIterator(ImageRecordReader recordReader, InputSplit data, ImageTransform transform, int batchSize, int outputNum) {
 		try {
 			recordReader.initialize(data, transform);
 		} catch (IOException e) {
