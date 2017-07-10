2404cc0f8f7a54c2fd569e8e8bf5271c262a74e6
ymoumna <ymoumna@middlewarefactory.com>
2017-07-10 17:17:51 +0200

    Added private path folder

--- gs-watch/src/main/java/org/genericsystem/watch/gamma/DistributedVerticle.java
+++ gs-watch/src/main/java/org/genericsystem/watch/gamma/DistributedVerticle.java
@@ -31,7 +31,8 @@ public class DistributedVerticle extends AbstractVerticle {
 	private static final String KO = "KO";
 	private static final String ID = "ID";
 	private static final String IP = "IP";
-	private final String PRIVATE_ADDRESS = "privateAddress " + hashCode();
+	private final String PRIVATE_ADDRESS = "privateAddress" + hashCode();
+	private final String PRIVATE_PATH = System.getenv("HOME") + "/copy/" + PRIVATE_ADDRESS + "/";
 	private static final DeliveryOptions TIMEOUT = new DeliveryOptions().setSendTimeout(500);
 	private final RoundRobin roundrobin = new RoundRobin();
 	private List<JsonObject> messages = new ArrayList<>();
@@ -44,31 +45,36 @@ public class DistributedVerticle extends AbstractVerticle {
 		System.out.println("Ip : " + ip);
 
 		long id = System.currentTimeMillis();
-		messages.add(new JsonObject().put(ID, id).put("task", new JsonObject().put(ID, id).put(FILENAME, "pdf/image.pdf").put(IP, ip)));
+		messages.add(new JsonObject().put(ID, id).put("task",
+				new JsonObject().put(ID, id).put(FILENAME, "pdf/image.pdf").put(IP, ip)));
 		id = System.currentTimeMillis();
-		messages.add(new JsonObject().put(ID, id).put("task", new JsonObject().put(ID, id).put(FILENAME, "pdf/image2.pdf").put(IP, ip)));
+		messages.add(new JsonObject().put(ID, id).put("task",
+				new JsonObject().put(ID, id).put(FILENAME, "pdf/image2.pdf").put(IP, ip)));
 		id = System.currentTimeMillis();
-		messages.add(new JsonObject().put(ID, id).put("task", new JsonObject().put(ID, id).put(FILENAME, "pdf/image3.pdf").put(IP, ip)));
+		messages.add(new JsonObject().put(ID, id).put("task",
+				new JsonObject().put(ID, id).put(FILENAME, "pdf/image3.pdf").put(IP, ip)));
 	}
 
 	@Override
 	public void start() throws Exception {
 
 		vertx.eventBus().consumer(PRIVATE_ADDRESS, handler -> {
-			System.out.println("Receive from : " + (String) handler.body() + " on : " + PRIVATE_ADDRESS + " " + Thread.currentThread());
+			System.out.println("Receive from : " + (String) handler.body() + " on : " + PRIVATE_ADDRESS + " "
+					+ Thread.currentThread());
 			JsonObject task = new JsonObject((String) handler.body()).getJsonObject("task");
 			tasks.add(task);
 			handler.reply(OK);
 			String fileName = task.getString(FILENAME);
-			File file = new File(System.getenv("HOME") + "/copy/" + fileName);
+			File file = new File(PRIVATE_PATH + fileName);
 			if (!file.exists()) {
 				vertx.executeBlocking(future -> download(future, fileName, task.getString(IP)), result -> {
 					if (result.failed()) {
-						System.out.println("");
+						System.out.println(result.cause());
 						throw new IllegalStateException(result.cause());
 					}
 					System.out.println("Blocking task callback on thread : " + Thread.currentThread());
-					System.out.println("Task +" + task.encodePrettily() + "is done, removing " + Thread.currentThread());
+					System.out
+							.println("Task +" + task.encodePrettily() + "is done, removing " + Thread.currentThread());
 					tasks.remove(task);
 
 				});
@@ -76,22 +82,27 @@ public class DistributedVerticle extends AbstractVerticle {
 				System.out.println("File : " + fileName + " is already dowloaded");
 		});
 		vertx.eventBus().consumer(PUBLIC_ADDRESS, message -> {
-			// System.out.println("Receive on plublic adress private address : " + (String) message.body() + " on : " + PRIVATE_ADDRESS + " " + Thread.currentThread());
+			// System.out.println("Receive on plublic adress private address : "
+			// + (String) message.body() + " on : " + PRIVATE_ADDRESS + " " +
+			// Thread.currentThread());
 			roundrobin.register((String) message.body());
 		});
 		vertx.setPeriodic(AVAILABILITY_PERIODICITY, h -> {
-			// System.out.println("Periodic publish : "+PRIVATE_ADDRESS+" " + Thread.currentThread());
+			// System.out.println("Periodic publish : "+PRIVATE_ADDRESS+" " +
+			// Thread.currentThread());
 			vertx.eventBus().publish(PUBLIC_ADDRESS, PRIVATE_ADDRESS);
 		});
 		vertx.setPeriodic(ROUNDROBIN_PERIODICITY, h -> {
 			for (JsonObject json : new ArrayList<>(messages)) {
 				String robin = roundrobin.getNextAddress();
 				if (robin != null) {
-					System.out.println("Periodic round robin from " + PRIVATE_ADDRESS + " to " + robin + " " + Thread.currentThread());
+					System.out.println("Periodic round robin from " + PRIVATE_ADDRESS + " to " + robin + " "
+							+ Thread.currentThread());
 					vertx.eventBus().send(robin, json.encodePrettily(), TIMEOUT, replyHandler -> {
 						if (replyHandler.failed())
 							throw new IllegalStateException(replyHandler.cause());
-						System.out.println("Receive response : " + replyHandler.result().body() + " " + Thread.currentThread());
+						System.out.println(
+								"Receive response : " + replyHandler.result().body() + " " + Thread.currentThread());
 						if (OK.equals(replyHandler.result().body()))
 							messages.remove(json);
 					});
@@ -101,7 +112,8 @@ public class DistributedVerticle extends AbstractVerticle {
 	}
 
 	private <T> void download(Future<T> future, String fileName, String ip) {
-		// System.out.println("Execute blocking task : " + Thread.currentThread());
+		// System.out.println("Execute blocking task : " +
+		// Thread.currentThread());
 		BlockingQueue<byte[]> blockingQueue = new ArrayBlockingQueue<>(1);
 		HttpClient httpClient = vertx.createHttpClient().getNow(8084, ip, fileName, resp -> resp.bodyHandler(body -> {
 			// System.out.println("get Bytes : " + Thread.currentThread());
@@ -124,8 +136,8 @@ public class DistributedVerticle extends AbstractVerticle {
 		FileOutputStream fos;
 
 		try {
-			File file = new File(System.getenv("HOME") + "/copy/" + fileName);
-			new File(System.getenv("HOME") + "/copy/" + fileName.substring(0, fileName.lastIndexOf("/"))).mkdirs();
+			File file = new File(PRIVATE_PATH + fileName);
+			new File(PRIVATE_PATH + fileName.substring(0, fileName.lastIndexOf("/"))).mkdirs();
 			fos = new FileOutputStream(file);
 			fos.write(bytes);
 			fos.close();
@@ -134,7 +146,8 @@ public class DistributedVerticle extends AbstractVerticle {
 			future.fail(e);
 			return;
 		}
-		// System.out.println("Blocking task complete on thread : " + Thread.currentThread());
+		// System.out.println("Blocking task complete on thread : " +
+		// Thread.currentThread());
 		future.complete();
 	}
 
