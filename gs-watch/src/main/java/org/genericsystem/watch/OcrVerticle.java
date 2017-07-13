694de2f88d488e3bc790b7de76925409b8268083
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-07-13 11:44:29 +0200

    Corrected the model

--- gs-watch/src/main/java/org/genericsystem/watch/OcrVerticle.java
+++ gs-watch/src/main/java/org/genericsystem/watch/OcrVerticle.java
@@ -47,7 +47,7 @@ public class OcrVerticle extends AbstractVerticle {
 		vertx.deployVerticle(new AbstractVerticle() {
 			@Override
 			public void start() throws Exception {
-				Path imagePath = Paths.get(System.getProperty("user.dir") + "/../gs-cv/classes/id-fr-front/image5-0.png");
+				Path imagePath = Paths.get(System.getProperty("user.dir") + "/../gs-cv/classes/id-fr-front/image4-0.png");
 				vertx.eventBus().publish(VerticleDeployer.ACCURATE_ZONES_FOUND, imagePath.toString());
 				}
 			} , res -> {
