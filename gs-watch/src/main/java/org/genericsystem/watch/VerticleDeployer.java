3d27c6891d015ee071509a890222ece7273d75b8
plassalas <plassalas@middlewarefactory.com>
2017-07-13 10:44:53 +0200

    Update VerticleDeployer.java

--- gs-watch/src/main/java/org/genericsystem/watch/VerticleDeployer.java
+++ gs-watch/src/main/java/org/genericsystem/watch/VerticleDeployer.java
@@ -45,6 +45,7 @@ public class VerticleDeployer extends AbstractVerticle {
 		vertx.deployVerticle(new MailWatcherVerticle());
 		vertx.deployVerticle(new PdfsConverterVerticle());
 		vertx.deployVerticle(new ClassifierVerticle());
+		vertx.deployVerticle(new DezonerVerticle());
 		vertx.deployVerticle(new OcrVerticle());
 		startFuture.complete();
 	}
