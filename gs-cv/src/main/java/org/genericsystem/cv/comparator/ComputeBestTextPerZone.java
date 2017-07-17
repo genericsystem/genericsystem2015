fdf4561102bf2c767d5abf582cba462414c1a176
ymoumna <ymoumna@middlewarefactory.com>
2017-07-17 12:25:45 +0200

    merge

---
+++ gs-cv/src/main/java/org/genericsystem/cv/comparator/ComputeBestTextPerZone.java
@@ -0,0 +1,159 @@
+package org.genericsystem.cv.comparator;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.genericsystem.common.Generic;
+import org.genericsystem.common.Root;
+import org.genericsystem.cv.model.Doc;
+import org.genericsystem.cv.model.Doc.DocInstance;
+import org.genericsystem.cv.model.DocClass;
+import org.genericsystem.cv.model.DocClass.DocClassInstance;
+import org.genericsystem.cv.model.ImgFilter;
+import org.genericsystem.cv.model.ImgFilter.ImgFilterInstance;
+import org.genericsystem.cv.model.MeanLevenshtein;
+import org.genericsystem.cv.model.Score;
+import org.genericsystem.cv.model.Score.ScoreInstance;
+import org.genericsystem.cv.model.ZoneGeneric;
+import org.genericsystem.cv.model.ZoneGeneric.ZoneInstance;
+import org.genericsystem.cv.model.ZoneText;
+import org.genericsystem.cv.model.ZoneText.ZoneTextInstance;
+import org.genericsystem.kernel.Engine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ComputeBestTextPerZone {
+
+	private final static String gsPath = System.getenv("HOME") + "/genericsystem/gs-cv_model3/";
+	private static Logger log = LoggerFactory.getLogger(ComputeTrainedScores.class);
+
+	public static void main(String[] mainArgs) {
+		final Engine engine = new Engine(gsPath, Doc.class, ImgFilter.class, ZoneGeneric.class, ZoneText.class,
+				Score.class, MeanLevenshtein.class);
+		engine.newCache().start();
+		compute(engine);
+		engine.close();
+	}
+
+	public static void compute(Root engine) {
+		final String docType = "id-fr-front";
+		compute(engine, docType);
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void compute(Root engine, String docType) {
+
+		Generic currentDocClass = engine.find(DocClass.class).getInstance(docType);
+		List<DocInstance> docInstances = (List) currentDocClass.getHolders(engine.find(Doc.class)).toList();
+		for (DocInstance docInstance : docInstances) {
+			computeOneFile(engine, docInstance, docType);
+		}
+	}
+	
+	public static void computeOneFile(String filename, String docType) {
+		final Engine engine = new Engine(gsPath, Doc.class, ImgFilter.class, ZoneGeneric.class, ZoneText.class,
+				Score.class, MeanLevenshtein.class);
+		engine.newCache().start();
+		computeOneFile(engine, filename, docType);
+		engine.close();
+	}
+	
+	public static void computeOneFile(String filename) {
+		final Engine engine = new Engine(gsPath, Doc.class, ImgFilter.class, ZoneGeneric.class, ZoneText.class,
+				Score.class, MeanLevenshtein.class);
+		final String docType = "id-fr-front";
+		engine.newCache().start();
+		computeOneFile(engine, filename, docType);
+		engine.close();
+	}
+	
+	public static void computeOneFile(Root engine, String filename, String docType) {
+		Doc doc = engine.find(Doc.class);
+		Generic currentDocClass = engine.find(DocClass.class).getInstance(docType);
+		DocInstance docInstance = doc.getDoc(filename, (DocClassInstance) currentDocClass);
+		computeOneFile(engine, docInstance, docType);
+	}
+	
+	// TODO: pass docClassInstance as a parameter instead of docType?
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void computeOneFile(Root engine, DocInstance docInstance, String docType) {
+		Generic currentDocClass = engine.find(DocClass.class).getInstance(docType);
+		ImgFilter imgFilter = engine.find(ImgFilter.class);
+		ZoneText zoneText = engine.find(ZoneText.class);
+		Score score = engine.find(Score.class);
+
+		List<ZoneInstance> zoneInstances = (List) currentDocClass.getHolders(engine.find(ZoneGeneric.class)).toList();
+		List<ImgFilterInstance> imgFilterInstances = (List) imgFilter.getInstances()
+				.filter(f -> !"reality".equals(f.getValue()) && !"best".equals(f.getValue())).toList();
+		ImgFilterInstance realityInstance = imgFilter.getImgFilter("reality");
+		ImgFilterInstance bestInstance = imgFilter.setImgFilter("best");
+		
+		log.debug("Processing doc: {}", docInstance.getValue());
+
+		for (ZoneInstance zoneInstance : zoneInstances) {
+			log.debug("Zone n°{}", zoneInstance.getValue());
+
+			ZoneTextInstance realTextInstance = zoneText.getZoneText(docInstance, zoneInstance, realityInstance);
+
+			// If not supervised, compute the best text
+			// TODO: deal with the case where a field must be left blank
+			if (realTextInstance == null || realTextInstance.getValue().toString().isEmpty()) {
+				// Map containing the distinct OCR texts as a key, and the
+				// names of the imgFilters that gave this OCR
+				Map<String, List<String>> ocrResults = new ConcurrentHashMap<>();
+
+				for (ImgFilterInstance imgFilterInstance : imgFilterInstances) {
+					ZoneTextInstance zti = zoneText.getZoneText(docInstance, zoneInstance, imgFilterInstance);
+					if (zti == null) {
+						// TODO case where zti doesn't exist == filter has
+						// not been applied
+						log.error("No text found for {} => zone n°{}, {}", docInstance.getValue(),
+								zoneInstance.getValue(), imgFilterInstance.getValue());
+					} else {
+						String text = zti.getValue().toString();
+						List<String> filters = ocrResults.get(text);
+						if (filters == null) {
+							filters = new ArrayList<>();
+						}
+						filters.add(imgFilterInstance.getValue().toString());
+						ocrResults.put(text, filters);
+					}
+				}
+
+				// Map containing each distinct OCR text as key, and the
+				// corresponding ponderation as a value (i.e., the sum of
+				// the individual scores of each filter that gave this
+				// string)
+				Map<String, Float> ocrElection = new ConcurrentHashMap<>();
+				
+				ocrResults.entrySet().forEach(entry -> {
+					Float ocrWeight = 0f;
+					for (String filter : entry.getValue()) {
+						ScoreInstance scoreInstance = score.getScore(zoneInstance, imgFilter.getImgFilter(filter));
+						if (scoreInstance == null) {
+							log.error("No score found for zone n°{} and filter {}", zoneInstance.getValue(),
+									filter);
+						} else {
+							ocrWeight += (Float) scoreInstance.getValue();
+						}
+					}
+					ocrElection.put(entry.getKey(), ocrWeight);
+				});
+
+				String bestText = ocrElection.entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey();
+				zoneText.setZoneText(bestText, docInstance, zoneInstance, bestInstance);
+
+			} else {
+				// If supervised, set the supervised text to best
+				zoneText.setZoneText(realTextInstance.getValue().toString(), docInstance, zoneInstance,
+						bestInstance);
+			}
+			
+			engine.getCurrentCache().flush();
+//			System.out.println("Best text : " + zoneText.getZoneText(docInstance, zoneInstance, bestInstance));
+		}
+	}
+}
---
+++ gs-cv/src/main/java/org/genericsystem/cv/model/ModelTools.java
@@ -0,0 +1,85 @@
+package org.genericsystem.cv.model;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import javax.xml.bind.DatatypeConverter;
+
+/**
+ * This class contains only static methods, which can be used as general purpose
+ * tools.
+ * 
+ * @author Pierrik Lassalas
+ *
+ */
+public class ModelTools {
+	
+	public static void main(String[] args) {
+		Path path = Paths.get(System.getProperty("user.home"), "Downloads", "photosafpa.zip");
+		File file = path.toFile();
+		System.out.println(file.getAbsolutePath());
+		if (!file.exists()){
+			System.out.println("File does not exists");
+			System.exit(0);
+		}
+		String hash = hashCode(path);
+		String md5 = getHashFromFile(path, "md5");
+		String sha1 = getHashFromFile(path, "sha-1");
+		String sha256 = getHashFromFile(path, "sha-256");
+		String sha512 = getHashFromFile(path, "sha-512");
+		
+		System.out.println("hash: " + hash + " - " + hash.length());
+		System.out.println("md5: " + md5 + " - " + md5.length());
+		System.out.println("sha-1: " + sha1 + " - " + sha1.length());
+		System.out.println("sha-256: " + sha256 + " - " + sha256.length());
+		System.out.println("sha-512: " + sha512 + " - " + sha512.length());
+	}
+
+	/**
+	 * Generates a hash code from a file.
+	 * 
+	 * Care must be taken to verify that the file exists before calling this
+	 * method (otherwise an exception is thrown).
+	 * 
+	 * @param path
+	 *            - the {@link Path} of the file
+	 * @param algorithm
+	 *            - the desired algorithm used to generate the has code. Every
+	 *            implementation of the Java platform is required to support the
+	 *            following standard MessageDigest algorithms: MD5, SHA-1,
+	 *            SHA-256
+	 * @return the computed {@code hash} as a hexadecimal String
+	 * @throws RuntimeException
+	 *             when the specified algorithm is not found, or when the file
+	 *             could not be read
+	 */
+	public static String getHashFromFile(Path path, String algorithm) throws RuntimeException {
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance(algorithm);
+			md.update(Files.readAllBytes(path));
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("Unable to generate a hash code (no such algorithm)", e);
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to generate a hash code (problem reading the file)", e);
+		}
+		byte[] hash = md.digest();
+		return DatatypeConverter.printHexBinary(hash);
+	}
+	
+	private static String hashCode(Path path) throws RuntimeException {
+		int hashCode;
+		try {
+			hashCode = Arrays.hashCode(Files.readAllBytes(path));
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to generate a hash code (problem reading the file", e);
+		}
+		return Integer.toHexString(hashCode).toUpperCase();
+	}
+}
