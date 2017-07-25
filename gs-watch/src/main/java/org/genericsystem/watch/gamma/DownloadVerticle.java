5214d389fd195e99fb32d202bc658dc399d98cfb
Fabienne Ducroquet <fducroquet@middlewarefactory.com>
2017-07-25 17:08:04 +0200

    gs-watch: Add dispatcher to save tasks and transmit them to the verticles

--- gs-watch/src/main/java/org/genericsystem/watch/gamma/DownloadVerticle.java
+++ gs-watch/src/main/java/org/genericsystem/watch/gamma/DownloadVerticle.java
@@ -3,7 +3,6 @@ package org.genericsystem.watch.gamma;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -16,8 +15,8 @@ public class DownloadVerticle extends ActionVerticle {
 
 	public static final String ACTION = "download";
 
-	public DownloadVerticle(String privateAddress, String privatePath, String ip, List<JsonObject> messages, List<JsonObject> tasks) {
-		super(privateAddress, privatePath, ip, messages, tasks);
+	public DownloadVerticle(String privateAddress, String privatePath, String ip) {
+		super(privateAddress, privatePath, ip);
 	}
 
 	@Override
@@ -26,7 +25,8 @@ public class DownloadVerticle extends ActionVerticle {
 	}
 
 	@Override
-	protected void handle(Future<Object> future, String fileName, JsonObject task) {
+	protected void handle(Future<Object> future, JsonObject task) {
+		String fileName = task.getString(DistributedVerticle.FILENAME);
 		File file = new File(getPrivatePath() + fileName);
 		if (!file.exists())
 			download(future, fileName, task.getString(DistributedVerticle.IP));
@@ -37,24 +37,20 @@ public class DownloadVerticle extends ActionVerticle {
 	}
 
 	@Override
-	protected void handleResult(AsyncResult<Object> result, String fileName) {
+	protected void handleResult(AsyncResult<Object> result, JsonObject task) {
 		if (result.failed()) {
 			System.out.println(result.cause());
 			throw new IllegalStateException(result.cause());
 		} else {
+			String fileName = task.getString(DistributedVerticle.FILENAME);
 			System.out.println("Download successful " + fileName);
-			long id = System.currentTimeMillis();
-			getMessages().add(new JsonObject().put(DistributedVerticle.ID, id).put("task", new JsonObject().put(DistributedVerticle.ID, id)
-					.put(DistributedVerticle.FILENAME, fileName).put(DistributedVerticle.IP, getIp()).put(DistributedVerticle.TYPE, PdfConverterVerticle.ACTION)));
+			addTask(getPrivatePath() + fileName, getIp(), PdfConverterVerticle.ACTION);
 		}
 	}
 
 	private <T> void download(Future<T> future, String fileName, String ip) {
-		// System.out.println("Execute blocking task : " +
-		// Thread.currentThread());
 		BlockingQueue<byte[]> blockingQueue = new ArrayBlockingQueue<>(1);
 		HttpClient httpClient = vertx.createHttpClient().getNow(8084, ip, fileName, resp -> resp.bodyHandler(body -> {
-			// System.out.println("get Bytes : " + Thread.currentThread());
 			try {
 				blockingQueue.put(body.getBytes());
 			} catch (InterruptedException e) {
@@ -84,8 +80,6 @@ public class DownloadVerticle extends ActionVerticle {
 			future.fail(e);
 			return;
 		}
-		// System.out.println("Blocking task complete on thread : " +
-		// Thread.currentThread());
 		future.complete();
 	}
 }
