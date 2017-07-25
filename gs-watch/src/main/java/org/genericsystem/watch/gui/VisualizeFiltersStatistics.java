d3f0daaae1d3cab70a0da82ba627f943700513fd
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-07-25 16:35:28 +0200

    gs-watch: started to improve the VisualizeFiltersStatistics GUI

--- gs-watch/src/main/java/org/genericsystem/watch/gui/VisualizeFiltersStatistics.java
+++ gs-watch/src/main/java/org/genericsystem/watch/gui/VisualizeFiltersStatistics.java
@@ -1,55 +1,69 @@
 package org.genericsystem.watch.gui;
 
+import java.util.Arrays;
+
+import org.genericsystem.api.core.Snapshot;
+import org.genericsystem.common.Generic;
 import org.genericsystem.common.Root;
 import org.genericsystem.cv.comparator.ComputeTrainedScores;
 import org.genericsystem.cv.model.Doc;
 import org.genericsystem.cv.model.DocClass;
+import org.genericsystem.cv.model.DocClass.DocClassInstance;
 import org.genericsystem.cv.model.ImgFilter;
 import org.genericsystem.cv.model.MeanLevenshtein;
 import org.genericsystem.cv.model.Score;
+import org.genericsystem.cv.model.Score.ScoreInstance;
 import org.genericsystem.cv.model.ZoneGeneric;
 import org.genericsystem.cv.model.ZoneText;
 import org.genericsystem.reactor.Context;
 import org.genericsystem.reactor.Tag;
 import org.genericsystem.reactor.annotations.BindAction;
+import org.genericsystem.reactor.annotations.BindText;
 import org.genericsystem.reactor.annotations.Children;
 import org.genericsystem.reactor.annotations.DependsOnModel;
 import org.genericsystem.reactor.annotations.DirectSelect;
+import org.genericsystem.reactor.annotations.ForEach;
 import org.genericsystem.reactor.annotations.SetText;
 import org.genericsystem.reactor.annotations.Style;
+import org.genericsystem.reactor.annotations.Style.FlexDirectionStyle;
+import org.genericsystem.reactor.annotations.StyleClass;
 import org.genericsystem.reactor.annotations.Switch;
 import org.genericsystem.reactor.appserver.ApplicationServer;
 import org.genericsystem.reactor.context.ContextAction;
+import org.genericsystem.reactor.context.ObservableListExtractor;
+import org.genericsystem.reactor.context.TagSwitcher;
 import org.genericsystem.reactor.gscomponents.AppHeader;
 import org.genericsystem.reactor.gscomponents.AppHeader.AppTitleDiv;
 import org.genericsystem.reactor.gscomponents.AppHeader.Logo;
+import org.genericsystem.reactor.gscomponents.FlexDirection;
+import org.genericsystem.reactor.gscomponents.FlexDiv;
 import org.genericsystem.reactor.gscomponents.HtmlTag.HtmlButton;
 import org.genericsystem.reactor.gscomponents.HtmlTag.HtmlH1;
 import org.genericsystem.reactor.gscomponents.InstancesTable;
 import org.genericsystem.reactor.gscomponents.RootTagImpl;
 import org.genericsystem.watch.VerticleDeployerFromWatchApp;
 import org.genericsystem.watch.gui.PageSwitcher.FILTERS_STATISTICS;
-import org.genericsystem.watch.gui.VisualizeFiltersStatistics.RunScriptButton;
+import org.genericsystem.watch.gui.VisualizeFiltersStatistics.DocClassStatisticsDiv;
 
 import io.vertx.core.Verticle;
+import javafx.beans.binding.Bindings;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
- * 
+ * This class provides a gs-reactor application to visualize and update the {@link Score} and {@link MeanLevenshtein} distances for each filters.
  * 
  * @author Pierrik Lassalas
- *
  */
 // TODO: redesign the interface (smaller, add foreach loops)
 @Switch(FILTERS_STATISTICS.class)
 @DependsOnModel({ Doc.class, DocClass.class, ZoneGeneric.class, ZoneText.class, ImgFilter.class, Score.class, MeanLevenshtein.class })
 @Style(name = "background-color", value = "#ffffff")
-@Children({ AppHeader.class, InstancesTable.class })
+@Children({ AppHeader.class, DocClassStatisticsDiv.class })
 @Style(path = AppHeader.class, name = "background-color", value = "#00afeb")
-@Style(path = InstancesTable.class, name = "width", value = "90%")
-@Style(path = InstancesTable.class, name = "margin", value = "auto")
-@Children(path = AppHeader.class, value = { Logo.class, AppTitleDiv.class, RunScriptButton.class })
+@Children(path = AppHeader.class, value = { Logo.class, AppTitleDiv.class })
 @SetText(path = { AppHeader.class, AppTitleDiv.class, HtmlH1.class }, value = "Global OCR accuracy per zone")
-@DirectSelect(path = InstancesTable.class, value = Score.class)
 public class VisualizeFiltersStatistics extends RootTagImpl {
 
 	public static void main(String[] mainArgs) {
@@ -61,10 +75,61 @@ public class VisualizeFiltersStatistics extends RootTagImpl {
 		createNewInitializedProperty(PageSwitcher.PAGE, c -> PageSwitcher.FILTERS_STATISTICS);
 	}
 
+	@Children({ FlexDiv.class, FlexDiv.class })
+	@Children(path = FlexDiv.class, pos = 1, value = InstancesTable.class)
+	@Children(path = FlexDiv.class, pos = 0, value = { FlexDiv.class, FlexDiv.class })
+	@Children(path = { FlexDiv.class, FlexDiv.class }, pos = { 0, 1 }, value = ButtonDiv.class)
+	@BindText(path = { FlexDiv.class, FlexDiv.class }, pos = { 0, 0 })
+	@FlexDirectionStyle(path = FlexDiv.class, pos = 0, value = FlexDirection.ROW)
+	@Style(path = FlexDiv.class, pos = 0, name = "justify-content", value = "center")
+	@Style(path = FlexDiv.class, pos = 0, name = "align-items", value = "center")
+	@Style(path = { FlexDiv.class, FlexDiv.class }, pos = { 0, 0 }, name = "flex", value = "1 1 auto")
+	@Style(path = { FlexDiv.class, FlexDiv.class }, pos = { 0, 1 }, name = "flex", value = "0 1 auto")
+	@StyleClass(path = FlexDiv.class, pos = 0, value = "doc-class-title")
+	@Style(name = "width", value = "90%")
+	@Style(name = "margin", value = "auto")
+	@ForEach(DOC_CLASS_SELECTOR.class)
+	@DirectSelect(path = { FlexDiv.class, InstancesTable.class }, pos = { 1, 0 }, value = Score.class)
+	// @Switch(path = { FlexDiv.class, InstancesTable.class }, pos = { 1, 0 }, value = SCORE_SWITCHER.class)
+	public static class DocClassStatisticsDiv extends FlexDiv {
+
+	}
+
+	@Children(RunScriptButton.class)
+	public static class ButtonDiv extends FlexDiv {
+
+	}
+
 	@SetText("Compute statistics")
 	@BindAction({ COMPUTE_STATS.class })
 	public static class RunScriptButton extends HtmlButton {
 
+	}
+
+	public static class DOC_CLASS_SELECTOR implements ObservableListExtractor {
+		@Override
+		public ObservableList<Generic> apply(Generic[] generics) {
+			Root root = generics[0].getRoot();
+			DocClass docClass = root.find(DocClass.class);
+			Snapshot<Generic> docClassInstances = docClass.getInstances();
+			if (null == docClassInstances)
+				return FXCollections.emptyObservableList();
+			return docClassInstances.toObservableList();
+		}
+	}
+
+	public static class SCORE_SWITCHER implements TagSwitcher {
+		@Override
+		public ObservableValue<Boolean> apply(Context context, Tag tag) {
+			return Bindings.createBooleanBinding(() -> {
+				Score score = (Score) context.getGeneric();
+				DocClassInstance docClassInstance = (DocClassInstance) context.getGenerics()[1];
+				// System.out.println("score: " + score.info());
+				// System.out.println("docClass: " + docClassInstance.info());
+				score.getInstances().forEach(g -> System.out.println(((ScoreInstance) g).getZone().getDocClass())); // XXX why null?
+				return true;
+			});
+		}
 	}
 
 	public static class COMPUTE_STATS implements ContextAction {
