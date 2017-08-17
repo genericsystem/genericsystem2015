099de1bfaf87de109b34634b881e9afd2b0e90a6
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-08-17 12:08:08 +0200

    gs-cv: documentation of ImgFilterFunction

--- gs-cv/src/main/java/org/genericsystem/cv/comparator/ImgFilterFunction.java
+++ gs-cv/src/main/java/org/genericsystem/cv/comparator/ImgFilterFunction.java
@@ -1,5 +1,10 @@
 package org.genericsystem.cv.comparator;
 
+/**
+ * This enum provides a list of filters that will be applied to the Img before going through OCR.
+ * 
+ * @author Pierrik Lassalas
+ */
 public enum ImgFilterFunction {
 	ORIGINAL("original", i -> i),
 	REALITY("reality", i -> i),
@@ -8,9 +13,22 @@ public enum ImgFilterFunction {
 	OTSU_AFTER_GAUSSIAN_BLUR("otsuAfterGaussianBlur", i -> i.otsuAfterGaussianBlur()),
 	ADAPTATIVE_GAUSSIAN_THRESHOLD("adaptativeGaussianThreshold", i -> i.adaptativeGaussianThreshold());
 
+	/**
+	 * Name of the filter. This will be persisted.
+	 */
 	private String name;
+
+	/**
+	 * Lambda function that will be applied to an image to perform the corresponding treatment.
+	 */
 	private ImgFunction lambda;
 
+	/**
+	 * Default constructor.
+	 * 
+	 * @param name - the name of the filter, which will be stored in Generic System
+	 * @param function - an {@link ImgFunction} that will be called to process the image with this filter
+	 */
 	private ImgFilterFunction(String name, ImgFunction function) {
 		this.name = name;
 		this.lambda = function;
