8042748b932cce5b2733af25b21e9293e262cb7d
lerotham <jmathorel@middlewarefactory.com>
2017-09-15 16:44:42 +0200

    OCR rebuild text from several distorted versions : add endString() and leastDifferent()

--- gs-cv/src/main/java/org/genericsystem/cv/docPattern/OCRPlasty.java
+++ gs-cv/src/main/java/org/genericsystem/cv/docPattern/OCRPlasty.java
@@ -5,6 +5,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
+
+import org.genericsystem.cv.Levenshtein;
 
 public class OCRPlasty {
 
@@ -15,6 +18,8 @@ public class OCRPlasty {
 		labels.add(
 				"had I expressed tbe agony I frejuently felt he would have been taught to long for its alleviationq");
 		labels.add("had I expresset th agny I frequently feltu he wouald have ben taufht to lng fr its alevation");
+		// labels.add("coucou");
+		leastDifferent(labels);
 
 		System.out.println(ocrPlasty(labels));
 
@@ -43,14 +48,16 @@ public class OCRPlasty {
 		for (String s : candidates)
 			occurrences.put("@" + s, (occurrences.containsKey("@" + s)) ? occurrences.get("@" + s) + 1 : 1);
 		int maxOcc = Collections.max(occurrences.values());
-		for (Map.Entry<String, Integer> e : occurrences.entrySet()) {
-			if (e.getValue().equals(maxOcc))
-				return e.getKey().substring(1);
+		if (maxOcc > 1) {
+			for (Map.Entry<String, Integer> e : occurrences.entrySet()) {
+				if (e.getValue().equals(maxOcc))
+					return e.getKey().substring(1);
+			}
 		}
-		return "";
+		return leastDifferent(candidates); // if there's no candidate with at least 2 occurrences
 	}
 
-	public static List<String> interString(String string, char c) {
+	private static List<String> interString(String string, char c) {
 		String inter = "";
 		int index = string.indexOf(c);
 		if (index > 0)
@@ -62,11 +69,38 @@ public class OCRPlasty {
 		return is;
 	}
 
-	public static List<String> endString(String string) {
+	private static List<String> endString(String string) {
 		List<String> is = new ArrayList<>();
 		is.add("");
 		is.add(string);
 		return is;
+	}
+
+	private static String leastDifferent(List<String> strings) { // "least different" string from the others (smallest
+																	// sum of levenshtein distance with the others)
+		int n = strings.size();
+		int[][] distances = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (j > i) {
+					int dist = Levenshtein.distance(strings.get(i), strings.get(j));
+					distances[i][j] = dist;
+					distances[j][i] = dist;
+				}
+			}
+			distances[i][i] = 0;
+		}
+		int minVal = Integer.MAX_VALUE;
+		String leastDiff = "";
+		for (int i = 0; i < n; i++) {
+			int val = IntStream.of(distances[i]).sum();
+			if (val < minVal) {
+				leastDiff = strings.get(i);
+				minVal = val;
+			}
+		}
+		return leastDiff;
+
 	}
 
 	public static String longestCommonSubsequence(List<String> labels) { // lcs between n strings
@@ -83,7 +117,6 @@ public class OCRPlasty {
 		int m = X.length();
 		int n = Y.length();
 		int[][] L = new int[m + 1][n + 1];
-
 		// Following steps build L[m+1][n+1] in bottom up fashion. Note
 		// that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1]
 		for (int i = 0; i <= m; i++) {
@@ -99,11 +132,9 @@ public class OCRPlasty {
 		// Following code is used to print LCS
 		int index = L[m][n];
 		int temp = index;
-
 		// Create a character array to store the lcs string
 		char[] lcs = new char[index + 1];
 		lcs[index] = '\0'; // Set the terminating character
-
 		// Start from the right-most-bottom-most corner and one by one store characters in lcs[]
 		int i = m, j = n;
 		while (i > 0 && j > 0) {
@@ -122,7 +153,6 @@ public class OCRPlasty {
 			else
 				j--;
 		}
-
 		String lcsValue = "";
 		for (int k = 0; k <= temp; k++)
 			lcsValue += lcs[k];
