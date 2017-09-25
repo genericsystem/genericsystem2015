d63c9ec50361ebf0fde3b29ef4d780ecf7962018
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-09-25 17:19:06 +0200

    gs-cv-utils: ZonesJsonConverterForUID: refactored the code (automatic detection of files)

--- gs-cv/src/main/java/org/genericsystem/cv/utils/ZonesJsonConverterForUID.java
+++ gs-cv/src/main/java/org/genericsystem/cv/utils/ZonesJsonConverterForUID.java
@@ -3,10 +3,14 @@ package org.genericsystem.cv.utils;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.genericsystem.cv.Zone;
@@ -18,55 +22,87 @@ import io.vertx.core.json.JsonObject;
 
 public class ZonesJsonConverterForUID {
 
-	private static final String docClass = "passport-fr";
-	private static final String filename = System.getProperty("user.dir") + "/../gs-cv/classes/" + docClass + "/zones/zones-old.json";
-	private static final String newFilename = System.getProperty("user.dir") + "/../gs-cv/classes/" + docClass + "/zones/zones.json";
+	private static final String filename = "/zones/zones-old.json";
+	private static final String newFilename = "/zones/zones.json";
 
 	public static void main(String[] args) {
-
-		// Read the initial file into a StringBuffer
-		StringBuffer sb = new StringBuffer();
-		try {
-			for (String string : Files.readAllLines(Paths.get(filename))) {
-				sb.append(string);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+		List<String> paths = new ArrayList<>();
+		for (String docClass : Arrays.asList("id-fr-front", "passport-fr")) {
+			paths.add(System.getenv("HOME") + "/genericsystem/gs-ir-files/classes/" + docClass + "/");
+			paths.add(System.getProperty("user.dir") + "/../gs-cv/classes/" + docClass + "/");
 		}
+		System.out.println(paths);
+		updateJson(paths);
+	}
 
-		// Convert to a JsonObject
-		JsonObject json = new JsonObject(sb.toString());
-		System.out.println(json.encodePrettily());
+	public static void updateJson(List<String> paths) {
+		for (String basePath : paths) {
+			Path pathname = Paths.get(basePath + filename);
+			Path newPathname = Paths.get(basePath + newFilename);
 
-		// Extract the JsonArray to build a new List of Zone objects
-		JsonArray zoneArray = json.getJsonArray("zones");
+			// If zones-old.json doesn't exist:
+			if (!pathname.toFile().exists()) {
+				// If zones.json exists:
+				if (pathname.toFile().exists()) {
+					// Copy zones -> zones-old; then remove zones
+					try {
+						Files.copy(newPathname, pathname, StandardCopyOption.REPLACE_EXISTING);
+						pathname.toFile().delete();
+					} catch (IOException e) {
+						throw new IllegalStateException(e);
+					}
+				} else {
+					throw new IllegalStateException(newPathname + " not found");
+				}
+			} else {
+				System.out.println("Found " + pathname);
+			}
 
-		List<Zone> zonesList = new ArrayList<>();
-		zoneArray.forEach(zone -> {
-			Zone z = null;
+			// Read the initial file into a StringBuffer
+			StringBuffer sb = new StringBuffer();
 			try {
-				z = (Zone) zone;
-			} catch (Exception e) {
-				z = Json.decodeValue(((JsonObject) zone).encode(), Zone.class);
-			} finally {
-				// Create a new Zone each time to compute the UID
-				zonesList.add(new Zone(z.getNum(), z.getRect()));
+				for (String string : Files.readAllLines(pathname)) {
+					sb.append(string);
+				}
+			} catch (IOException e) {
+				throw new IllegalStateException(e);
 			}
-		});
 
-		// Create the Zones object, and convert it into a prettily encoded Json String
-		Zones zones = new Zones(zonesList);
-		JsonObject newJson = new JsonObject(Json.encode(zones));
-		newJson.remove("empty");
-		String newValues = newJson.encodePrettily();
-		System.out.println(newValues);
+			// Convert to a JsonObject
+			JsonObject json = new JsonObject(sb.toString());
+			System.out.println(json.encodePrettily());
 
-		// Save the String into a new File
-		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(newFilename), StandardCharsets.UTF_8, StandardOpenOption.CREATE_NEW)) {
-			writer.write(newValues);
-		} catch (Exception e) {
-			e.printStackTrace();
+			// Extract the JsonArray to build a new List of Zone objects
+			JsonArray zoneArray = json.getJsonArray("zones");
+
+			List<Zone> zonesList = new ArrayList<>();
+			zoneArray.forEach(zone -> {
+				Zone z = null;
+				try {
+					z = (Zone) zone;
+				} catch (Exception e) {
+					z = Json.decodeValue(((JsonObject) zone).encode(), Zone.class);
+				} finally {
+					// Create a new Zone each time to compute the UID
+					zonesList.add(new Zone(z.getNum(), z.getRect()));
+				}
+			});
+
+			// Create the Zones object, and convert it into a prettily encoded Json String
+			Zones zones = new Zones(zonesList);
+			JsonObject newJson = new JsonObject(Json.encode(zones));
+			newJson.remove("empty");
+			String newValues = newJson.encodePrettily();
+			System.out.println(newValues);
+
+			// Save the String into a new File
+			try (BufferedWriter writer = Files.newBufferedWriter(newPathname, StandardCharsets.UTF_8, StandardOpenOption.CREATE_NEW)) {
+				writer.write(newValues);
+			} catch (FileAlreadyExistsException e) {
+				System.out.println(newPathname + " already exists. Skipped...");
+			} catch (Exception e) {
+				throw new IllegalStateException(e);
+			}
 		}
-
 	}
 }
