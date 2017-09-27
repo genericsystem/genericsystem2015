7bb149f898c8baefef9cf7a74e881a65fdb9354c
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-09-27 15:56:44 +0200

    gs-cv-classifier: working version of the Deskewer

--- gs-cv/src/main/java/org/genericsystem/cv/classifier/DeskewVisualizer.java
+++ gs-cv/src/main/java/org/genericsystem/cv/classifier/DeskewVisualizer.java
@@ -18,27 +18,25 @@ public class DeskewVisualizer extends AbstractApp {
 	protected void fillGrid(GridPane mainGrid) {
 		int row = 0;
 		int col = 0;
-		final String filename = System.getenv("HOME") + "/genericsystem/gs-ir-files/converted-png/image-1.png";
+		final String filename = System.getenv("HOME") + "/genericsystem/gs-ir-files/converted-png/image-4.png";
 
 		Img original = new Img(filename);
-		// mainGrid.add(original.getImageView(), col++, row);
-
 		Img deskewed = Deskewer.deskew(Paths.get(filename));
-		// mainGrid.add(deskewed.getImageView(), col++, row);
-
-		// col = 0;
-		// row++;
-
 		Img annotated = Deskewer.getRotatedRectanglesDrawn(original, new Scalar(0, 0, 255), 4);
-		mainGrid.add(annotated.getImageView(), col++, row);
-
 		Img annotated2 = Deskewer.getRotatedRectanglesDrawn(deskewed, new Scalar(0, 0, 255), 4);
+		// Img binary = Deskewer.getBinary(original);
+
+		// mainGrid.add(original.getImageView(), col++, row);
+		mainGrid.add(annotated.getImageView(), col++, row);
+		// mainGrid.add(deskewed.getImageView(), col++, row);
 		mainGrid.add(annotated2.getImageView(), col++, row);
+		// mainGrid.add(binary.getImageView(), col, row++);
 
 		original.close();
 		annotated.close();
 		deskewed.close();
 		annotated2.close();
+		// binary.close();
 	}
 
 }
--- gs-cv/src/main/java/org/genericsystem/cv/classifier/Deskewer.java
+++ gs-cv/src/main/java/org/genericsystem/cv/classifier/Deskewer.java
@@ -2,8 +2,10 @@ package org.genericsystem.cv.classifier;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 
 import org.genericsystem.cv.Img;
 import org.opencv.core.Core;
@@ -18,27 +20,61 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+/**
+ * This class contains static methods that can be used to deskew an {@link Img}.
+ * 
+ * @author Nicolas Feybesse
+ * @author Pierrik Lassalas
+ */
 public class Deskewer {
 
-	private static final double closedImgSize = 5;
+	private static final double closedImgSizeFactor = 2E-6;
+	private static final double minAreaFactor = 3E-5;
 
-	public static Img deskew(Path imgPath) {
+	/**
+	 * Deskew an image.
+	 * 
+	 * @param imgPath - the path to the image
+	 * @return a new {@link Img}
+	 */
+	public static Img deskew(final Path imgPath) {
 		Img img = new Img(imgPath.toString());
 		Img deskewed = _deskew(img);
 		img.close();
 		return deskewed;
 	}
 
+	/**
+	 * Draw the Rotated rectangles used to calculate the deskew angle.
+	 * 
+	 * @param img - the source image
+	 * @param scalar - the color used to draw the rectangles
+	 * @param thickness - the thickness
+	 * @return - an annotated Img
+	 */
 	public static Img getRotatedRectanglesDrawn(final Img img, Scalar scalar, int thickness) {
 		Img imgCopy = new Img(img.getSrc(), true);
 		Img closed = getClosedImg(imgCopy);
 		List<RotatedRect> rectangles = getRotatedRects(closed.getSrc());
-		rectangles.forEach(rect -> drawSingleRotatedRectangle(imgCopy.getSrc(), rect, scalar, thickness));
+		rectangles.stream().forEach(rect -> drawSingleRotatedRectangle(imgCopy.getSrc(), rect, scalar, thickness));
+		List<RotatedRect> filteredRectangles = getInliers(rectangles, 1.0);
+		filteredRectangles.stream().forEach(rect -> drawSingleRotatedRectangle(imgCopy.getSrc(), rect, new Scalar(0, 255, 0), thickness));
 		closed.close();
 		return imgCopy;
 	}
 
-	private static void drawSingleRotatedRectangle(Mat mat, RotatedRect rect, Scalar scalar, int thickness) {
+	/**
+	 * Get the binary image used to compute the deskew angle.
+	 * 
+	 * @param img - the source image
+	 * @return a binary image
+	 */
+	public static Img getBinary(final Img img) {
+		return getClosedImg(img);
+	}
+
+	// This function modifies the Mat mat
+	private static void drawSingleRotatedRectangle(Mat mat, final RotatedRect rect, final Scalar scalar, final int thickness) {
 		Point points[] = new Point[4];
 		rect.points(points);
 		for (int i = 0; i < 4; ++i) {
@@ -46,17 +82,18 @@ public class Deskewer {
 		}
 	}
 
-	private static Img _deskew(Img img) {
-		Img closed = getClosedImg(img);
+	private static Img _deskew(final Img img) {
+		final Img closed = getClosedImg(img);
 		final double angle = contoursDetection(closed.getSrc());
 		System.out.println("angle: " + angle);
-		Point center = new Point(img.width() / 2, img.height() / 2);
+
+		final Point center = new Point(img.width() / 2, img.height() / 2);
 		// Rotation matrix
 		Mat rotationMatrix = Imgproc.getRotationMatrix2D(center, angle, 1);
 
 		// Get the bounding rectangle
 		Rect bbox = new RotatedRect(center, img.size(), angle).boundingRect();
-		// Adjust the transformation matrix
+		// Adjust the transformation matrix to prevent image cropping
 		double[] array = rotationMatrix.get(0, 2);
 		array[0] += bbox.width / 2 - center.x;
 		rotationMatrix.put(0, 2, array);
@@ -84,34 +121,51 @@ public class Deskewer {
 		return new Img(rotated, false);
 	}
 
-	private static Img getClosedImg(Img img) {
-		return img.adaptativeGaussianInvThreshold(17, 3).morphologyEx(Imgproc.MORPH_CLOSE, Imgproc.MORPH_ELLIPSE, new Size(closedImgSize, closedImgSize));
+	private static Img getClosedImg(final Img img) {
+		double size = (closedImgSizeFactor * img.size().area());
+		// Round the size factor to the nearest odd int
+		size = 2 * (Math.floor(size / 2)) + 1;
+		// return img.bilateralFilter(20, 80, 80).adaptativeGaussianInvThreshold(17, 9).morphologyEx(Imgproc.MORPH_CLOSE, Imgproc.MORPH_ELLIPSE, new Size(closedImgSizeFactor, closedImgSizeFactor));
+		return img.bilateralFilter(20, 80, 80).bgr2Gray().grad(2.0d, 2.0d).thresHold(0, 255, Imgproc.THRESH_BINARY_INV + Imgproc.THRESH_OTSU).bitwise_not().morphologyEx(Imgproc.MORPH_CLOSE, Imgproc.MORPH_ELLIPSE, new Size(size, size));
 	}
 
-	private static double contoursDetection(Mat dilated) {
+	private static double contoursDetection(final Mat dilated) {
 		List<RotatedRect> rotatedRects = getRotatedRects(dilated);
-		double mean = 0;
 		for (RotatedRect rotatedRect : rotatedRects) {
-			if (rotatedRect.angle < -45.) {
+			if (rotatedRect.angle <= -45.0) {
 				rotatedRect.angle += 90.0;
 				double tmp = rotatedRect.size.width;
 				rotatedRect.size.width = rotatedRect.size.height;
 				rotatedRect.size.height = tmp;
 			}
-			mean += rotatedRect.angle;
 		}
-		final double average = mean / rotatedRects.size();
-		List<RotatedRect> goodRects = rotatedRects.stream().filter(rotatedRect -> Math.abs(rotatedRect.angle - average) < 5).collect(Collectors.toList());
-		double goodRectsMean = 0;
-		for (RotatedRect rotatedRect : goodRects)
-			goodRectsMean += rotatedRect.angle;
-		return goodRectsMean / goodRects.size();
+		return getInliers(rotatedRects, 1.0).stream().mapToDouble(i -> i.angle).average().getAsDouble();
 	}
 
-	private static List<RotatedRect> getRotatedRects(Mat dilated) {
+	private static List<RotatedRect> getInliers(final List<RotatedRect> data, final double confidence) {
+		if (null == data)
+			return null;
+
+		double average = data.stream().mapToDouble(rect -> rect.angle).average().getAsDouble();
+		double sd = Math.sqrt(data.stream().mapToDouble(rect -> Math.pow(rect.angle - average, 2)).average().getAsDouble());
+		Collections.sort(data, (r1, r2) -> Double.compare(r1.angle, r2.angle));
+		int middle = data.size() / 2;
+		double median;
+		if (middle % 2 == 1)
+			median = data.get(middle).angle;
+		else
+			median = DoubleStream.of(data.get(middle).angle, data.get(middle - 1).angle).average().getAsDouble();
+
+		List<RotatedRect> result = data.stream().filter(rect -> Math.abs(rect.angle - median) < confidence * sd).collect(Collectors.toList());
+		// List<RotatedRect> result = data.stream().filter(rect -> Math.abs(rect.angle - average) < confidence * sd).collect(Collectors.toList());
+		// List<RotatedRect> result = data.stream().filter(rect -> Math.abs(rect.angle - average) < 5).collect(Collectors.toList());
+		return result;
+	}
+
+	private static List<RotatedRect> getRotatedRects(final Mat dilated) {
 		List<MatOfPoint> contours = new ArrayList<>();
-		Imgproc.findContours(dilated, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-		double minArea = 100;
+		Imgproc.findContours(dilated, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+		double minArea = minAreaFactor * dilated.size().area();
 		List<RotatedRect> rotatedRects = contours.stream().filter(contour -> Imgproc.contourArea(contour) > minArea).map(contour -> Imgproc.minAreaRect(new MatOfPoint2f(contour.toArray()))).collect(Collectors.toList());
 		return rotatedRects;
 	}
