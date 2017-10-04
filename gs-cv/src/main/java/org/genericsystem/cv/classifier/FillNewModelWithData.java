17d8f6f8f29907fd94f1f09dece81c6f134ef3c3
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-10-04 11:14:28 +0200

    gs-cv-classifier: began the implementation of the new SimpleModel (wip)
    FillNewModelWithData is a simpler copy of FillModelWithData with approximately the same behavior,
    but that will work only with the new model. Instead of using Zones and Zone, it uses DocFields and
    DocField.

---
+++ gs-cv/src/main/java/org/genericsystem/cv/classifier/FillNewModelWithData.java
@@ -0,0 +1,199 @@
+package org.genericsystem.cv.classifier;
+
+import java.lang.invoke.MethodHandles;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.genericsystem.api.core.exceptions.RollbackException;
+import org.genericsystem.common.Root;
+import org.genericsystem.cv.Img;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.Consolidated;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.Consolidated.ConsolidatedInstance;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.Doc;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.Doc.DocInstance;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.DocPath;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.DocPath.DocPathInstance;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.DocTimestamp;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.DocTimestamp.DocTimestampInstance;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.ImgFilter;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.ImgFilter.ImgFilterInstance;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.Zone;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.Zone.ZoneInstance;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.ZoneNum;
+import org.genericsystem.cv.classifier.newmodel.SimpleModel.ZoneNum.ZoneNumInstance;
+import org.genericsystem.cv.comparator.FillModelWithData;
+import org.genericsystem.cv.comparator.ImgFilterFunction;
+import org.genericsystem.cv.comparator.ImgFunction;
+import org.genericsystem.cv.model.ModelTools;
+import org.genericsystem.cv.utils.NativeLibraryLoader;
+import org.genericsystem.kernel.Engine;
+import org.opencv.core.Rect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.vertx.core.json.JsonObject;
+
+public class FillNewModelWithData {
+
+	static {
+		NativeLibraryLoader.load();
+	}
+
+	public static final String DOC_PATH = "docPath";
+	public static final String FILENAME = "filename";
+	public static final String ENCODED_FILENAME = "encodedFilename";
+	public static final String DOC_TIMESTAMP = "docTimestamp";
+	public static final String ZONES = "zones";
+	public static final String FIELD_NUM = "fieldNum";
+	public static final String RECT = "rectangle";
+	public static final String CONSOLIDATED = "consolidated";
+
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	private static final String gsPath = System.getenv("HOME") + "/genericsystem/gs-cv-newmodel";
+
+	private static final String BASE_PATH = System.getenv("HOME") + "/genericsystem/gs-ir-files/";
+
+	public static void main(String[] args) {
+		Path filePath = Paths.get(BASE_PATH, "converted-png", "image-1.png");
+		Path deskewedPath = deskewFile(filePath);
+		JsonObject jsonFields = detectFields(deskewedPath);
+		System.out.println(jsonFields.encodePrettily());
+		JsonObject data = processFile(deskewedPath, jsonFields);
+		System.out.println(data.encodePrettily());
+		Root engine = getEngine(gsPath);
+		saveOcrDataInModel(engine, data);
+		engine.close();
+
+	}
+
+	public static Root getEngine(String gsPath) {
+		return new Engine(gsPath, ImgFilter.class, ImgFilterInstance.class, Doc.class, DocInstance.class, Zone.class, ZoneInstance.class, ZoneNum.class, ZoneNumInstance.class, Consolidated.class, ConsolidatedInstance.class, DocPath.class,
+				DocPathInstance.class, DocTimestamp.class, DocTimestampInstance.class);
+	}
+
+	public static Path deskewFile(Path imgPath) {
+		Path deskewed = Deskewer.deskewAndSave(imgPath);
+		return deskewed;
+	}
+
+	public static JsonObject detectFields(Path imgPath) {
+		try (Img deskewed = new Img(imgPath.toString())) {
+			List<Rect> rects = ClassifierUsingFields.detectRects(deskewed);
+			DocFields fields = DocFields.of(rects);
+			JsonObject result = fields.toJsonObject();
+			return result;
+		} catch (Exception e) {
+			throw new IllegalStateException("An error has occured while detecting the fields on file " + imgPath.toString(), e);
+		}
+	}
+
+	public static JsonObject processFile(Path imgPath, JsonObject jsonFields) {
+		// Create a JsonObject for the answer
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.put(DOC_PATH, Paths.get(BASE_PATH).relativize(imgPath).toString());
+		jsonObject.put(FILENAME, imgPath.getFileName().toString());
+		jsonObject.put(ENCODED_FILENAME, ModelTools.generateFileName(imgPath));
+		jsonObject.put(DOC_TIMESTAMP, ModelTools.getCurrentDate());
+
+		// Get the doc fields
+		DocFields fields = DocFields.of(jsonFields);
+
+		// Get the imgFilterFunctions, and create a Map with the processed images
+		Img deskewed = new Img(imgPath.toString());
+		final List<ImgFilterFunction> imgFilterFunctions = FillModelWithData.getFilterFunctions();
+		Map<String, Img> imgs = new ConcurrentHashMap<>(imgFilterFunctions.size() + 1);
+		imgFilterFunctions.forEach(entry -> {
+			String filtername = entry.getName();
+			ImgFunction function = entry.getLambda();
+			logger.info("Applying algorithm {}...", filtername);
+			Img img = null;
+
+			long start = System.nanoTime();
+			if ("original".equals(filtername) || "reality".equals(filtername)) {
+				img = deskewed;
+			} else {
+				img = function.apply(deskewed);
+			}
+			long stop = System.nanoTime();
+			logger.info("({} ms)", (stop - start) / 1_000_000);
+
+			if (null != img) {
+				imgs.put(filtername, img);
+			} else {
+				logger.error("An error as occured for image {} and filter {}", imgPath.getFileName(), filtername);
+			}
+		});
+
+		// Loop through each field, and do the OCR
+		Map<String, JsonObject> result = new ConcurrentHashMap<>(fields.size() + 1);
+		fields.forEach(field -> {
+			logger.info("Field {}", field.getNum());
+			imgs.entrySet().forEach(entry -> {
+				if (!("reality".equals(entry.getKey()) || "best".equals(entry.getKey()))) {
+					// Do the ocr, and store the value in the "labels" Map
+					field.ocr(entry.getValue());
+				}
+			});
+			// Loop through the "labels" Map and choose the best text
+			field.consolidateOcr();
+
+			// Store the field data in a json object
+			JsonObject json = new JsonObject();
+			json.put(FIELD_NUM, field.getNum());
+			json.put(CONSOLIDATED, field.getConsolidated().orElse(""));
+			json.put(RECT, JsonObject.mapFrom(field.getRect()));
+
+			// Add this to the result
+			result.put(field.getUid(), json);
+		});
+		// Store the ocr in the JsonObject
+		jsonObject.put(ZONES, result);
+
+		// Close the resources used by OpenCV
+		deskewed.close();
+		imgs.values().forEach(Img::close);
+
+		// Return the result
+		return jsonObject;
+	}
+
+	public static void saveOcrDataInModel(Root engine, JsonObject data) {
+		// Parse the data
+		String docPath = data.getString(DOC_PATH);
+		String filenameExt = data.getString(ENCODED_FILENAME);
+		Long timestamp = data.getLong(DOC_TIMESTAMP);
+		JsonObject zonesResults = data.getJsonObject(ZONES);
+
+		// Get the generics
+		Doc doc = engine.find(Doc.class);
+
+		// Set the doc instance and some attributes
+		DocInstance docInstance = doc.setDoc(filenameExt);
+		try {
+			docInstance.setDocPath(docPath);
+			docInstance.setDocTimestamp(timestamp);
+		} catch (RollbackException e) {
+			logger.debug("Filename or timestamp have already been set. Resuming task...");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+		// Save the results for each field
+		zonesResults.forEach(entry -> {
+			logger.info("Current zone: {}", entry.getKey());
+			JsonObject field = (JsonObject) entry.getValue();
+			String ocr = field.getString(CONSOLIDATED);
+			JsonObject rect = field.getJsonObject(RECT);
+			ZoneInstance zoneInstance = docInstance.setZone(rect.encode());
+			zoneInstance.setConsolidated(ocr);
+			zoneInstance.setZoneNum(field.getInteger(FIELD_NUM));
+		});
+
+		// Flush the cache
+		engine.getCurrentCache().flush();
+	}
+
+}
