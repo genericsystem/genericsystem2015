e8158a932fba914fe1b76d729ce3cefafee0ac87
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-10-04 15:01:31 +0200

    gs-ir: deprecated OcrParameters verticle, not needed with the new model

--- gs-ir/src/main/java/org/genericsystem/ir/OcrParametersVerticle.java
+++ gs-ir/src/main/java/org/genericsystem/ir/OcrParametersVerticle.java
@@ -14,6 +14,7 @@ import io.vertx.core.json.JsonObject;
  * 
  * @author Pierrik Lassalas
  */
+@Deprecated
 public class OcrParametersVerticle extends ActionPersistentVerticle {
 
 	public static final String ACTION = "ocr";
