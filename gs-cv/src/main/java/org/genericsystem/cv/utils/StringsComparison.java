bd876c9246c1c784c1090479e8cd8918d3e81eb4
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-10-19 09:37:48 +0200

    gs-cv-utils: getShingles returns a Set instead of a List

--- gs-cv/src/main/java/org/genericsystem/cv/utils/StringsComparison.java
+++ gs-cv/src/main/java/org/genericsystem/cv/utils/StringsComparison.java
@@ -3,6 +3,7 @@ package org.genericsystem.cv.utils;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,25 +18,25 @@ public class StringsComparison {
 	private static final int DEFAULT_K = 3;
 	private static final Pattern SPACE_PATTERN = Pattern.compile("\\s+");
 
-	public static List<String> getShinglesSpacesRemoved(String string) {
+	public static Set<String> getShinglesSpacesRemoved(String string) {
 		return getShinglesSpacesRemoved(string, DEFAULT_K);
 	}
 
-	public static List<String> getShinglesSpacesRemoved(String string, int k) {
+	public static Set<String> getShinglesSpacesRemoved(String string, int k) {
 		String stringNoSpaces = SPACE_PATTERN.matcher(string).replaceAll("");
 		return getShingles(stringNoSpaces, k);
 	}
 
-	public static List<String> getShingles(String string) {
+	public static Set<String> getShingles(String string) {
 		return getShingles(string, DEFAULT_K);
 	}
 
-	public static List<String> getShingles(String string, int k) {
+	public static Set<String> getShingles(String string, int k) {
 		if (k <= 0)
 			throw new IllegalArgumentException(String.format("k should be positive (provided value: %d)", k));
 		if (string == null || string.isEmpty())
-			return Collections.emptyList();
-		List<String> shingles = SPACE_PATTERN.splitAsStream(string).flatMap(word -> {
+			return Collections.emptySet();
+		Set<String> shingles = SPACE_PATTERN.splitAsStream(string).flatMap(word -> {
 			if (word.length() < k)
 				return Stream.empty();
 			int size = word.length() - k + 1;
@@ -44,7 +45,7 @@ public class StringsComparison {
 				tmp.add(word.substring(i, i + k));
 			}
 			return tmp.stream();
-		}).collect(Collectors.toList());
+		}).collect(Collectors.toSet());
 		return shingles;
 	}
 
