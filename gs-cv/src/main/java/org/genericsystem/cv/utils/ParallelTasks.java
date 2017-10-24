5aac1c56edcb40d86099b181903402f9eb8121aa
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-10-24 16:38:23 +0200

    gs-cv-utils: refactored ParallelTasks

--- gs-cv/src/main/java/org/genericsystem/cv/utils/ParallelTasks.java
+++ gs-cv/src/main/java/org/genericsystem/cv/utils/ParallelTasks.java
@@ -1,59 +1,65 @@
-package org.genericsystem.layout;
+package org.genericsystem.cv.utils;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ParallelTasks {
-	private final Collection<Runnable> tasks = new ArrayList<>();
+	private final List<Runnable> tasks;
+	private final int counter;
 
 	public ParallelTasks() {
+		this(new ArrayList<>());
+
+	}
+
+	public ParallelTasks(List<Runnable> tasks) {
+		this.tasks = tasks;
+		this.counter = Runtime.getRuntime().availableProcessors();
 	}
 
 	public void add(final Runnable task) {
 		tasks.add(task);
 	}
 
-	public void go() throws InterruptedException {
-		final ExecutorService threads = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+	public void run() throws InterruptedException {
+		final ExecutorService threads = Executors.newFixedThreadPool(counter);
 		try {
 			final CountDownLatch latch = new CountDownLatch(tasks.size());
-			for (final Runnable task : tasks)
-				threads.execute(new Runnable() {
-					@Override
-					public void run() {
-						try {
-							task.run();
-						} finally {
-							latch.countDown();
-						}
-					}
-				});
+			tasks.forEach(task -> threads.execute(() -> {
+				try {
+					task.run();
+				} finally {
+					latch.countDown();
+				}
+			}));
 			latch.await();
 		} finally {
 			threads.shutdown();
 		}
 	}
 
+	public int getCounter() {
+		return counter;
+	}
+
 	public static void main(final String[] args) throws Exception {
 		ParallelTasks tasks = new ParallelTasks();
-		final Runnable waitOneSecond = new Runnable() {
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-				}
+		final Runnable waitOneSecond = () -> {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
 			}
 		};
 		tasks.add(waitOneSecond);
 		tasks.add(waitOneSecond);
 		tasks.add(waitOneSecond);
 		tasks.add(waitOneSecond);
+
 		final long start = System.currentTimeMillis();
-		tasks.go();
+		tasks.run();
 		System.err.println(System.currentTimeMillis() - start);
 	}
 }
