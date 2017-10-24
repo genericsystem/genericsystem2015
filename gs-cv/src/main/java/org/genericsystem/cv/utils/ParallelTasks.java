3ef6b25b07b164f93adb0d8829324101dd90a2c3
Pierrik Lassalas <plassalas@middlewarefactory.com>
2017-10-24 16:45:29 +0200

    gs-cv-utils: documented ParallelTasks

--- gs-cv/src/main/java/org/genericsystem/cv/utils/ParallelTasks.java
+++ gs-cv/src/main/java/org/genericsystem/cv/utils/ParallelTasks.java
@@ -6,24 +6,48 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * Utility class that can be used to run multiple tasks in parallel.
+ * 
+ * @author Nicolas Feybesse
+ * @author Pierrik Lassalas
+ */
 public class ParallelTasks {
 	private final List<Runnable> tasks;
 	private final int counter;
 
+	/**
+	 * Default constructor.
+	 */
 	public ParallelTasks() {
 		this(new ArrayList<>());
 
 	}
 
+	/**
+	 * Construct a ParallelTasks with a pre-defined list of {@link Runnable}.
+	 * 
+	 * @param tasks - a list of {@link Runnable}
+	 */
 	public ParallelTasks(List<Runnable> tasks) {
 		this.tasks = tasks;
 		this.counter = Runtime.getRuntime().availableProcessors();
 	}
 
+	/**
+	 * Add a single task to the tasks list.
+	 * 
+	 * @param task - a {@link Runnable}
+	 */
 	public void add(final Runnable task) {
 		tasks.add(task);
 	}
 
+	/**
+	 * Run all the tasks in parallel.
+	 * 
+	 * @throws InterruptedException if interrupted
+	 */
 	public void run() throws InterruptedException {
 		final ExecutorService threads = Executors.newFixedThreadPool(counter);
 		try {
@@ -41,25 +65,12 @@ public class ParallelTasks {
 		}
 	}
 
+	/**
+	 * The internal Threadpool size (number of available processors)
+	 * 
+	 * @return the number of available processors
+	 */
 	public int getCounter() {
 		return counter;
-	}
-
-	public static void main(final String[] args) throws Exception {
-		ParallelTasks tasks = new ParallelTasks();
-		final Runnable waitOneSecond = () -> {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-			}
-		};
-		tasks.add(waitOneSecond);
-		tasks.add(waitOneSecond);
-		tasks.add(waitOneSecond);
-		tasks.add(waitOneSecond);
-
-		final long start = System.currentTimeMillis();
-		tasks.run();
-		System.err.println(System.currentTimeMillis() - start);
 	}
 }
