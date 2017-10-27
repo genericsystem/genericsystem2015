8628326f886fe08211cce472a346813dbd2ef351
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2017-10-27 15:16:14 +0200

    test another executor

--- gs-cv/src/test/java/org/genericsystem/cv/utils/ExecutorsTest.java
+++ gs-cv/src/test/java/org/genericsystem/cv/utils/ExecutorsTest.java
@@ -2,6 +2,8 @@ package org.genericsystem.cv.utils;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import org.testng.annotations.Test;
@@ -38,4 +40,29 @@ public class ExecutorsTest {
 		executor.shutdown();
 	}
 
+	@Test
+	public void testExecutor0001() {
+		ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 1000, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(), new ThreadPoolExecutor.DiscardPolicy());
+
+		int[] i = new int[] { 0 };
+		executorService.scheduleAtFixedRate(() -> {
+			final int j = i[0];
+			executor.submit(() -> {
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("executed : " + j);
+			});
+			i[0]++;
+		}, 0, 100, TimeUnit.MILLISECONDS);
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		executorService.shutdown();
+	}
 }
