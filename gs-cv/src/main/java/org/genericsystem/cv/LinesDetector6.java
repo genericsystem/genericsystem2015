6e0a7a70cfdd5ada639a6da4b1d4f3e60a9d7358
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2017-11-03 15:40:56 +0100

    Two new implementations with Levenberg

---
+++ gs-cv/src/main/java/org/genericsystem/cv/LinesDetector6.java
@@ -0,0 +1,325 @@
+package org.genericsystem.cv;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import org.genericsystem.cv.lm.LMHostImpl;
+import org.genericsystem.cv.utils.Line;
+import org.genericsystem.cv.utils.NativeLibraryLoader;
+import org.genericsystem.cv.utils.Ransac;
+import org.genericsystem.cv.utils.Ransac.Model;
+import org.genericsystem.cv.utils.Tools;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
+import org.opencv.videoio.VideoCapture;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class LinesDetector6 extends AbstractApp {
+
+	static {
+		NativeLibraryLoader.load();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	private final VideoCapture capture = new VideoCapture(0);
+	private ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
+	private Point vp = new Point(0, 0);
+
+	@Override
+	protected void fillGrid(GridPane mainGrid) {
+		Mat frame = new Mat();
+		capture.read(frame);
+		ImageView frameView = new ImageView(Tools.mat2jfxImage(frame));
+		mainGrid.add(frameView, 0, 0);
+		ImageView deskewedView = new ImageView(Tools.mat2jfxImage(frame));
+		mainGrid.add(deskewedView, 0, 1);
+		Mat dePerspectived = frame.clone();
+
+		if (Lines.K == null) {
+			Lines.K = new Mat(3, 3, CvType.CV_64FC1, new Scalar(0));
+			Lines.K.put(0, 0, frame.width());
+			Lines.K.put(0, 2, frame.width() / 2);
+			Lines.K.put(1, 1, frame.height());
+			Lines.K.put(1, 2, frame.height() / 2);
+			Lines.K.put(2, 2, 1);
+		}
+		timer.scheduleAtFixedRate(() -> {
+			try {
+				capture.read(frame);
+				Img grad = new Img(frame, false).morphologyEx(Imgproc.MORPH_GRADIENT, Imgproc.MORPH_ELLIPSE, new Size(2, 2)).otsu().morphologyEx(Imgproc.MORPH_CLOSE, Imgproc.MORPH_ELLIPSE, new Size(7, 7));
+				Lines lines = new Lines(grad.houghLinesP(1, Math.PI / 180, 10, 100, 10));
+				if (lines.size() > 10) {
+					lines.draw(frame, new Scalar(0, 0, 255));
+					frameView.setImage(Tools.mat2jfxImage(frame));
+					Mat vpCalib = calibrate(Converters.vector_double_to_Mat(Arrays.asList(vp.x, vp.y, 1d)));
+
+					LMHostImpl<Line> fitHost = new LMHostImpl<>((datas, params) -> {
+						Mat lineMat = Lines.getLineMat(datas);
+						double di = params[0] * lineMat.get(0, 0)[0] + params[1] * lineMat.get(1, 0)[0] + params[2] * lineMat.get(2, 0)[0];
+						di /= (Math.sqrt(params[0] * params[0] + params[1] * params[1] + params[2] * params[2]) * Core.norm(lineMat));
+						return di * di;
+					}, lines.lines, new double[] { vpCalib.get(0, 0)[0], vpCalib.get(1, 0)[0], vpCalib.get(2, 0)[0] });
+					double[] newVp = fitHost.getParms();
+					Mat result = unCalibrate(Converters.vector_double_to_Mat(Arrays.asList(newVp[0], newVp[1], newVp[2])));
+					vp = new Point(result.get(0, 0)[0], result.get(1, 0)[0]);
+
+					System.out.println("vp = " + vp);
+					Point bary = new Point(frame.width() / 2, frame.height() / 2);
+					Mat homography = findHomography(vp, bary, frame.width(), frame.height());
+					Imgproc.warpPerspective(frame, dePerspectived, homography, frame.size(), Imgproc.INTER_LINEAR, Core.BORDER_REPLICATE, Scalar.all(255));
+					deskewedView.setImage(Tools.mat2jfxImage(dePerspectived));
+
+				} else
+					System.out.println("Not enough lines : " + lines.size());
+
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+
+		}, 33, 250, TimeUnit.MILLISECONDS);
+
+	}
+
+	public static Mat calibrate(Mat uncalibrated) {
+		Mat dst = new Mat();
+		Core.gemm(Lines.K.inv(), uncalibrated, 1, new Mat(), 0, dst);
+		Core.normalize(dst, dst);
+		return dst;
+	}
+
+	public static Mat unCalibrate(Mat calibrated) {
+		Mat dst = new Mat();
+		Core.gemm(Lines.K, calibrated, 1, new Mat(), 0, dst);
+		if (dst.get(2, 0)[0] != 0) {
+			dst.put(0, 0, dst.get(0, 0)[0] / dst.get(2, 0)[0]);
+			dst.put(1, 0, dst.get(1, 0)[0] / dst.get(2, 0)[0]);
+			dst.put(2, 0, 1d);
+		}
+		return dst;
+	}
+
+	public Point[] rotate(Point bary, double alpha, Point... p) {
+		Mat matrix = Imgproc.getRotationMatrix2D(bary, alpha / Math.PI * 180, 1);
+		MatOfPoint2f results = new MatOfPoint2f();
+		Core.transform(new MatOfPoint2f(p), results, matrix);
+		return results.toArray();
+	}
+
+	private Mat findHomography(Point vp, Point bary, double width, double height) {
+
+		double alpha_ = Math.atan2((vp.y - bary.y), (vp.x - bary.x));
+		if (alpha_ < -Math.PI / 2 && alpha_ > -Math.PI)
+			alpha_ = alpha_ + Math.PI;
+		if (alpha_ < Math.PI && alpha_ > Math.PI / 2)
+			alpha_ = alpha_ - Math.PI;
+		double alpha = alpha_;
+
+		Point rotatedVp = rotate(bary, alpha, vp)[0];
+
+		Point A = new Point(0, 0);
+		Point B = new Point(width, 0);
+		Point C = new Point(width, height);
+		Point D = new Point(0, height);
+
+		Point AB2 = new Point(width / 2, 0);
+		Point CD2 = new Point(width / 2, height);
+
+		Point A_, B_, C_, D_;
+		if (rotatedVp.x >= width / 2) {
+			A_ = new Line(AB2, rotatedVp).intersection(0);
+			D_ = new Line(CD2, rotatedVp).intersection(0);
+			C_ = new Line(A_, bary).intersection(new Line(CD2, rotatedVp));
+			B_ = new Line(D_, bary).intersection(new Line(AB2, rotatedVp));
+		} else {
+			B_ = new Line(AB2, rotatedVp).intersection(width);
+			C_ = new Line(CD2, rotatedVp).intersection(width);
+			A_ = new Line(C_, bary).intersection(new Line(AB2, rotatedVp));
+			D_ = new Line(B_, bary).intersection(new Line(CD2, rotatedVp));
+		}
+
+		System.out.println("vp : " + vp);
+		System.out.println("rotated vp : " + rotatedVp);
+		System.out.println("Alpha : " + alpha * 180 / Math.PI);
+		// System.out.println("A : " + A + " " + A_);
+		// System.out.println("B : " + B + " " + B_);
+		// System.out.println("C : " + C + " " + C_);
+		// System.out.println("D : " + D + " " + D_);
+
+		return Imgproc.getPerspectiveTransform(new MatOfPoint2f(rotate(bary, -alpha, A_, B_, C_, D_)), new MatOfPoint2f(A, B, C, D));
+	}
+
+	public static class Lines extends org.genericsystem.cv.utils.Lines {
+
+		private static Mat K;
+
+		public Lines(Mat src) {
+			super(src);
+		}
+
+		public Lines(Collection<Line> lines) {
+			super(lines);
+		}
+
+		public static Lines of(Collection<Line> lines) {
+			return new Lines(lines);
+		}
+
+		public static void calibrate(Mat uncalibrate) {
+			Core.gemm(Lines.K.inv(), uncalibrate, 1, new Mat(), 0, uncalibrate);
+			Core.normalize(uncalibrate, uncalibrate);
+		}
+
+		public static Mat uncalibrate(Mat calibrated) {
+			Mat uncalibrate = new Mat(3, 1, CvType.CV_64FC1);
+			Core.gemm(Lines.K, calibrated, 1, new Mat(), 0, uncalibrate);
+			if (uncalibrate.get(2, 0)[0] != 0) {
+				uncalibrate.put(0, 0, uncalibrate.get(0, 0)[0] / uncalibrate.get(2, 0)[0]);
+				uncalibrate.put(1, 0, uncalibrate.get(1, 0)[0] / uncalibrate.get(2, 0)[0]);
+				uncalibrate.put(2, 0, 1);
+			}
+			return uncalibrate;
+		}
+
+		private static Mat getLineMat(Line line) {
+			Mat a = new Mat(3, 1, CvType.CV_64FC1);
+			Mat b = new Mat(3, 1, CvType.CV_64FC1);
+			a.put(0, 0, line.getX1());
+			a.put(1, 0, line.getY1());
+			a.put(2, 0, 1d);
+			b.put(0, 0, line.getX2());
+			b.put(1, 0, line.getY2());
+			b.put(2, 0, 1d);
+			Mat an = new Mat(3, 1, CvType.CV_64FC1);
+			Mat bn = new Mat(3, 1, CvType.CV_64FC1);
+			Core.gemm(K.inv(), a, 1, new Mat(), 0, an);
+			Core.gemm(K.inv(), b, 1, new Mat(), 0, bn);
+			Mat li = an.cross(bn);
+			Core.normalize(li, li);
+			a.release();
+			b.release();
+			an.release();
+			bn.release();
+			return li;
+		}
+
+		public Ransac<Line> vanishingPointRansac(double width, double height) {
+			int minimal_sample_set_dimension = 2;
+			double maxError = (float) 0.01623 * 2;
+			if (K == null) {
+				K = new Mat(3, 3, CvType.CV_64FC1, new Scalar(0));
+				K.put(0, 0, width);
+				K.put(0, 2, width / 2);
+				K.put(1, 1, height);
+				K.put(1, 2, height / 2);
+				K.put(2, 2, 1d);
+			}
+			return new Ransac<>(getLines(), getModelProvider(minimal_sample_set_dimension, maxError), minimal_sample_set_dimension, 100, maxError, Double.valueOf(Math.floor(this.size() * 0.7)).intValue());
+		}
+
+		private Function<Collection<Line>, Model<Line>> getModelProvider(int minimal_sample_set_dimension, double maxError) {
+			return datas -> {
+				Mat[] vp = new Mat[1];
+				if (datas.size() == minimal_sample_set_dimension) {
+					Iterator<Line> it = datas.iterator();
+					vp[0] = getLineMat(it.next()).cross(getLineMat(it.next()));
+					Core.normalize(vp[0], vp[0]);
+				} else {
+					// Extract the line segments corresponding to the indexes contained in the set
+					Mat li_set = new Mat(3, datas.size(), CvType.CV_64FC1);
+					Mat tau = new Mat(datas.size(), datas.size(), CvType.CV_64FC1, new Scalar(0, 0, 0));
+					int i = 0;
+					for (Line line : datas) {
+						Mat li = getLineMat(line);
+						li_set.put(0, i, li.get(0, 0));
+						li_set.put(1, i, li.get(1, 0));
+						li_set.put(2, i, li.get(2, 0));
+						tau.put(i, i, line.size());
+						i++;
+					}
+
+					// Least squares solution
+					// Generate the matrix ATA (from LSS_set=A)
+					Mat L = li_set.t();
+					Mat ATA = new Mat(3, 3, CvType.CV_64FC1);
+					Mat dst = new Mat();
+
+					Core.gemm(L.t(), tau.t(), 1, new Mat(), 0, dst);
+					Core.gemm(dst, tau, 1, new Mat(), 0, dst);
+					Core.gemm(dst, L, 1, new Mat(), 0, ATA);
+
+					// Obtain eigendecomposition
+					Mat v = new Mat();
+					Core.SVDecomp(ATA, new Mat(), v, new Mat());
+
+					// Check eigenvecs after SVDecomp
+					if (v.rows() < 3)
+						throw new IllegalStateException();
+
+					// Assign the result (the last column of v, corresponding to the eigenvector with lowest eigenvalue)
+					vp[0] = new Mat(3, 1, CvType.CV_64FC1);
+					vp[0].put(0, 0, v.get(0, 2));
+					vp[0].put(1, 0, v.get(1, 2));
+					vp[0].put(2, 0, v.get(2, 2));
+					Core.normalize(vp[0], vp[0]);
+					vp[0] = uncalibrate(vp[0]);
+				}
+
+				return new Model<Line>() {
+					@Override
+					public double computeError(Line line) {
+						Mat lineMat = getLineMat(line);
+						double di = vp[0].dot(lineMat);
+						di /= (Core.norm(vp[0]) * Core.norm(lineMat));
+						return di * di;
+					}
+
+					@Override
+					public double computeGlobalError(List<Line> datas, Collection<Line> consensusDatas) {
+						double globalError = 0;
+						for (Line line : datas) {
+							double error = computeError(line);
+							if (error > maxError)
+								error = maxError;
+							globalError += error;
+						}
+						globalError = globalError / datas.size();
+						return globalError;
+					}
+
+					@Override
+					public Object[] getParams() {
+						return new Object[] { vp[0] };
+					}
+
+				};
+			};
+		}
+	}
+
+	@Override
+	public void stop() throws Exception {
+		super.stop();
+		timer.shutdown();
+		timer.awaitTermination(5000, TimeUnit.MILLISECONDS);
+		capture.release();
+	}
+
+}
---
+++ gs-cv/src/main/java/org/genericsystem/cv/LinesDetector7.java
@@ -0,0 +1,367 @@
+package org.genericsystem.cv;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import org.genericsystem.cv.lm.LMHostImpl;
+import org.genericsystem.cv.utils.NativeLibraryLoader;
+import org.genericsystem.cv.utils.Tools;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
+import org.opencv.videoio.VideoCapture;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class LinesDetector7 extends AbstractApp {
+
+	static {
+		NativeLibraryLoader.load();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	private final VideoCapture capture = new VideoCapture(0);
+	private ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
+	private Point vp = new Point(0, 0);
+	private Mat calibrateVp;
+
+	@Override
+	protected void fillGrid(GridPane mainGrid) {
+		Mat frame = new Mat();
+		capture.read(frame);
+
+		ImageView frameView = new ImageView(Tools.mat2jfxImage(frame));
+		mainGrid.add(frameView, 0, 0);
+		ImageView deskiewedView = new ImageView(Tools.mat2jfxImage(frame));
+		mainGrid.add(deskiewedView, 0, 1);
+		Mat dePerspectived = frame.clone();
+		if (Lines.K == null) {
+			Lines.K = new Mat(3, 3, CvType.CV_64FC1, new Scalar(0));
+			Lines.K.put(0, 0, frame.width());
+			Lines.K.put(0, 2, frame.width() / 2);
+			Lines.K.put(1, 1, frame.height());
+			Lines.K.put(1, 2, frame.height() / 2);
+			Lines.K.put(2, 2, 1);
+		}
+		calibrateVp = new Mat(3, 1, CvType.CV_64FC1);
+		calibrateVp.put(0, 0, vp.x);
+		calibrateVp.put(1, 0, vp.y);
+		calibrateVp.put(2, 0, 1d);
+		calibrate(calibrateVp);
+		timer.scheduleAtFixedRate(() -> {
+			try {
+				capture.read(frame);
+				Img grad = new Img(frame, false).morphologyEx(Imgproc.MORPH_GRADIENT, Imgproc.MORPH_ELLIPSE, new Size(2, 2)).otsu().morphologyEx(Imgproc.MORPH_CLOSE, Imgproc.MORPH_ELLIPSE, new Size(7, 7));
+				Lines lines = new Lines(grad.houghLinesP(1, Math.PI / 180, 10, 100, 10));
+				if (lines.size() > 10) {
+					lines.draw(frame, new Scalar(0, 0, 255));
+					frameView.setImage(Tools.mat2jfxImage(frame));
+					double r = Core.norm(calibrateVp);
+					double theta = Math.acos(calibrateVp.get(2, 0)[0] / r);
+					double phi = Math.atan2(calibrateVp.get(1, 0)[0], calibrateVp.get(0, 0)[0]);
+					double[] parameters = new LMHostImpl<>((line, params) -> {
+						Mat vn = new Mat(3, 1, CvType.CV_64FC1);
+						vn.put(0, 0, Math.cos(params[1]) * Math.sin(params[0]));
+						vn.put(1, 0, Math.sin(params[1]) * Math.sin(params[0]));
+						vn.put(2, 0, Math.cos(params[0]));
+						return distance(uncalibrate(vn), line);
+					}, lines.lines, new double[] { theta, phi }).getParms();
+					calibrateVp.put(0, 0, r * Math.cos(parameters[1]) * Math.sin(parameters[0]));
+					calibrateVp.put(1, 0, r * Math.sin(parameters[1]) * Math.sin(parameters[0]));
+					calibrateVp.put(2, 0, r * Math.cos(parameters[0]));
+
+					Mat uncalibrate = uncalibrate(calibrateVp);
+					vp = new Point(uncalibrate.get(0, 0)[0], uncalibrate.get(1, 0)[0]);
+					System.out.println("Vanishing point : " + vp);
+					Point bary = new Point(frame.width() / 2, frame.height() / 2);
+					Mat homography = findHomography(vp, bary, frame.width(), frame.height());
+					// lines = new Lines(ransac.getBestDataSet().values()).perspectivTransform(homography);
+					Imgproc.warpPerspective(frame, dePerspectived, homography, frame.size(), Imgproc.INTER_LINEAR, Core.BORDER_REPLICATE, Scalar.all(255));
+					// lines.draw(dePerspectived, new Scalar(0, 255, 0));
+					deskiewedView.setImage(Tools.mat2jfxImage(dePerspectived));
+
+				} else
+					System.out.println("Not enough lines : " + lines.size());
+
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+
+		}, 33, 250, TimeUnit.MILLISECONDS);
+
+	}
+
+	private double distance(Mat vp, Line line) {
+
+		Mat lineSegment = getLineMat(line);
+		double n0 = -lineSegment.get(1, 0)[0];
+		double n1 = lineSegment.get(0, 0)[0];
+		double nNorm = Math.sqrt(n0 * n0 + n1 * n1);
+
+		// Mid point
+		Mat midPoint = getLineMiMat(line);
+		double c0 = midPoint.get(0, 0)[0];
+		double c1 = midPoint.get(1, 0)[0];
+		double c2 = midPoint.get(2, 0)[0];
+
+		// Vanishing point (uncalibrated)
+		double v0 = vp.get(0, 0)[0];
+		double v1 = vp.get(1, 0)[0];
+		double v2 = vp.get(2, 0)[0];
+
+		double r0, r1;
+		r0 = v1 * c2 - v2 * c1;
+		r1 = v2 * c0 - v0 * c2;
+		double rNorm = Math.sqrt(r0 * r0 + r1 * r1);
+
+		double num = (r0 * n0 + r1 * n1);
+		if (num < 0)
+			num = -num;
+
+		double d = 0;
+		if (nNorm != 0 && rNorm != 0)
+			d = num / (nNorm * rNorm);
+
+		// d *= line.size();
+
+		return d;
+	}
+
+	private Mat getLineMat(Line line) {
+		Mat a = new Mat(3, 1, CvType.CV_64FC1);
+		Mat b = new Mat(3, 1, CvType.CV_64FC1);
+		a.put(0, 0, line.x1);
+		a.put(1, 0, line.y1);
+		a.put(2, 0, 1d);
+		b.put(0, 0, line.x2);
+		b.put(1, 0, line.y2);
+		b.put(2, 0, 1d);
+		Mat li = a.cross(b);
+		Core.normalize(li, li);
+		return li;
+	}
+
+	private Mat getLineMiMat(Line line) {
+		Mat a = new Mat(3, 1, CvType.CV_64FC1);
+		Mat b = new Mat(3, 1, CvType.CV_64FC1);
+		a.put(0, 0, line.x1);
+		a.put(1, 0, line.y1);
+		a.put(2, 0, 1d);
+		b.put(0, 0, line.x2);
+		b.put(1, 0, line.y2);
+		b.put(2, 0, 1d);
+		Mat c = new Mat(3, 1, CvType.CV_64FC1);
+		Core.addWeighted(a, 0.5, b, 0.5, 0, c);
+		return c;
+	}
+
+	public static void calibrate(Mat uncalibrate) {
+		Core.gemm(Lines.K.inv(), uncalibrate, 1, new Mat(), 0, uncalibrate);
+		Core.normalize(uncalibrate, uncalibrate);
+	}
+
+	public static Mat uncalibrate(Mat calibrated) {
+		Mat uncalibrate = new Mat(3, 1, CvType.CV_64FC1);
+		Core.gemm(Lines.K, calibrated, 1, new Mat(), 0, uncalibrate);
+		if (uncalibrate.get(2, 0)[0] != 0) {
+			uncalibrate.put(0, 0, uncalibrate.get(0, 0)[0] / uncalibrate.get(2, 0)[0]);
+			uncalibrate.put(1, 0, uncalibrate.get(1, 0)[0] / uncalibrate.get(2, 0)[0]);
+			uncalibrate.put(2, 0, 1);
+		}
+		return uncalibrate;
+	}
+
+	public Point[] rotate(Point bary, double alpha, Point... p) {
+		Mat matrix = Imgproc.getRotationMatrix2D(bary, alpha / Math.PI * 180, 1);
+		MatOfPoint2f results = new MatOfPoint2f();
+		Core.transform(new MatOfPoint2f(p), results, matrix);
+		return results.toArray();
+	}
+
+	private Mat findHomography(Point vp, Point bary, double width, double height) {
+		double alpha_ = Math.atan2((vp.y - bary.y), (vp.x - bary.x));
+		if (alpha_ < -Math.PI / 2 && alpha_ > -Math.PI)
+			alpha_ = alpha_ + Math.PI;
+		if (alpha_ < Math.PI && alpha_ > Math.PI / 2)
+			alpha_ = alpha_ - Math.PI;
+		double alpha = alpha_;
+
+		Point rotatedVp = rotate(bary, alpha, vp)[0];
+
+		Point A = new Point(0, 0);
+		Point B = new Point(width, 0);
+		Point C = new Point(width, height);
+		Point D = new Point(0, height);
+
+		Point AB2 = new Point(width / 2, 0);
+		Point CD2 = new Point(width / 2, height);
+
+		Point A_, B_, C_, D_;
+		if (rotatedVp.x >= width / 2) {
+			A_ = new Line(AB2, rotatedVp).intersection(0);
+			D_ = new Line(CD2, rotatedVp).intersection(0);
+			C_ = new Line(A_, bary).intersection(new Line(CD2, rotatedVp));
+			B_ = new Line(D_, bary).intersection(new Line(AB2, rotatedVp));
+		} else {
+			B_ = new Line(AB2, rotatedVp).intersection(width);
+			C_ = new Line(CD2, rotatedVp).intersection(width);
+			A_ = new Line(C_, bary).intersection(new Line(AB2, rotatedVp));
+			D_ = new Line(B_, bary).intersection(new Line(CD2, rotatedVp));
+		}
+
+		System.out.println("vp : " + vp);
+		System.out.println("rotated vp : " + rotatedVp);
+		System.out.println("Alpha : " + alpha * 180 / Math.PI);
+		// System.out.println("A : " + A + " " + A_);
+		// System.out.println("B : " + B + " " + B_);
+		// System.out.println("C : " + C + " " + C_);
+		// System.out.println("D : " + D + " " + D_);
+
+		return Imgproc.getPerspectiveTransform(new MatOfPoint2f(rotate(bary, -alpha, A_, B_, C_, D_)), new MatOfPoint2f(A, B, C, D));
+	}
+
+	public static class Lines {
+
+		private final List<Line> lines;
+		private static Mat K;
+
+		public Lines(Mat src) {
+			lines = new ArrayList<>();
+			for (int i = 0; i < src.rows(); i++) {
+				double[] val = src.get(i, 0);
+				Line line = new Line(val[0], val[1], val[2], val[3]);
+				lines.add(line);
+			}
+		}
+
+		public Lines(Collection<Line> lines) {
+			this.lines = new ArrayList<>(lines);
+		}
+
+		public Lines rotate(Mat matrix) {
+			return new Lines(lines.stream().map(line -> line.transform(matrix)).collect(Collectors.toList()));
+		}
+
+		public Lines perspectivTransform(Mat matrix) {
+			return new Lines(lines.stream().map(line -> line.perspectivTransform(matrix)).collect(Collectors.toList()));
+		}
+
+		public void draw(Mat frame, Scalar color) {
+			lines.forEach(line -> line.draw(frame, color));
+		}
+
+		public int size() {
+			return lines.size();
+		}
+	}
+
+	public static class Line {
+		private final double x1, y1, x2, y2, angle;
+
+		public Line(Point p1, Point p2) {
+			this(p1.x, p1.y, p2.x, p2.y);
+		}
+
+		public Line(double x1, double y1, double x2, double y2) {
+			this.x1 = x1;
+			this.x2 = x2;
+			this.y1 = y1;
+			this.y2 = y2;
+			this.angle = Math.atan2(y2 - y1, x2 - x1);
+		}
+
+		public double size() {
+			return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
+		}
+
+		public Line transform(Mat rotationMatrix) {
+			MatOfPoint2f results = new MatOfPoint2f();
+			Core.transform(Converters.vector_Point2f_to_Mat(Arrays.asList(new Point(x1, y1), new Point(x2, y2))), results, rotationMatrix);
+			Point[] targets = results.toArray();
+			return new Line(targets[0].x, targets[0].y, targets[1].x, targets[1].y);
+		}
+
+		public Line perspectivTransform(Mat homography) {
+			MatOfPoint2f results = new MatOfPoint2f();
+			Core.perspectiveTransform(Converters.vector_Point2f_to_Mat(Arrays.asList(new Point(x1, y1), new Point(x2, y2))), results, homography);
+			Point[] targets = results.toArray();
+			return new Line(targets[0].x, targets[0].y, targets[1].x, targets[1].y);
+		}
+
+		public void draw(Mat frame, Scalar color) {
+			Imgproc.line(frame, new Point(x1, y1), new Point(x2, y2), color, 1);
+		}
+
+		@Override
+		public String toString() {
+			return "Line : " + angle;
+		}
+
+		public double getAngle() {
+			return angle;
+		}
+
+		public double geta() {
+			return (y2 - y1) / (x2 - x1);
+		}
+
+		public double getOrthoa() {
+			return (x2 - x1) / (y1 - y2);
+		}
+
+		public double getOrthob(Point p) {
+			return p.y - getOrthoa() * p.x;
+		}
+
+		public double getb() {
+			return y1 - geta() * x1;
+		}
+
+		public double distance(Point p) {
+			return Math.abs(geta() * p.x - p.y + getb()) / Math.sqrt(1 + Math.pow(geta(), 2));
+		}
+
+		public Point intersection(double a, double b) {
+			double x = (b - getb()) / (geta() - a);
+			double y = a * x + b;
+			return new Point(x, y);
+		}
+
+		public Point intersection(Line line) {
+			double x = (line.getb() - getb()) / (geta() - line.geta());
+			double y = geta() * x + getb();
+			return new Point(x, y);
+		}
+
+		public Point intersection(double verticalLinex) {
+			double x = verticalLinex;
+			double y = geta() * x + getb();
+			return new Point(x, y);
+		}
+	}
+
+	@Override
+	public void stop() throws Exception {
+		super.stop();
+		timer.shutdown();
+		timer.awaitTermination(5000, TimeUnit.MILLISECONDS);
+		capture.release();
+	}
+
+}
