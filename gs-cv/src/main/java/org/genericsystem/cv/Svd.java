8f7db274f3cedfb8969e17bdd561ce325894d28d
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2018-03-09 11:10:23 +0100

    fix one issue

--- gs-cv/src/main/java/org/genericsystem/cv/Svd.java
+++ gs-cv/src/main/java/org/genericsystem/cv/Svd.java
@@ -1,12 +1,12 @@
 package org.genericsystem.cv;
 
-import java.util.Arrays;
-
 import org.genericsystem.cv.utils.NativeLibraryLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
+
+import java.util.Arrays;
 
 public class Svd {
 	static {
@@ -14,33 +14,13 @@ public class Svd {
 	}
 
 	public static void main(String[] args) {
-		double[][] pts = { { -2, 1, 1 }, { 0, 1, 1 }, { 1, 1, 1 }, { -1, 0, 1 }, { 0, 0, 1 }, { 1, 0, 1 }, { -1, -1, 1 }, { 0, -1, 1 }, { 1, -1, 1 } };
+		double[][] pts = { { 2, 4, 1 }, { 3, 4, 1 }, { 4, 4, 1 }, { 3, 4, 1 }, { 3, 3, 1 }, { 4, 3, 1 }, { 2, 2, 1 }, { 3, 2, 1 }, { 4, 2, 1 } };
 
 		int[][] rects = { { 0, 1, 4, 3 }, { 1, 2, 5, 4 }, { 2, 3, 7, 6 }, { 4, 5, 7, 8 } };
 
 		double[][] result = solve(pts, rects);
 		System.out.println(Arrays.deepToString(result));
 
-		// double[][] datas = { { pts[0][0], -pts[1][0], 0, -pts[3][0], pts[4][0], 0, 0, 0, 0 }, { 0, pts[1][0], -pts[2][0], 0, -pts[4][0], pts[5][0], 0, 0, 0 }, { 0, 0, 0, pts[3][0], -pts[4][0], 0, -pts[6][0], pts[7][0], 0 },
-		// { 0, 0, 0, 0, pts[4][0], -pts[5][0], 0, -pts[7][0], pts[8][0] },
-		//
-		// { pts[0][1], -pts[1][1], 0, -pts[3][1], pts[4][1], 0, 0, 0, 0 }, { 0, pts[1][1], -pts[2][1], 0, -pts[4][1], pts[5][1], 0, 0, 0 }, { 0, 0, 0, pts[3][1], -pts[4][1], 0, -pts[6][1], pts[7][1], 0 },
-		// { 0, 0, 0, 0, pts[4][1], -pts[5][1], 0, -pts[7][1], pts[8][1] },
-		//
-		// { pts[0][2], -pts[1][2], 0, -pts[3][2], pts[4][2], 0, 0, 0, 0 }, { 0, pts[1][2], -pts[2][2], 0, -pts[4][2], pts[5][2], 0, 0, 0 }, { 0, 0, 0, pts[3][2], -pts[4][2], 0, -pts[6][2], pts[7][2], 0 },
-		// { 0, 0, 0, 0, pts[4][2], -pts[5][2], 0, -pts[7][2], pts[8][2] } };
-
-		// double[] coeffs = new double[datas.length];
-		// Arrays.fill(coeffs, 1 / Math.sqrt(9));
-		// double zs[] = solve(datas, coeffs);
-		// System.out.println(Arrays.toString(zs));
-		// System.out.println(Math.sqrt(Arrays.stream(zs).map(d -> d * d).sum()));
-		// System.out.println(Arrays.toString(mul(datas, zs)));
-		// int i = 0;
-		// for (double[] pt : pts) {
-		// System.out.println(zs[i] * pt[0] + ", " + zs[i] * pt[1] + ", " + zs[i] * pt[2]);
-		// i++;
-		// }
 	}
 
 	public static double[][] solve(double[][] srcPts, int[][] rects) {
@@ -48,6 +28,8 @@ public class Svd {
 		// options = argutil_setdefaults(options, 'lambda', [], 'z_constraint', true, 'scale', 1);
 
 		double[] stdxy = { Math.sqrt(Arrays.stream(pts).mapToDouble(pt -> pt[0] * pt[0]).average().getAsDouble()), Math.sqrt(Arrays.stream(pts).mapToDouble(pt -> pt[1] * pt[1]).average().getAsDouble()) };
+
+		System.out.println(Arrays.toString(stdxy));
 
 		for (double[] pt : pts) {
 			pt[0] /= stdxy[0];
@@ -137,26 +119,46 @@ public class Svd {
 		Mat eigenValues = new Mat();
 		Mat eigenVectors = new Mat();
 		Core.eigen(M, eigenValues, eigenVectors);
+
 		Mat result = eigenVectors.col(eigenVectors.cols() - 1);
 		// [V, D] = eigs(M, 1, 'SM');
 		// [minEigValue, minIndex] = min(diag(D));
 		// sol = V(:, minIndex);
 
 		Mat sum = new Mat();
-		Core.reduce(result, sum, 0, Core.REDUCE_SUM);
+		Core.reduce(result, sum, 0, Core.REDUCE_SUM, CvType.CV_64FC1);
 
 		double sum_ = sum.get(0, 0)[0];
 
-		for (int i = 0; i < pts.length; i++)
-			pts[i][2] = sum_ > 0 ? -result.get(i, 0)[0] : result.get(i, 0)[0];
+		Mat error = new Mat();
+
+		for (int i = 0; i < pts.length; i++) {
+			pts[i][0] = sum_ > 0 ? -result.get(3 * i, 0)[0] : result.get(3 * i, 0)[0];
+			pts[i][1] = sum_ > 0 ? -result.get(3 * i + 1, 0)[0] : result.get(3 * i + 1, 0)[0];
+			pts[i][2] = sum_ > 0 ? -result.get(3 * i + 2, 0)[0] : result.get(3 * i + 2, 0)[0];
+		}
 
 		// % normalze it back
 		for (int i = 0; i < pts.length; i++) {
-			pts[i][0] = pts[i][0] * stdxy[0];
-			pts[i][1] = pts[i][1] * stdxy[1];
+			pts[i][0] = result.get(3 * i, 0)[0] * stdxy[0];
+			pts[i][1] = result.get(3 * i + 1, 0)[0] * stdxy[1];
 		}
 
+		// Core.gemm(A, result, 1, new Mat(), 0, error);
+		// for (int row = 0; row < error.rows(); row++) {
+		// for (int col = 0; col < error.cols(); col++) {
+		// System.out.print(error.get(row, col)[0] + " ");
+		// }
+		// System.out.println();
+		// }
+		// System.out.println();
+		//
+		// for (int row = 0; row < result.rows(); row++) {
+		// System.out.println("=== " + result.get(row, 0)[0]);
+		// }
+
 		return pts;
+
 	}
 
 	static double[] mul(double[][] datas, double[] sol) {
@@ -167,39 +169,6 @@ public class Svd {
 			}
 		}
 		return result;
-	}
-
-	static double[] solve(double[][] datas, double[] coeffs) {
-		Mat li_set = new Mat(9, datas.length, CvType.CV_64FC1);
-		Mat tau = new Mat(datas.length, datas.length, CvType.CV_64FC1, new Scalar(0, 0, 0));
-		int i = 0;
-		for (double[] data : datas) {
-			int j = 0;
-			for (double dat : data) {
-				li_set.put(j, i, dat);
-				j++;
-			}
-			tau.put(i, i, coeffs[i]);
-			i++;
-		}
-		Mat L = li_set.t();
-		Mat ATA = new Mat();
-		Mat dst = new Mat();
-
-		Core.gemm(L.t(), tau.t(), 1, new Mat(), 0, dst);
-		Core.gemm(dst, tau, 1, new Mat(), 0, dst);
-		Core.gemm(dst, L, 1, new Mat(), 0, ATA);
-
-		// Obtain eigendecomposition
-		Mat v = new Mat();
-		Core.SVDecomp(ATA, new Mat(), v, new Mat());
-
-		// Check eigenvecs after SVDecomp
-		if (v.rows() < 9)
-			throw new IllegalStateException();
-
-		// normalize ??
-		return new double[] { v.get(0, 8)[0], v.get(1, 8)[0], v.get(2, 8)[0], v.get(3, 8)[0], v.get(4, 8)[0], v.get(5, 8)[0], v.get(6, 8)[0], v.get(7, 8)[0], v.get(8, 8)[0] };
 	}
 
 }
