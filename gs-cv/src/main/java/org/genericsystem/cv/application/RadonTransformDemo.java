5ce6fe16f38fcb88225c617c2fc63447d6b4f088
Nicolas Feybesse <nfeybesse@middlewarefactory.com>
2018-04-03 18:47:00 +0200

    Forget demo

---
+++ gs-cv/src/main/java/org/genericsystem/cv/application/RadonTransformDemo.java
@@ -0,0 +1,130 @@
+package org.genericsystem.cv.application;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.genericsystem.cv.AbstractApp;
+import org.genericsystem.cv.Img;
+import org.genericsystem.cv.utils.NativeLibraryLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import javafx.application.Platform;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class RadonTransformDemo extends AbstractApp {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	static {
+		NativeLibraryLoader.load();
+	}
+
+	private final double f = 6.053 / 0.009;
+
+	private final GSCapture gsCapture = new GSVideoCapture(0, f, GSVideoCapture.HD, GSVideoCapture.VGA);
+	private SuperFrameImg superFrame = gsCapture.read();
+	private ScheduledExecutorService timer = new BoundedScheduledThreadPoolExecutor();
+	private Config config = new Config();
+	private final ImageView[][] imageViews = new ImageView[][] { new ImageView[3], new ImageView[3], new ImageView[3], new ImageView[3] };
+
+	private void startTimer() {
+		timer.scheduleAtFixedRate(() -> {
+			try {
+				Image[] images = doWork();
+				if (images != null)
+					Platform.runLater(() -> {
+						Iterator<Image> it = Arrays.asList(images).iterator();
+						for (int row = 0; row < imageViews.length; row++)
+							for (int col = 0; col < imageViews[row].length; col++)
+								if (it.hasNext())
+									imageViews[row][col].setImage(it.next());
+					});
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+		}, 30, 30, TimeUnit.MILLISECONDS);
+	}
+
+	@Override
+	protected void fillGrid(GridPane mainGrid) {
+		double displaySizeReduction = 1.5;
+		for (int col = 0; col < imageViews.length; col++)
+			for (int row = 0; row < imageViews[col].length; row++) {
+				ImageView imageView = new ImageView();
+				imageViews[col][row] = imageView;
+				mainGrid.add(imageViews[col][row], col, row);
+				imageView.setFitWidth(superFrame.width() / displaySizeReduction);
+				imageView.setFitHeight(superFrame.height() / displaySizeReduction);
+			}
+		startTimer();
+	}
+
+	private Image[] doWork() {
+		System.out.println("do work");
+		if (!config.stabilizedMode)
+			superFrame = gsCapture.read();
+		Image[] images = new Image[4];
+
+		Img binarized = superFrame.getFrame().adaptativeGaussianInvThreshold(7, 3);
+		Rect roi = new Rect(new Point(300, 0), new Point(340, 360));
+
+		Img display = new Img(binarized.getSrc(), true);
+		Imgproc.rectangle(display.getSrc(), roi.br(), roi.tl(), new Scalar(255));
+		images[0] = display.toJfxImage();
+
+		Img strip = new Img(new Mat(binarized.getSrc(), roi));
+		// System.out.println(strip.getSrc());
+		Mat mat = Mat.zeros(360, 640, CvType.CV_8UC1);
+		strip.getSrc().copyTo(new Mat(mat, roi));
+		// System.out.println(mat);
+		images[1] = new Img(mat, false).toJfxImage();
+
+		Mat radon = RadonTransform.transform(strip.getSrc());
+		images[2] = new Img(radon, false).toJfxImage();
+
+		Mat projectionMap = RadonTransform.projectionMap(radon);
+		images[3] = new Img(projectionMap, false).toJfxImage();
+		return images;
+	}
+
+	@Override
+	protected void onS() {
+		config.stabilizedMode = !config.stabilizedMode;
+	}
+
+	@Override
+	protected void onSpace() {
+		if (config.isOn)
+			timer.shutdown();
+		else {
+			timer = new BoundedScheduledThreadPoolExecutor();
+			startTimer();
+		}
+		config.isOn = !config.isOn;
+	}
+
+	@Override
+	protected void onT() {
+		config.textsEnabledMode = !config.textsEnabledMode;
+	}
+
+	@Override
+	public void stop() throws Exception {
+		super.stop();
+		timer.shutdown();
+		timer.awaitTermination(5000, TimeUnit.MILLISECONDS);
+		gsCapture.release();
+	}
+
+}
