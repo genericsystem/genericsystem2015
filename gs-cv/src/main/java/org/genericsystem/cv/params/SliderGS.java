68d97de41357055aad945495af6a15e47907e844
EtienneMWF <esavary@middlewarefactory.com>
2018-06-18 18:26:41 +0200

    Getter and setter configuration for sliderValue

--- gs-cv/src/main/java/org/genericsystem/cv/params/SliderGS.java
+++ gs-cv/src/main/java/org/genericsystem/cv/params/SliderGS.java
@@ -1,22 +1,39 @@
 package org.genericsystem.cv.params;
+import ch.qos.logback.core.net.SyslogOutputStream;
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 
-@SuppressWarnings("restriction")
+
+
 public class SliderGS extends Application {
 
+	final Model model = new Model();
+	
 	final Slider sliderGS = new Slider(0,1,0.5);
 	final Label sliderValueCaption = new Label("Slider Value:");
-	final Label sliderValue = new Label(Double.toString(sliderGS.getValue()));
+//	final Label sliderValue = new Label(Double.toString(sliderGS.getValue()));
+	
 	
 	final static Color textColor = Color.BLACK;
+	
+	public static class Model{
 		
+		public DoubleProperty sliderValue = new SimpleDoubleProperty();
+		
+		public void setSliderValue(DoubleProperty sliderValue) {
+			this.sliderValue = sliderValue;
+		}
+		public DoubleProperty getSliderValue() {
+			return sliderValue;
+		}
+	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
